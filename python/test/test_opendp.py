9147ffe7b489001b6fab8163e5218bfef2c58a78
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2021-03-25 17:41:16 -0700

    Enhanced worflow: * Store artifacts. * Cache dependencies. * Build Python package. * Run Python tests.

--- python/test/test_opendp.py
+++ python/test/test_opendp.py
@@ -4,3 +4,16 @@ import opendp
 def test_init():
     odp = opendp.OpenDP()
     assert odp
+
+def test_identity():
+    odp = opendp.OpenDP()
+
+    ### HELLO WORLD
+    identity = odp.trans.make_identity(b"<String>")
+    arg = odp.data.from_string(b"hello, world!")
+    res = odp.core.transformation_invoke(identity, arg)
+    # TODO: Fix extra quotes
+    assert odp.to_str(res) == '"hello, world!"'
+    odp.data.data_free(arg)
+    odp.data.data_free(res)
+    odp.core.transformation_free(identity)
