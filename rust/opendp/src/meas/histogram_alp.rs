5bc32693ef47aa410b5af7fba94bcf80e2199b8f
chle <chle@itu.dk>
2021-07-26 13:40:43 +0200

    added projection function #183

--- rust/opendp/src/meas/histogram_alp.rs
+++ rust/opendp/src/meas/histogram_alp.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::Rc;
 
-use num::{Integer, Float};
+use num::Integer;
+#[cfg(feature="use-mpfr")]
+use rug::{Float, ops::DivAssignRound, float::Round, Assign, ops::AssignRound};
 
 use crate::core::{Measurement, Function, PrivacyRelation};
 use crate::dist::{L1Distance, MaxDivergence};
@@ -10,6 +12,7 @@ use crate::dom::{AllDomain, MapDomain, SizedDomain};
 use crate::error::Fallible;
 use crate::interactive::Queryable;
 use crate::traits::DistanceCast;
+use crate::samplers::{CastInternalReal, SampleBernoulli};
 
 const ALPHA_DEFAULT : u32 = 4;
 const SIZE_FACTOR_DEFAULT : u32 = 30;
@@ -19,7 +22,7 @@ type SizedHistogramDomain<K, C> = SizedDomain<MapDomain<AllDomain<K>, AllDomain<
 type BitVector = Vec<bool>;
 type HashFunctions<K> = Vec<Rc<dyn Fn(&K) -> usize>>;
 pub struct AlpState<K, T>{
-    alpha: T,
+    alpha: u32,
     scale: T,
     h: HashFunctions<K>,
     z: BitVector 
@@ -30,23 +33,64 @@ fn sample_hash_functions<K>(l: u32, m: usize) -> Fallible<HashFunctions<K>> {
     unimplemented!()
 }
 
-fn compute_projection<K, C, T>(x: &HashMap<K, C>, h: &HashFunctions<K>, alpha: T, scale: T, s: usize) -> BitVector {
+#[cfg(feature="use-mpfr")]
+fn scale_and_round<C, T>(x : C, alpha: u32, scale: T) -> Fallible<usize> 
+    where C: Copy + DistanceCast,
+          T: Copy + DistanceCast {
+    let r = Float::with_val(150, f64::distance_cast(x)?) * Float::with_val(150, f64::distance_cast(scale)?) / Float::with_val(150, alpha);
+    let floored = f64::from_internal(r.clone().floor()) as usize;
+    // TODO: Potential rounding when casting to f64. Should be considered for privacy proof
+    match bool::sample_bernoulli(f64::from_internal(r.fract()), false)? {
+        true => Ok(floored + 1),
+        false => Ok(floored)
+    }
+}
+
+#[cfg(not(feature="use-mpfr"))]
+fn scale_and_round<C, T>(x : C, alpha: &u32, scale: &T) -> Fallible<usize> {
     unimplemented!()
+}
+
+#[cfg(feature="use-mpfr")]
+fn compute_prob(alpha: u32) -> f64 {
+    let mut p = Float::with_val(53, 1.0);
+    p.div_assign_round( Float::with_val(53, alpha + 2), Round::Up); // Round up to preserve privacy
+    f64::from_internal(p)
+}
+
+#[cfg(not(feature="use-mpfr"))]
+fn compute_prob(alpha: &u32) -> f64 {
+    unimplemented!()
+}
+
+fn compute_projection<K, C, T>(x: &HashMap<K, C>, h: &HashFunctions<K>, alpha: u32, scale: T, s: usize) -> Fallible<BitVector> 
+    where C: Copy + DistanceCast,
+          T: Copy + CastInternalReal + DistanceCast {
+    let mut z = vec![false; s];
+
+    for (k, v) in x.iter() {
+        let round = scale_and_round(*v, alpha, scale)?; 
+        h.iter().take(round).for_each(|f| z[f(k) % s] = true); // TODO: Hash collisions can be handled using OR or XOR
+    }
+
+    let p = compute_prob(alpha);
+
+    z.iter().map(|b| bool::sample_bernoulli(p , false).map(|flip| b ^ flip)).collect::<Fallible<Vec<bool>>>()
 }
 
 fn compute_estimate<K, T>(state: &AlpState<K, T>, key: &K) -> T {
     unimplemented!()
 }
 
-pub fn make_alp_histogram<K, C, T>(n: usize, alpha: T, scale: T, s: usize, h: HashFunctions<K>) 
+pub fn make_alp_histogram<K, C, T>(n: usize, alpha: u32, scale: T, s: usize, h: HashFunctions<K>) 
         -> Fallible<Measurement<SizedHistogramDomain<K, C>, 
                                 AlpDomain<K, T>, 
                                 L1Distance<C>, MaxDivergence<T>>>
     where K: 'static + Eq + Hash,
-          C: 'static + Integer + DistanceCast,
-          T: 'static + Float + DistanceCast {
+          C: 'static + Copy + Integer + DistanceCast,
+          T: 'static + num::Float + DistanceCast + CastInternalReal {
     
-    if alpha.is_sign_negative() || alpha.is_zero() {
+    if alpha == 0 {
         return fallible!(MakeMeasurement, "alpha must be positive")
     }
     if scale.is_sign_negative() || scale.is_zero() {
@@ -59,13 +103,13 @@ pub fn make_alp_histogram<K, C, T>(n: usize, alpha: T, scale: T, s: usize, h: Ha
     Ok(Measurement::new(
         SizedDomain::new(MapDomain { key_domain: AllDomain::new(), value_domain: AllDomain::new() }, n),
         AllDomain::new(),
-        Function::new(move |x: &HashMap<K, C>| {
-            let z = compute_projection(x, &h, alpha, scale, s);
-            AlpState { alpha, scale, h:h.clone(), z }
+        Function::new_fallible(move |x: &HashMap<K, C>| {
+            let z = compute_projection(x, &h, alpha, scale, s)?;
+            Ok(AlpState { alpha, scale, h:h.clone(), z })
         }),
         L1Distance::default(),
         MaxDivergence::default(),
-        PrivacyRelation::new_from_constant(scale * alpha)
+        PrivacyRelation::new_from_constant(scale * T::distance_cast(alpha)?)
     ))
 }
 
@@ -75,7 +119,7 @@ pub fn make_alp_histogram_parameterized<K, C, T>(n: usize, alpha: T, scale: T, b
                                 L1Distance<C>, MaxDivergence<T>>>
     where K: 'static + Eq + Hash,
           C: 'static + Integer + DistanceCast,
-          T: 'static + Float + DistanceCast {
+          T: 'static + num::Float + DistanceCast {
     
     unimplemented!()
 }
@@ -86,7 +130,7 @@ pub fn make_alp_histogram_simple<K, C, T>(n: usize, scale: T, beta: C)
                                 L1Distance<C>, MaxDivergence<T>>>
     where K: 'static + Eq + Hash,
           C: 'static + Integer + DistanceCast,
-          T: 'static + Float + DistanceCast {
+          T: 'static + num::Float + DistanceCast {
     
     make_alp_histogram_parameterized(n, T::distance_cast(ALPHA_DEFAULT)?, scale, beta, T::distance_cast(SIZE_FACTOR_DEFAULT)?)
 }
