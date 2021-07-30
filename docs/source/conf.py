9705d356df0a71a3bb51552687e685e1ccf72295
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2021-07-30 12:30:43 -0700

    Remove 148-docs hard wiring.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ html_sidebars = {
 smv_tag_whitelist = r'^v.*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r'(stable|latest|148-docs)'
+smv_branch_whitelist = r'(stable|latest)'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r'origin'
