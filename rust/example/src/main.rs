c0652a920cc421e95e20d798dfb9f577515864fe
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2021-08-04 00:32:41 +0200

    some examples

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use opendp::error::Fallible;
+use opendp::dom::AllDomain;
+use opendp::meas::{LaplaceDomain, make_base_laplace};
+
+fn main() -> Fallible<()> {
+    println!("OpenDP example");
+    let measurement = make_base_laplace::<AllDomain<f64>>(1.0)?;
+    let _ret = measurement.function.eval(&0.0)?;
+    println!("{:?}", _ret);
+
+    println!("{:?}", (measurement.privacy_relation.relation)(&1.0, &0.01));
+    return Ok(());
 }
