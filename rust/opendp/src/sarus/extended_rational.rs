43c458a684aea395dc9d4d7e292c3a79e6212f30
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2021-08-08 22:00:51 +0200

    extended_rational

--- rust/opendp/src/sarus/extended_rational.rs
+++ rust/opendp/src/sarus/extended_rational.rs
@@ -1,78 +1,119 @@
 use std::ops::{Add, Sub, Mul, Div, Rem};
 use std::cmp::Ordering;
-use std::fmt;
 use std::iter::Iterator;
 use std::convert::TryFrom;
-use rug::Integer;
-use num::{Num, One, Zero};
+use std::fmt;
+use rug::Rational;
+use num::{One, Zero};
+use crate::error::*;
 
 #[derive(Clone,Debug)]
-pub struct ExtendedRational {
-    numerator: Integer,
-    /// This can be 0 to represent + or - infnity
-    denominator: Integer,
+pub enum ExtendedRational {
+    NegativeInfinity,
+    PositiveInfinity,
+    Number(Rational),
 }
+
+use ExtendedRational::*;
 
 impl ExtendedRational {
     /// Constructor
-    pub fn new<N:Into<Integer>,D:Into<Integer>>(numerator:N, denominator:D) -> ExtendedRational {
-        ExtendedRational {numerator:numerator.into(), denominator:denominator.into()}
-    }
-
-    pub fn positive_infinity() -> ExtendedRational {ExtendedRational::new(1,0)}
-    pub fn negative_infinity() -> ExtendedRational {ExtendedRational::new(-1,0)}
-    pub fn indeterminate() -> ExtendedRational {ExtendedRational::new(0,0)}
-    pub fn is_number(&self) -> bool {self.denominator == 0}
-    pub fn is_positive_infinity(&self) -> bool {self.numerator == 1 && self.denominator == 0}
-    pub fn is_negative_infinity(&self) -> bool {self.numerator == -1 && self.denominator == 0}
-    pub fn is_indeterminate(&self) -> bool {self.numerator == 0 && self.denominator == 0}
-    /// Simplify the fraction
-    fn simplify(&self) -> ExtendedRational {
-        if self.denominator!=0 {
-            let gcd = self.numerator.clone().gcd(&self.denominator);
-            ExtendedRational::new(
-                self.numerator.clone()/&gcd,
-                self.denominator.clone()/&gcd
-            )
-        } else {
-            // Does nothing
-            self.clone()
+    pub fn new(numerator:i128, denominator:i128) -> Fallible<ExtendedRational> {
+        match (numerator,denominator) {
+            (n,d) if d>0 => Ok(Number(Rational::from((n, d)))),
+            (-1, 0) => Ok(NegativeInfinity),
+            ( 1, 0) => Ok(PositiveInfinity),
+            _ => fallible!(DomainMismatch),
         }
     }
 }
 
-impl Zero for ExtendedRational {
-    fn zero() -> ExtendedRational {
-        return ExtendedRational::new(0,1)
+use Ordering::*;
+
+/// Order relation of extended rationals
+impl PartialOrd for ExtendedRational {
+    fn partial_cmp(&self, other: &ExtendedRational) -> Option<Ordering> {
+        match (self, other) {
+            (Number(r), Number(s)) => r.partial_cmp(s),
+            (NegativeInfinity, Number(s)) => Some(Less),
+            (PositiveInfinity, Number(s)) => Some(Greater),
+            (Number(r), NegativeInfinity) => Some(Greater),
+            (Number(r), PositiveInfinity) => Some(Less),
+            (NegativeInfinity, NegativeInfinity) => Some(Equal),
+            (NegativeInfinity, PositiveInfinity) => Some(Less),
+            (PositiveInfinity, NegativeInfinity) => Some(Greater),
+            (PositiveInfinity, PositiveInfinity) => Some(Equal),
+            _ => None,
+        }
     }
+}
+
+/// Order relation of extended rationals
+impl Ord for ExtendedRational {
+    fn cmp(&self, other: &ExtendedRational) -> Ordering {
+        match (self, other) {
+            (Number(r), Number(s)) => r.cmp(s),
+            (NegativeInfinity, Number(s)) => Less,
+            (PositiveInfinity, Number(s)) => Greater,
+            (Number(r), NegativeInfinity) => Greater,
+            (Number(r), PositiveInfinity) => Less,
+            (NegativeInfinity, NegativeInfinity) => Equal,
+            (NegativeInfinity, PositiveInfinity) => Less,
+            (PositiveInfinity, NegativeInfinity) => Greater,
+            (PositiveInfinity, PositiveInfinity) => Equal,
+            _ => Less,
+        }
+    }
+}
+
+/// Equivalence of extended rationals
+impl PartialEq for ExtendedRational {
+    fn eq(&self, other: &ExtendedRational) -> bool {
+        match (self, other) {
+            (Number(r), Number(s)) => r.eq(s),
+            (NegativeInfinity, NegativeInfinity) => true,
+            (PositiveInfinity, PositiveInfinity) => true,
+            _ => false,
+        }
+    }
+}
+
+/// Equivalence of extended rationals
+impl Eq for ExtendedRational {}
+
+/// Integer to extended rational
+impl From<i128> for ExtendedRational {
+    fn from(integer: i128) -> Self {
+        ExtendedRational::new(integer,1).unwrap_assert("Denominador is positive")
+    }
+}
+
+impl Zero for ExtendedRational {
+    fn zero() -> ExtendedRational {0.into()}
 
     fn is_zero(&self) -> bool {
-        return self.numerator == 0 && self.denominator != 0;
+        match self {
+            ExtendedRational::Number(rational) => *rational == Rational::from(0),
+            _ => false,
+        }
     }
 }
 
 impl One for ExtendedRational {
-    fn one() -> ExtendedRational {
-        return ExtendedRational::new(1,1)
-    }
+    fn one() -> ExtendedRational {1.into()}
 }
 
 /// Sum of extended rationals
 impl Add for ExtendedRational {
     type Output = ExtendedRational;
     fn add(self, other: ExtendedRational) -> ExtendedRational {
-        if self.denominator > 0 {
-            if other.denominator > 0 {
-                ExtendedRational::new(
-                    self.numerator*&other.denominator + &self.denominator*other.numerator,
-                    self.denominator * other.denominator,
-                ).simplify()
-            } else {other}
-        } else {
-            if other.denominator > 0 {self}
-            else if self.numerator == other.denominator {
-                self
-            } else {ExtendedRational::indeterminate()}
+        match (self, other) {
+            (Number(r), Number(s)) => Number(r+s),
+            (NegativeInfinity, Number(s)) => NegativeInfinity,
+            (PositiveInfinity, Number(s)) => PositiveInfinity,
+            (Number(r), NegativeInfinity) => NegativeInfinity,
+            (Number(r), PositiveInfinity) => PositiveInfinity,
+            (s,_) => s,
         }
     }
 }
@@ -81,18 +122,13 @@ impl Add for ExtendedRational {
 impl Sub for ExtendedRational {
     type Output = ExtendedRational;
     fn sub(self, other: ExtendedRational) -> ExtendedRational {
-        if self.denominator > 0 {
-            if other.denominator > 0 {
-                ExtendedRational::new(
-                    self.numerator*&other.denominator + &self.denominator*other.numerator,
-                    self.denominator * other.denominator,
-               ).simplify()
-            } else {ExtendedRational::new(-other.numerator, other.denominator)}
-        } else {
-            if other.denominator > 0 {self}
-            else if self.numerator == -other.denominator {
-                self
-            } else {ExtendedRational::indeterminate()}
+        match (self, other) {
+            (Number(r), Number(s)) => Number(r-s),
+            (NegativeInfinity, Number(s)) => NegativeInfinity,
+            (PositiveInfinity, Number(s)) => PositiveInfinity,
+            (Number(r), NegativeInfinity) => PositiveInfinity,
+            (Number(r), PositiveInfinity) => NegativeInfinity,
+            (s,_) => s,
         }
     }
 }
@@ -101,14 +137,18 @@ impl Sub for ExtendedRational {
 impl Mul for ExtendedRational {
     type Output = ExtendedRational;
     fn mul(self, other: ExtendedRational) -> ExtendedRational {
-        if self.denominator > 0 {
-            if other.denominator > 0 {
-                ExtendedRational::new(
-                    self.numerator * other.numerator,
-                    self.denominator * other.denominator,
-               ).simplify()
-            } else {ExtendedRational::new((self.numerator*other.numerator).signum(), 0)}
-        } else {ExtendedRational::new((self.numerator*other.numerator).signum(), self.denominator)}
+        match (self, other) {
+            (Number(r), Number(s)) => Number(r*s),
+            (NegativeInfinity, s) if s>0.into() => NegativeInfinity,
+            (PositiveInfinity, s) if s>0.into() => PositiveInfinity,
+            (NegativeInfinity, s) if s<0.into() => PositiveInfinity,
+            (PositiveInfinity, s) if s<0.into() => NegativeInfinity,
+            (r, NegativeInfinity) if r>0.into() => NegativeInfinity,
+            (r, PositiveInfinity) if r>0.into() => PositiveInfinity,
+            (r, NegativeInfinity) if r<0.into() => PositiveInfinity,
+            (r, PositiveInfinity) if r<0.into() => NegativeInfinity,
+            (s,_) => s,
+        }
     }
 }
 
@@ -116,10 +156,20 @@ impl Mul for ExtendedRational {
 impl Div for ExtendedRational {
     type Output = ExtendedRational;
     fn div(self, other: ExtendedRational) -> ExtendedRational {
-        ExtendedRational::new( 
-             self.numerator / other.denominator,
-             self.denominator / other.numerator,
-        ).simplify()
+        match (self, other) {
+            (r, s) if r > 0.into() && s==0.into() => PositiveInfinity,
+            (r, s) if r < 0.into() && s==0.into() => NegativeInfinity,
+            (Number(r), Number(s)) if s != 0 => Number(r/s),
+            (NegativeInfinity, s) if s>0.into() => NegativeInfinity,
+            (PositiveInfinity, s) if s>0.into() => PositiveInfinity,
+            (NegativeInfinity, s) if s<0.into() => PositiveInfinity,
+            (PositiveInfinity, s) if s<0.into() => NegativeInfinity,
+            (r, NegativeInfinity) if r>0.into() => NegativeInfinity,
+            (r, PositiveInfinity) if r>0.into() => PositiveInfinity,
+            (r, NegativeInfinity) if r<0.into() => PositiveInfinity,
+            (r, PositiveInfinity) if r<0.into() => NegativeInfinity,
+            (s,_) => s,
+        }
     }
 }
 
@@ -127,53 +177,16 @@ impl Div for ExtendedRational {
 impl Rem for ExtendedRational {
     type Output = ExtendedRational;
     fn rem(self, other: ExtendedRational) -> ExtendedRational {
-        ExtendedRational::new(0, 1).simplify()
-    }
-}
-
-/// Order relation of extended rationals
-impl PartialOrd for ExtendedRational {
-    fn partial_cmp(&self, other: &ExtendedRational) -> Option<Ordering> {
-        let copy = self.clone();
-        (copy.numerator*&other.denominator).partial_cmp(&(copy.denominator*&other.numerator))
-    }
-}
-
-impl PartialEq for ExtendedRational {
-    fn eq(&self, other: &ExtendedRational) -> bool {
-        let copy = self.clone();
-        return copy.numerator*&other.denominator == copy.denominator*&other.numerator;
-    }
-}
-
-/// Equivalence of extended rationals
-impl Eq for ExtendedRational {}
-
-/// Order relation of extended rationals
-impl Ord for ExtendedRational {
-    fn cmp(&self, other: &ExtendedRational) -> Ordering {
-        let copy = self.clone();
-        (copy.numerator*&other.denominator).cmp(&(copy.denominator*&other.numerator))
-    }
-}
-
-/// Integer to extended rational
-impl From<Integer> for ExtendedRational {
-    fn from(integer: Integer) -> Self {
-        ExtendedRational::new(integer,1)
+        ExtendedRational::from(0)
     }
 }
 
 impl fmt::Display for ExtendedRational {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.denominator {
-            d if *d==0 => match &self.numerator {
-                n if *n==-1 => write!(f, "-inf"),
-                n if *n==0 => write!(f, "indeterminate"),
-                n if *n==1 => write!(f, "+inf"),
-                _ => write!(f, "invalid"),
-            },
-            _ => write!(f, "{}/{}", self.numerator, self.denominator),
+        match &self {
+            Number(r) => write!(f, "{}", r),
+            NegativeInfinity => write!(f, "-inf"),
+            PositiveInfinity => write!(f, "+inf"),
         }
     }
 }
@@ -188,15 +201,15 @@ impl From<&str> for ExtendedRational {
                 ((0,0,0,0,false),'i'|'I') => (2, 1,1,0,false),
                 ((1,-1,0,0,false),'i'|'I') => (2,-1,1,0,false),
                 ((1, 1,0,0,false),'i'|'I') => (2, 1,1,0,false),
-                ((0,0,0,0,false),'0'..='9') => (1,1,i32::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),1,false),
-                ((read,sign,num,1,false),'0'..='9') => (read+1,sign, num*10 + i32::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),1,false),
+                ((0,0,0,0,false),'0'..='9') => (1,1,i128::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),1,false),
+                ((read,sign,num,1,false),'0'..='9') => (read+1,sign, num*10 + i128::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),1,false),
                 ((read,sign,num,1,false),'.') => (read+1,sign,num,1,true),
-                ((read,sign,num,denom,true),'0'..='9') => (read+1,sign,num*10 + i32::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),denom*10,true),
+                ((read,sign,num,denom,true),'0'..='9') => (read+1,sign,num*10 + i128::try_from(char.to_digit(10).unwrap_or_default()).unwrap_or_default(),denom*10,true),
                 ((2,sign,1,0,false),'n'|'N') => (3,sign,1,0,false),
                 ((3,sign,1,0,false),'f'|'F') => (4,sign,1,0,false),
                 (state,_) => state,
             }
         });
-        ExtendedRational::new(sign*numerator,denominator).simplify()
+        ExtendedRational::new(sign*numerator,denominator).unwrap_or(ExtendedRational::zero())
     }
 }
