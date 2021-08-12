5ea56fa39039969f9606f606864871f9c372e89d
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2021-08-13 01:36:43 +0200

    Considered done

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -1,17 +1,14 @@
-use opendp::error::Fallible;
 use opendp::dom::AllDomain;
-use opendp::meas::{make_base_laplace};
-use opendp::sarus::{make_pld_gaussian, make_pld_laplace};
-use opendp::chain::make_basic_composition;
+use opendp::sarus::{make_pld_composition, make_pld_gaussian, make_pld_laplace};
 
 use serde::{Deserialize, Serialize};
 use vega_lite_4::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("OpenDP example");
-
     // gaussian_plot_example()?;
     laplace_plot_example()?;
+    laplace_comp_plot_example()?;
 
     // gaussian_comp_example()?;
     return Ok(());
@@ -22,7 +19,7 @@ fn gaussian_plot_example() -> Result<(), Box<dyn std::error::Error>> {
     let gauss_meas = make_pld_gaussian::<AllDomain<f64>>(1.0)?;
     plot_fs(vec![
         gauss_meas.output_measure.f(&1.0),
-        // gauss_meas.output_measure.f(&2.0),
+        gauss_meas.output_measure.f(&2.0),
     ])?;
     // let values: Vec<Point> = meas_0.output_measure.f(&1.0).into_iter().map(|(x,y)| Point {x,y}).collect();
     // the chart
@@ -31,13 +28,25 @@ fn gaussian_plot_example() -> Result<(), Box<dyn std::error::Error>> {
 
 fn laplace_plot_example() -> Result<(), Box<dyn std::error::Error>> {
     println!("Plot Laplace examples");
-    let lap_meas = make_pld_laplace::<AllDomain<f64>>(1.0)?;
+    let lap_meas = make_pld_laplace::<AllDomain<f64>>(0.1)?;
     plot_fs(vec![
-        lap_meas.output_measure.f(&0.5),
-        // lap_meas.output_measure.f(&2.0),
+        lap_meas.output_measure.f(&0.1),
+        lap_meas.output_measure.f(&1.0),
     ])?;
     // let values: Vec<Point> = meas_0.output_measure.f(&1.0).into_iter().map(|(x,y)| Point {x,y}).collect();
     // the chart
+    Ok(())
+}
+
+fn laplace_comp_plot_example() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Laplace and Gauussian example");
+    let meas_0 = make_pld_laplace::<AllDomain<f64>>(1.0)?;
+    let meas_1 = make_pld_laplace::<AllDomain<f64>>(1.0)?;
+    let comp_meas = make_pld_composition(&meas_0, &meas_1)?;
+    plot_fs(vec![
+        comp_meas.output_measure.f(&0.5),
+        meas_0.output_measure.f(&1.0),
+    ])?;
     Ok(())
 }
 
@@ -84,26 +93,5 @@ fn plot_fs(fs: Vec<Vec<(f64,f64)>>) -> Result<(), Box<dyn std::error::Error>> {
     chart.show()?;
     // // print the vega lite spec
     // eprint!("{}", chart.to_string()?);
-    Ok(())
-}
-
-fn laplace_example() -> Fallible<()> {
-    println!("Laplace example");
-    let measurement = make_base_laplace::<AllDomain<f64>>(1.0)?;
-    let _ret = measurement.function.eval(&0.0)?;
-    println!("{:?}", _ret);
-    println!("{:?}", (measurement.privacy_relation.relation)(&1.0, &0.01));
-    Ok(())
-}
-
-fn gaussian_comp_example() -> Fallible<()> {
-    println!("Laplace and Gauussian example");
-    let meas_0 = make_pld_gaussian::<AllDomain<f64>>(1.0)?;
-    let meas_1 = make_pld_gaussian::<AllDomain<f64>>(1.0)?;
-
-    println!("{:#?}", meas_0.output_measure.f(&1.0));
-
-    let comp_meas = make_basic_composition(&meas_0, &meas_1)?;
-    println!("Composed {:?}", (comp_meas.privacy_relation.relation)(&0.1, &(1., 0.02)));
     Ok(())
 }
