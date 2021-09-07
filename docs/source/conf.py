1ba91592b781a24217df240e02c0186c66bb09b1
Shoeboxam <shoeboxam@gmail.com>
2021-09-07 11:57:07 -0400

    version 0.2.0-rc.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ copyright = u'%d' % datetime.now().year
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.0-rc.3'
+version = '0.2.0-rc.4'
 # The full version, including alpha/beta/rc tags.
 #release = ''
 
