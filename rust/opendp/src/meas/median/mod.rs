495a18f8eb2eb8484b0c823ce74b6156f8b101ee
Shoeboxam <shoeboxam@gmail.com>
2021-09-29 18:54:33 -0400

    add test

--- rust/opendp/src/meas/median/mod.rs
+++ rust/opendp/src/meas/median/mod.rs
@@ -1,6 +1,6 @@
 use std::cmp::Ordering;
 
-use crate::core::{Domain, Function, Measurement, Metric, PrivacyRelation};
+use crate::core::{Function, Measurement, PrivacyRelation};
 use crate::dist::{MaxDivergence, SymmetricDistance};
 use crate::dom::{AllDomain, BoundedDomain, VectorDomain};
 use crate::error::Fallible;
@@ -31,7 +31,7 @@ fn median_smooth_sensitivity(
         .max_by(partial_max).unwrap_or(f64::INFINITY)
 }
 
-pub fn make_smooth_sensitivity_median<DIA: Domain, DO: Domain, MI: 'static + Metric>(
+pub fn make_smooth_sensitivity_median(
     bounds: (f64, f64), influence: u32, epsilon: f64,
 ) -> Fallible<Measurement<VectorDomain<BoundedDomain<f64>>, AllDomain<f64>, SymmetricDistance, MaxDivergence<f64>>> {
     if influence < 1 { return fallible!(MakeMeasurement, "influence must be positive") }
@@ -43,7 +43,7 @@ pub fn make_smooth_sensitivity_median<DIA: Domain, DO: Domain, MI: 'static + Met
         AllDomain::new(),
         Function::new_fallible(move |data: &Vec<f64>| {
             let mut sorted_data = data.clone();
-            sorted_data.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+            sorted_data.sort_by(partial_max);
             let median = sorted_data[(sorted_data.len() + 1) / 2];
 
             let sensitivity = median_smooth_sensitivity(sorted_data, bounds, epsilon_prime);
@@ -61,8 +61,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_function() {
+    fn test_function() -> Fallible<()> {
         let sens = median_smooth_sensitivity(vec![2., 3., 4.], (1., 0.3), 1.);
         println!("{}", sens);
+
+        let meas = make_smooth_sensitivity_median((-1., 5.), 1, 1.)?;
+
+        let res= meas.invoke(&vec![-0.05, 2., 3., 0., 4., 1.2, 0.7, 1.3])?;
+        println!("{}", res);
+        Ok(())
     }
 }
