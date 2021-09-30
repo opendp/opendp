30403a14bcca22848b6764a87fb649da5da37063
Shoeboxam <shoeboxam@gmail.com>
2021-09-29 22:02:00 -0400

    add rust tests

--- rust/opendp/src/trans/index/mod.rs
+++ rust/opendp/src/trans/index/mod.rs
@@ -68,3 +68,36 @@ pub fn make_index<TOA>(
         StabilityRelation::new_from_constant(1)
     ))
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find() -> Fallible<()> {
+        let find = make_find(vec!["1", "3", "4"])?;
+        assert_eq!(
+            find.invoke(&vec!["1", "2", "3", "4", "5"])?,
+            vec![0, 3, 1, 2, 3]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_bin() -> Fallible<()> {
+        let bin = make_find_bin(vec![2, 3, 5])?;
+        assert_eq!(
+            bin.invoke(&(1..10).collect())?,
+            vec![0, 1, 2, 2, 3, 3, 3, 3, 3]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_index() -> Fallible<()> {
+        let index = make_index(vec!["A", "B", "C"], "NA")?;
+        assert_eq!(
+            index.invoke(&vec![0, 1, 3, 1, 5])?,
+            vec!["A", "B", "NA", "B", "NA"]);
+        Ok(())
+    }
+}
