7f78f8df8c0f8622d2ec457cdc0a6c4b77eca0ee
Shoeboxam <shoeboxam@gmail.com>
2021-11-05 19:08:07 -0400

    fix tests

--- python/test/test_comb.py
+++ python/test/test_comb.py
@@ -1,5 +1,4 @@
 from opendp.mod import enable_features
-from opendp.typing import *
 
 enable_features("floating-point", "contrib")
 
@@ -8,12 +7,16 @@ def test_amplification():
     from opendp.trans import make_sized_bounded_mean
     from opendp.meas import make_base_laplace
     from opendp.comb import make_population_amplification
+    # prior info
+    size = 10
+    bounds = (0., 10.)
 
-    meas = make_sized_bounded_mean(size=10, bounds=(0., 10.)) >> make_base_laplace(scale=0.5)
-
-    amplified = make_population_amplification(meas, population_size=100)
+    scale = 0.5
+    meas = make_sized_bounded_mean(size=size, bounds=bounds) >> make_base_laplace(scale=scale)
+    ideal_epsilon = (bounds[1] - bounds[0]) / size / scale
+    assert meas.check(2, ideal_epsilon)
+    assert not meas.check(2, ideal_epsilon - 1e-6)
+    amplified = make_population_amplification(meas, population_size=size * 10)
     print("amplified base laplace:", amplified([1.] * 10))
-    assert meas.check(1, 1.)
-    assert not meas.check(1, 0.999)
-    assert amplified.check(1, 0.159)
-    assert not amplified.check(1, 0.158)
+    assert amplified.check(2, ideal_epsilon / 4.)
+    assert not amplified.check(2, ideal_epsilon / 5.)
