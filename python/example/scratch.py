b25fc085f9d246f7a57350c4121e8b897a951f5c
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2022-01-03 12:12:27 -0800

    WIP

--- python/example/scratch.py
+++ python/example/scratch.py
@@ -1,6 +1,7 @@
 import ctypes
-import pyarrow as pa
 import sys
+
+import pyarrow as pa
 
 ARROW_FLAG_DICTIONARY_ORDERED = 1
 ARROW_FLAG_NULLABLE = 2
@@ -20,19 +21,17 @@ ARROW_FLAG_MAP_KEYS_SORTED = 4
 #   // Opaque producer-specific data
 #   void* private_data;
 # };
-class ArrowSchema(ctypes.Structure):
+class FFI_ArrowSchema(ctypes.Structure):
     pass
-    # def __del__(self):
-    #     print("ArrowSchema del")
-ArrowSchema._fields_ = [
+FFI_ArrowSchema._fields_ = [
     ("format", ctypes.c_char_p),
     ("name", ctypes.c_char_p),
     ("metadata", ctypes.c_char_p),
     ("flags", ctypes.c_int64),
     ("n_children", ctypes.c_int64),
-    ("children", ctypes.POINTER(ctypes.POINTER(ArrowSchema))),
-    ("dictionary", ctypes.POINTER(ArrowSchema)),
-    ("release", ctypes.POINTER(ctypes.CFUNCTYPE(None, ctypes.POINTER(ArrowSchema)))),
+    ("children", ctypes.POINTER(ctypes.POINTER(FFI_ArrowSchema))),
+    ("dictionary", ctypes.POINTER(FFI_ArrowSchema)),
+    ("release", ctypes.POINTER(ctypes.CFUNCTYPE(None, ctypes.POINTER(FFI_ArrowSchema)))),
     ("private_data", ctypes.c_void_p)
 ]
 
@@ -51,108 +50,143 @@ ArrowSchema._fields_ = [
 #   // Opaque producer-specific data
 #   void* private_data;
 # };
-class ArrowArray(ctypes.Structure):
-    pass
-    # def __del__(self):
-    #     print("ArrowArray del")
-ArrowArray._fields_ = [
+class FFI_ArrowArray(ctypes.Structure):
+    def __str__(self):
+        return "{}: {{{}}}".format(self.__class__.__name__,
+                                   ", ".join(["{}: {}".format(field[0],
+                                                              getattr(self,
+                                                                      field[0]))
+                                              for field in self._fields_]))
+FFI_ArrowArray._fields_ = [
     ("length", ctypes.c_int64),
     ("null_count", ctypes.c_int64),
     ("offset", ctypes.c_int64),
     ("n_buffers", ctypes.c_int64),
     ("n_children", ctypes.c_int64),
     ("buffers", ctypes.POINTER(ctypes.c_void_p)),
-    ("children", ctypes.POINTER(ctypes.POINTER(ArrowArray))),
-    ("dictionary", ctypes.POINTER(ArrowArray)),
-    ("release", ctypes.POINTER(ctypes.CFUNCTYPE(None, ctypes.POINTER(ArrowArray)))),
+    ("children", ctypes.POINTER(ctypes.POINTER(FFI_ArrowArray))),
+    ("dictionary", ctypes.POINTER(FFI_ArrowArray)),
+    ("release", ctypes.POINTER(ctypes.CFUNCTYPE(None, ctypes.POINTER(FFI_ArrowArray)))),
     ("private_data", ctypes.c_void_p)
 ]
 
-class ArrowArraySchema(ctypes.Structure):
-    pass
-    # def __del__(self):
-    #     print("ArrowArraySchema del")
+class FFI_Array(ctypes.Structure):
     _fields_ = [
-        ("array", ctypes.POINTER(ArrowArray)),
-        ("schema", ctypes.POINTER(ArrowSchema))
+        ("array", ctypes.POINTER(FFI_ArrowArray)),
+        ("schema", ctypes.POINTER(FFI_ArrowSchema))
     ]
+    @classmethod
+    def new_empty(cls):
+        array = cls()
+        array.array = ctypes.pointer(FFI_ArrowArray())
+        array.schema = ctypes.pointer(FFI_ArrowSchema())
+        return array
+
+AllocCBType = ctypes.CFUNCTYPE(None, ctypes.POINTER(FFI_Array))
+
 
 lib_path = "../../rust/target/debug/libopendp_ffi.dylib"
 lib = ctypes.cdll.LoadLibrary(lib_path)
 
-arrow_identity = lib.opendp__arrow_identity
-arrow_identity.argtypes = [ctypes.POINTER(ArrowArray), ctypes.POINTER(ArrowSchema)]
-arrow_identity.restype = ctypes.POINTER(ArrowArraySchema)
+arrow_init = lib.opendp__arrow_init
+arrow_init.argtypes = [AllocCBType]
+arrow_init.restype = None
 
-arrow_identity_param = lib.opendp__arrow_identity_param
-arrow_identity_param.argtypes = [ctypes.POINTER(ArrowArray), ctypes.POINTER(ArrowSchema), ctypes.c_bool, ctypes.c_bool]
-arrow_identity_param.restype = ctypes.POINTER(ArrowArraySchema)
+arrow_identity = lib.opendp__arrow_identity
+arrow_identity.argtypes = [FFI_Array, FFI_Array]
+arrow_identity.restype = None
+
+arrow_identity_cb = lib.opendp__arrow_identity_cb
+arrow_identity_cb.argtypes = [FFI_Array]
+arrow_identity_cb.restype = FFI_Array
 
 arrow_sort = lib.opendp__arrow_sort
-arrow_sort.argtypes = [ctypes.POINTER(ArrowArray), ctypes.POINTER(ArrowSchema)]
-arrow_sort.restype = ctypes.POINTER(ArrowArraySchema)
+arrow_sort.argtypes = [FFI_Array, FFI_Array]
+arrow_sort.restype = None
 
 arrow_add = lib.opendp__arrow_add
-arrow_add.argtypes = [ctypes.POINTER(ArrowArray), ctypes.POINTER(ArrowSchema), ctypes.POINTER(ArrowArray), ctypes.POINTER(ArrowSchema)]
-arrow_add.restype = ctypes.POINTER(ArrowArraySchema)
+arrow_add.argtypes = [FFI_Array, FFI_Array, FFI_Array]
+arrow_add.restype = None
+
+
+arrow_consume = lib.opendp__arrow_consume
+arrow_consume.argtypes = [FFI_Array]
+arrow_consume.restype = None
+
+arrow_produce = lib.opendp__arrow_produce
+arrow_produce.argtypes = [FFI_Array]
+arrow_produce.restype = None
+
+
+
+arrow_alloc = lib.opendp__arrow_alloc
+arrow_alloc.argtypes = []
+arrow_alloc.restype = FFI_Array
 
 arrow_free = lib.opendp__arrow_free
-arrow_free.argtypes = [ctypes.POINTER(ArrowArraySchema)]
+arrow_free.argtypes = [FFI_Array]
 arrow_free.restype = None
 
+_leaks = []
 
-def to_ffi(array):
-    ffi_array = ArrowArray()
-    ffi_schema = ArrowSchema()
-    array._export_to_c(ctypes.addressof(ffi_array), ctypes.addressof(ffi_schema))
-    ffi_array.release = None
-    ffi_schema.release = None
-    return ffi_array, ffi_schema
+def alloc(ffi_array_ptr):
+    global _leaks
+    print(f"python: alloc()")
+    ffi_array = ffi_array_ptr.contents
+    arrow_array = FFI_ArrowArray()
+    arrow_schema = FFI_ArrowSchema()
+    _leaks += [arrow_array, arrow_schema]
+    ffi_array.array = ctypes.pointer(arrow_array)
+    ffi_array.schema = ctypes.pointer(arrow_schema)
+
+alloc_cb = AllocCBType(alloc)
+
+
+def init():
+    arrow_init(alloc_cb)
+
 
 def _ptr_to_int(ptr):
     vptr = ctypes.cast(ptr, ctypes.c_void_p)
     value = vptr.value
     return value if value is not None else 0
 
-def from_ffi(ffi_array_schema):
-    if not bool(ffi_array_schema):
-        return None
-    parse = True
-    if not parse:
-        return None
-    copy = True
-    ffi_array = ffi_array_schema.contents.array
-    ffi_schema = ffi_array_schema.contents.schema
-    if copy:
-        py_ffi_array = ArrowArray()
-        ctypes.pointer(py_ffi_array)[0] = ffi_array.contents
-        py_ffi_schema = ArrowSchema()
-        ctypes.pointer(py_ffi_schema)[0] = ffi_schema.contents
-        ffi_array.contents.release = None
-        ffi_schema.contents.release = None
-        arrow_free(ffi_array_schema)
-        return pa.Array._import_from_c(ctypes.addressof(py_ffi_array), ctypes.addressof(py_ffi_schema))
-    else:
-        return pa.Array._import_from_c(_ptr_to_int(ffi_array), _ptr_to_int(ffi_schema))
+
+def to_ffi(array):
+    ffi_array = arrow_alloc()
+    array._export_to_c(_ptr_to_int(ffi_array.array), _ptr_to_int(ffi_array.schema))
+    return ffi_array
+
+
+def from_ffi(ffi_array):
+    return pa.Array._import_from_c(_ptr_to_int(ffi_array.array), _ptr_to_int(ffi_array.schema))
+
 
 def do_identity():
-    create_array = True
-    call = True
-    parse = True and create_array
-    gen = False
-    if create_array:
-        arg = pa.array([1, 2, None, 4])
-        print(f"python: arg = {arg}")
+    arg = pa.array([1, 2, None, 4])
+    print(f"python: arg = {arg}")
 
-        ffi_arg_array, ffi_arg_schema = to_ffi(arg)
-        ffi_arg_array_ptr, ffi_arg_schema_ptr = ctypes.byref(ffi_arg_array), ctypes.byref(ffi_arg_schema)
-    else:
-        ffi_arg_array_ptr, ffi_arg_schema_ptr = None, None
-    if call:
-        # ffi_res = arrow_identity(ffi_arg_array_ptr, ffi_arg_schema_ptr)
-        ffi_res = arrow_identity_param(ffi_arg_array_ptr, ffi_arg_schema_ptr, parse, gen)
-    else:
-        ffi_res = None
+    ffi_arg = to_ffi(arg)
+    ffi_res = FFI_Array.new_empty()
+    arrow_identity(ffi_arg, ffi_res)
+
+    res = from_ffi(ffi_res)
+    print(f"python: res = {res}")
+    arrow_free(ffi_arg)
+    # print(f"python: ffi_arg.array = {ctypes.cast(ffi_arg.array.contents.release, ctypes.c_void_p)}")
+    # ffi_arg.array.contents.release = ctypes.cast(ctypes.c_void_p(123), ctypes.POINTER(ctypes.CFUNCTYPE(None, ctypes.POINTER(FFI_ArrowArray))))
+    # # ffi_arg.array.contents.release = None
+    # print(f"python: ffi_arg.array = {ctypes.cast(ffi_arg.array.contents.release, ctypes.c_void_p)}")
+
+
+def do_identity_cb():
+    arg = pa.array([1, 2, None, 4])
+    print(f"python: arg = {arg}")
+
+    ffi_arg = to_ffi(arg)
+    print(f"python: did to_ffi")
+    ffi_res = arrow_identity_cb(ffi_arg)
+    print(f"python: did identity_cb")
 
     res = from_ffi(ffi_res)
     print(f"python: res = {res}")
@@ -162,8 +196,9 @@ def do_sort():
     arg = pa.array([1, 2, None, 4])
     print(f"python: arg = {arg}")
 
-    ffi_arg_array, ffi_arg_schema = to_ffi(arg)
-    ffi_res = arrow_sort(ctypes.byref(ffi_arg_array), ctypes.byref(ffi_arg_schema))
+    ffi_arg = to_ffi(arg)
+    ffi_res = FFI_Array.new_empty()
+    arrow_sort(ffi_arg, ffi_res)
 
     res = from_ffi(ffi_res)
     print(f"python: res = {res}")
@@ -174,17 +209,44 @@ def do_add():
     arg1 = pa.array([1, 2, 3, None])
     print(f"python: arg0 = {arg0}, arg1 = {arg1}")
 
-    ffi_arg0_array, ffi_arg0_schema = to_ffi(arg0)
-    ffi_arg1_array, ffi_arg1_schema = to_ffi(arg1)
-    ffi_res = arrow_add(ctypes.byref(ffi_arg0_array), ctypes.byref(ffi_arg0_schema), ctypes.byref(ffi_arg1_array), ctypes.byref(ffi_arg1_schema))
+    ffi_arg0 = to_ffi(arg0)
+    ffi_arg1 = to_ffi(arg1)
+    ffi_res = FFI_Array.new_empty()
+    arrow_add(ffi_arg0, ffi_arg1, ffi_res)
 
     res = from_ffi(ffi_res)
     print(f"python: res = {res}")
 
 
+def do_consume():
+    arg = pa.array([1, 2, None, 4] * 10000)
+
+    ffi_arg = to_ffi(arg)
+    arrow_consume(ffi_arg)
+    # print(f"python: arg = {arg}")
+    # print(f"python: DONE")
+
+
+def do_produce():
+    ffi_res = FFI_Array.new_empty()
+    arrow_produce(ffi_res)
+
+    res = from_ffi(ffi_res)
+    # print(f"python: res = {res}")
+    # arrow_free(ffi_res)
+
+
 if __name__ == "__main__":
-    for _ in range(100):
-        do_identity()
-    # do_sort()
-    # do_add()
+    init()
+    for i in range(200000):
+    # for i in range(2):
+        if (i + 1) % 1000 == 0:
+            print(i + 1)
+        # do_identity()
+        # do_sort()
+        # do_add()
+        # do_consume()
+        do_produce()
     print("YEAH")
+    import time
+    time.sleep(10)
--- rust/opendp-ffi/src/arr.rs
+++ rust/opendp-ffi/src/arr.rs
@@ -1,93 +1,145 @@
 #![allow(non_camel_case_types)]
 
-use arrow::{array, compute};
-use arrow::array::{Int64Array, ArrayRef, BooleanArray};
-use arrow::ffi::{ArrowArray, FFI_ArrowArray, FFI_ArrowSchema};
+use std::ffi::c_void;
+use std::fmt::Debug;
+use std::mem::ManuallyDrop;
+use std::ptr;
 use std::sync::Arc;
 
-#[derive(Debug)]
+use arrow::{array, compute};
+use arrow::array::{ArrayRef, Int64Array};
+use arrow::ffi::{ArrowArray, FFI_ArrowArray, FFI_ArrowSchema};
+
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
-pub struct FFI_ArrowArraySchema {
+pub struct FFI_Array {
     pub array: *const FFI_ArrowArray,
     pub schema: *const FFI_ArrowSchema,
+    // pub user_data: *const c_void,
 }
 
-impl FFI_ArrowArraySchema {
-    pub fn new(ffi_array: *const FFI_ArrowArray, ffi_schema: *const FFI_ArrowSchema) -> Self {
-        Self { array: ffi_array, schema: ffi_schema }
-    }
-    pub fn new_raw(ffi_array: *const FFI_ArrowArray, ffi_schema: *const FFI_ArrowSchema) -> *const Self {
-        let res = Self::new(ffi_array, ffi_schema);
-        Box::into_raw(Box::new(res))
+impl FFI_Array {
+    pub fn new(array: *const FFI_ArrowArray, schema: *const FFI_ArrowSchema, user_data: *const c_void) -> Self {
+        Self { array, schema }
     }
 }
 
-fn from_ffi(ffi_array: *const FFI_ArrowArray, ffi_schema: *const FFI_ArrowSchema) -> ArrayRef {
-    let arr = unsafe { array::make_array_from_raw(ffi_array, ffi_schema) }.unwrap();
-    std::mem::forget(arr.clone());
-    arr
+type AllocCB = extern fn (*const FFI_Array);
+static mut ALLOC_CB: Option<AllocCB> = None;
+
+#[no_mangle]
+pub extern "C" fn opendp__arrow_init(alloc_cb: AllocCB) {
+    println!("rust: init");
+    unsafe { ALLOC_CB = Some(alloc_cb); }
+    // let _res = do_alloc_cb();
 }
 
-fn to_ffi(array: ArrayRef) -> *const FFI_ArrowArraySchema {
-    let (ffi_array, ffi_schema) = array.to_raw().unwrap();
-    FFI_ArrowArraySchema::new_raw(ffi_array, ffi_schema)
+
+// rust: before from_ffi, faa strong_count = 1
+// rust: after try_from_raw, faa strong_count = 1
+// rust: after to_data, faa strong_count = 3
+// rust: after make_array, faa strong_count = 3
+// rust: after from_ffi, faa strong_count = 2
+
+fn from_ffi(ffi_array: FFI_Array) -> ArrayRef {
+    unsafe { array::make_array_from_raw(ffi_array.array, ffi_array.schema) }.unwrap()
+}
+
+fn de_arc<T: Debug>(ptr: *const T) -> *const T {
+    let arc = unsafe { Arc::from_raw(ptr) };
+    let val = Arc::try_unwrap(arc).unwrap();
+    Box::into_raw(Box::new(val))
+}
+
+fn strong_count<T: Debug>(ptr: *const T) -> usize {
+    let arc= unsafe { Arc::from_raw(ptr) };
+    let strong_count = Arc::strong_count(&arc);
+    std::mem::forget(arc);
+    strong_count
+}
+
+// fn print_strong_count(tag: &str, ffi_arr: FFI_Array) {
+//     let strong_count = strong_count(ffi_arr.array);
+//     println!("rust: {}: strong_count = {}", tag, strong_count);
+// }
+
+fn to_ffi(array: ArrayRef, ffi_array: FFI_Array) {
+    let (temp_array, temp_schema) = array.to_raw().unwrap();
+    unsafe { ptr::copy_nonoverlapping(temp_array, ffi_array.array as *mut FFI_ArrowArray, 1); }
+    unsafe { ptr::copy_nonoverlapping(temp_schema, ffi_array.schema as *mut FFI_ArrowSchema, 1); }
+    unsafe { std::mem::forget(Arc::try_unwrap(Arc::from_raw(temp_array)).unwrap()); }
+    unsafe { std::mem::forget(Arc::try_unwrap(Arc::from_raw(temp_schema)).unwrap()); }
+}
+
+fn do_alloc_cb() -> FFI_Array {
+    println!("rust: do_alloc_cb");
+    let alloc_cb = unsafe { ALLOC_CB }.unwrap();
+    let ffi_array = FFI_Array::new(ptr::null(), ptr::null(), ptr::null());
+    alloc_cb(&ffi_array);
+    ffi_array
+}
+
+fn to_ffi_cb(array: ArrayRef) -> FFI_Array {
+    let (temp_array, temp_schema) = array.to_raw().unwrap();
+    let (temp_array, temp_schema) = (de_arc(temp_array), de_arc(temp_schema));
+    let ffi_array = do_alloc_cb();
+    unsafe { ptr::copy_nonoverlapping(temp_array, ffi_array.array as *mut FFI_ArrowArray, 1); }
+    unsafe { ptr::copy_nonoverlapping(temp_schema, ffi_array.schema as *mut FFI_ArrowSchema, 1); }
+    ffi_array
 }
 
 #[no_mangle]
-pub extern "C" fn opendp__arrow_identity(ffi_arg_array: *const FFI_ArrowArray, ffi_arg_schema: *const FFI_ArrowSchema) -> *const FFI_ArrowArraySchema {
-    let arg = from_ffi(ffi_arg_array, ffi_arg_schema);
+pub extern "C" fn opendp__arrow_identity(ffi_arg: FFI_Array, ffi_res: FFI_Array) {
+    println!("rust: identity");
+    let faa = ffi_arg.array;
+    println!("rust: before from_ffi, faa strong_count = {:?}", strong_count(faa));
+
+    let arg = from_ffi(ffi_arg);
+    println!("rust: after from_ffi, faa strong_count = {:?}", strong_count(faa));
+    // println!("rust: arg = {:?}", arg);
+
+    let res = compute::limit(&arg, arg.len());
+    println!("rust: after limit, faa strong_count = {:?}", strong_count(faa));
+    // println!("rust: res = {:?}", res);
+
+    to_ffi(res, ffi_res);
+    println!("rust: DONE");
+
+    println!("rust: after to_ffi, faa strong_count = {:?}", strong_count(faa));
+    // drop(arg);
+    // println!("rust: after drop, faa strong_count = {:?}", strong_count(faa));
+
+}
+
+#[no_mangle]
+pub extern "C" fn opendp__arrow_identity_cb(ffi_arg: FFI_Array) -> FFI_Array {
+    let arg = from_ffi(ffi_arg);
     println!("rust: arg = {:?}", arg);
 
-    // let res = compute::limit(&arg, arg.len());
-    // let res = compute::filter(&*arg, &BooleanArray::from(vec![true;arg.len()])).unwrap();
-    let res = Arc::new(arrow::array::Int64Array::from(vec![Some(1), Some(2), Some(3)]));
+    let res = compute::limit(&arg, arg.len());
     println!("rust: res = {:?}", res);
 
-    let ffi_res = to_ffi(res);
+    let ffi_res = to_ffi_cb(res);
     println!("rust: DONE");
     ffi_res
 }
 
 #[no_mangle]
-pub extern "C" fn opendp__arrow_identity_param(ffi_arg_array: *const FFI_ArrowArray, ffi_arg_schema: *const FFI_ArrowSchema, parse: bool, gen: bool) -> *const FFI_ArrowArraySchema {
-    if parse {
-        unsafe { println!("rust: array = {:?}", &*ffi_arg_array); }
-        let _arg = from_ffi(ffi_arg_array, ffi_arg_schema);
-        std::mem::forget(_arg)
-        // println!("rust: arg = {:?}", arg);
-    }
-
-    if gen {
-        let res = Arc::new(arrow::array::Int64Array::from(vec![Some(1), Some(2), Some(3)]));
-        println!("rust: res = {:?}", res);
-
-        let ffi_res = to_ffi(res);
-        println!("rust: DONE");
-        ffi_res
-    } else {
-        let ffi_res = std::ptr::null();
-        println!("rust: DONE");
-        ffi_res
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn opendp__arrow_sort(ffi_arg_array: *const FFI_ArrowArray, ffi_arg_schema: *const FFI_ArrowSchema) -> *const FFI_ArrowArraySchema {
-    let arg = from_ffi(ffi_arg_array, ffi_arg_schema);
+pub extern "C" fn opendp__arrow_sort(ffi_arg: FFI_Array, ffi_res: FFI_Array) {
+    let arg = from_ffi(ffi_arg);
     println!("rust: arg = {:?}", arg);
 
     let res = compute::sort(&arg, None).unwrap();
     println!("rust: res = {:?}", res);
 
-    let ffi_res = to_ffi(res);
+    to_ffi(res, ffi_res);
     println!("rust: DONE");
-    ffi_res
 }
 
 #[no_mangle]
-pub extern "C" fn opendp__arrow_add(ffi_arg0_array: *const FFI_ArrowArray, ffi_arg0_schema: *const FFI_ArrowSchema, ffi_arg1_array: *const FFI_ArrowArray, ffi_arg1_schema: *const FFI_ArrowSchema) -> *const FFI_ArrowArraySchema {
-    let arg0 = from_ffi(ffi_arg0_array, ffi_arg0_schema);
-    let arg1 = from_ffi(ffi_arg1_array, ffi_arg1_schema);
+pub extern "C" fn opendp__arrow_add(ffi_arg0: FFI_Array, ffi_arg1: FFI_Array, ffi_res: FFI_Array) {
+    let arg0 = from_ffi(ffi_arg0);
+    let arg1 = from_ffi(ffi_arg1);
     println!("rust: arg0 = {:?}, arg1 = {:?}", arg0, arg1);
 
     let arg0 = arg0.as_any().downcast_ref::<Int64Array>().unwrap();
@@ -96,15 +148,40 @@ pub extern "C" fn opendp__arrow_add(ffi_arg0_array: *const FFI_ArrowArray, ffi_a
     let res = Arc::new(res);
     println!("rust, res = {:?}", res);
 
-    let ffi_res = to_ffi(res);
+    to_ffi(res, ffi_res);
     println!("rust: DONE");
-    ffi_res
 }
 
 
 #[no_mangle]
-pub extern "C" fn opendp__arrow_free(ffi_arg: *mut FFI_ArrowArraySchema) {
-    let ffi_arr_sch = unsafe { Box::from_raw(ffi_arg) };
-    let _drop = unsafe { ArrowArray::try_from_raw(ffi_arr_sch.array, ffi_arr_sch.schema) };
-    println!("rust: DONE");
+pub extern "C" fn opendp__arrow_consume(ffi_arg: FFI_Array) {
+    let _arg = from_ffi(ffi_arg);
+    // println!("rust: DONE");
+}
+
+#[no_mangle]
+pub extern "C" fn opendp__arrow_produce(ffi_res: FFI_Array) {
+    // println!("rust: produce");
+    let res = Int64Array::from(vec![Some(999);10000]);
+    let res = Arc::new(res);
+    to_ffi(res, ffi_res);
+    // println!("rust: did to_ffi");
+    // println!("rust: DONE");
+}
+
+
+#[no_mangle]
+pub extern "C" fn opendp__arrow_alloc() -> FFI_Array {
+    let arrow_array = unsafe { ArrowArray::empty() };
+    let (ffi_array, ffi_schema) = ArrowArray::into_raw(arrow_array);
+    FFI_Array::new(ffi_array, ffi_schema, ptr::null())
+}
+
+#[no_mangle]
+pub extern "C" fn opendp__arrow_free(ffi_arg: FFI_Array) {
+    println!("rust: free");
+    // let arrow_array = unsafe { ArrowArray::try_from_raw(ffi_arg.array, ffi_arg.schema) }.unwrap();
+    unsafe { Box::from_raw(ffi_arg.array as *mut FFI_ArrowArray); }
+    unsafe { Box::from_raw(ffi_arg.schema as *mut FFI_ArrowSchema); }
+    println!("rust: free: DONE");
 }
