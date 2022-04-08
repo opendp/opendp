9a3d4aab02bfcf9e1546093f3691e80c39a296b5
Shoeboxam <shoeboxam@gmail.com>
2022-04-07 21:26:42 -0600

    fix merged test

--- python/src/opendp/mod.py
+++ python/src/opendp/mod.py
@@ -470,7 +470,7 @@ def binary_search(
     >>> binary_search(
     ...     lambda d_out: dp_mean.check(3, (d_out, 1e-8)), 
     ...     bounds = (0., 1.))
-    0.6105625927448273
+    0.6105625903337409
 
     Find the L2 distance sensitivity of a histogram when neighboring datasets differ by up to 3 additions/removals.
 
