8c437e477b2e195d58e8cdc53b752c09d2ca27f7
Shoeboxam <shoeboxam@gmail.com>
2022-04-10 10:33:27 -0600

    debug make_b_ary_tree

--- rust/src/transformations/b_ary_tree/mod.rs
+++ rust/src/transformations/b_ary_tree/mod.rs
@@ -1,44 +1,44 @@
-use std::{iter::Sum, ops::{MulAssign, AddAssign}};
+use std::{
+    iter::Sum,
+    ops::{AddAssign, MulAssign},
+};
 
-use num::{Zero, Float};
+use num::{Float, Zero};
 
 use crate::{
     core::{Function, Metric, StabilityMap, Transformation},
     metrics::{AgnosticMetric, LpDistance},
     domains::{AllDomain, VectorDomain},
-    error::{ExplainUnwrap, Fallible},
+    error::Fallible,
     traits::{CheckNull, DistanceConstant, InfCast, RoundCast},
 };
 
-// find i such that b^i >= x
-fn log_b_ceil(x: usize, b: usize) -> usize {
-    if x == 1 {
-        return 0;
-    }
+use super::postprocess::make_postprocess;
 
-    let mut checker = b;
-    (0usize..)
-        .find(|_| {
-            checker *= b;
-            checker >= x
-        })
-        .unwrap_assert("loop runs until value is found")
+// find i such that b^i >= x
+// returns 0 when x == 0
+fn log_b_ceil(x: usize, b: usize) -> usize {
+    let mut checker = 1;
+    let mut pow = 0;
+    loop {
+        if checker >= x {
+            return pow;
+        }
+        checker *= b;
+        pow += 1;
+    }
 }
 
-fn height_from_leaf_count(num_leaves: usize, b: usize) -> usize {
+fn num_layers_from_num_leaves(num_leaves: usize, b: usize) -> usize {
     log_b_ceil(num_leaves, b) + 1
 }
 
-fn layers_from_num_nodes(num_nodes: usize, b: usize) -> usize {
+fn num_layers_from_num_nodes(num_nodes: usize, b: usize) -> usize {
     log_b_ceil((b - 1) * num_nodes + 1, b)
 }
 
-// height = num_layers - 1
-fn num_nodes_from_height(height: usize, b: usize) -> Fallible<usize> {
-    // num_nodes = b^height / (b - 1)
-    b.checked_pow(height as u32)
-        .map(|n| n / (b - 1))
-        .ok_or_else(|| err!(FailedFunction, "tree is too large to be indexed (highly unlikely)"))
+fn num_nodes_from_num_layers(num_layers: usize, b: usize) -> usize {
+    (b.pow(num_layers as u32) - 1) / (b - 1)
 }
 
 pub trait BAryTreeMetric: Metric {}
@@ -50,7 +50,7 @@ pub fn make_b_ary_tree<T, M>(
     b: usize,
 ) -> Fallible<Transformation<VectorDomain<AllDomain<T>>, VectorDomain<AllDomain<T>>, M, M>>
 where
-    T: CheckNull + Zero + Clone + for<'a> Sum<&'a T>,
+    T: CheckNull + Zero + Clone + for<'a> Sum<&'a T> + std::fmt::Debug,
     M: BAryTreeMetric,
     M::Distance: DistanceConstant<M::Distance> + InfCast<usize>,
 {
@@ -61,8 +61,8 @@ where
         return fallible!(MakeTransformation, "branching factor must be at least two");
     }
 
-    let new_height = height_from_leaf_count(num_bins, b);
-    let num_leaves = b.pow(new_height as u32);
+    let num_layers = num_layers_from_num_leaves(num_bins, b);
+    let num_leaves = b.pow(num_layers as u32 - 1);
 
     Ok(Transformation::new(
         VectorDomain::new_all(),
@@ -80,20 +80,25 @@ where
                 .chain((0..pad_length).map(|_| T::zero()))
                 .collect::<Vec<T>>()];
 
-            (0..new_height + 1).for_each(|i| {
+            (0..num_layers - 1).for_each(|i| {
                 layers.push(
                     layers[i]
                         .chunks(b)
                         .map(|chunk| chunk.iter().sum::<T>())
                         .collect(),
-                )
+                );
             });
-
-            layers.into_iter().rev().flatten().collect()
+            let tree_length = num_nodes_from_num_layers(num_layers, b) - pad_length;
+            layers
+                .into_iter()
+                .rev()
+                .flatten()
+                .take(tree_length)
+                .collect()
         }),
         M::default(),
         M::default(),
-        StabilityMap::new_from_constant(M::Distance::inf_cast(new_height + 1)?),
+        StabilityMap::new_from_constant(M::Distance::inf_cast(num_layers)?),
     ))
 }
 
@@ -109,15 +114,15 @@ pub fn make_b_ary_tree_consistent<TI, TO>(
 >
 where
     TI: CheckNull + Clone,
-    TO: CheckNull + Float + RoundCast<TI> + for <'a> Sum<&'a TO> + MulAssign + AddAssign,
+    TO: CheckNull + Float + RoundCast<TI> + for<'a> Sum<&'a TO> + MulAssign + AddAssign,
 {
-    Ok(Transformation::new(
+    make_postprocess(
         VectorDomain::new_all(),
         VectorDomain::new_all(),
         Function::new_fallible(move |arg: &Vec<TI>| {
-            let layers = layers_from_num_nodes(arg.len(), b);
+            let layers = num_layers_from_num_nodes(arg.len(), b);
 
-            let mut vars = vec![TO::one(); num_nodes_from_height(layers - 1, b)?];
+            let mut vars = vec![TO::one(); num_nodes_from_num_layers(layers, b)];
             let zero_leaves = vars.len() - arg.len();
             let mut tree: Vec<TO> = arg
                 .iter()
@@ -127,20 +132,17 @@ where
                 .collect::<Fallible<_>>()?;
 
             // zero out all zero variance zero nodes on the tree
-            (0..layers).try_for_each(|l| {
+            (0..layers).for_each(|l| {
                 // number of zeros in layer l
                 let l_zeros = zero_leaves / b.pow((layers - l - 1) as u32);
-                let l_end = num_nodes_from_height(l, b)?;
+                let l_end = num_nodes_from_num_layers(l + 1, b);
                 vars[l_end - l_zeros..l_end].fill(TO::zero());
                 tree[l_end - l_zeros..l_end].fill(TO::zero());
-                Fallible::Ok(())
-            })?;
+            });
 
             // bottom-up scan to compute z
-            (0..layers - 1)
-            .rev()
-            .try_for_each(|l| {
-                let l_start = num_nodes_from_height(l - 1, b)?;
+            (0..layers - 1).rev().for_each(|l| {
+                let l_start = num_nodes_from_num_layers(l, b);
                 (0..b.pow(l as u32)).for_each(|offset| {
                     let i = l_start + offset;
                     if vars[i].is_zero() {
@@ -165,13 +167,12 @@ where
                     // postprocess by weighted inverse variance
                     tree[i] = alpha * tree[i] + (TO::one() - alpha) * child_val;
                 });
-                Fallible::Ok(())
-            })?;
+            });
 
             // top down scan to compute h
             let mut h_b = tree.clone();
-            (0..layers - 1).try_for_each(|l| {
-                let l_start = num_nodes_from_height(l - 1, b)?;
+            (0..layers - 1).for_each(|l| {
+                let l_start = num_nodes_from_num_layers(l, b);
 
                 (0..b.pow(l as u32)).for_each(|offset| {
                     let i = l_start + offset;
@@ -191,20 +192,120 @@ where
                         .zip(child_vars)
                         .for_each(|(v, child_var)| *v += correction * child_var / sum_var);
                 });
-                Fallible::Ok(())
-            })?;
+            });
 
             // entire tree is consistent, so only the nonzero leaves in bottom layer are needed
-            let leaf_start = num_nodes_from_height(layers - 2, b)?;
-            let leaf_end = num_nodes_from_height(layers - 1, b)? - zero_leaves;
+            let leaf_start = num_nodes_from_num_layers(layers - 1, b);
+            let leaf_end = num_nodes_from_num_layers(layers, b) - zero_leaves;
             Ok(h_b[leaf_start..leaf_end].to_vec())
-        }),
-        AgnosticMetric::default(),
-        AgnosticMetric::default(),
-        StabilityRelation::new_all(
-            |_d_in: &(), _d_out: &()| Ok(true),
-            None::<fn(&_) -> _>,
-            None::<fn(&_) -> _>,
-        ),
-    ))
+        }))
+}
+
+#[cfg(test)]
+pub mod test_b_trees {
+    use crate::{metrics::L1Distance, meas::make_base_geometric};
+
+    use super::*;
+
+    fn log_b_ceil_float(x: usize, b: usize) -> usize {
+        // naive implementation susceptible to float approximation errors
+        // for example, log_5(125) == 3, but evaluates to 3.0000000000000004 -> 4
+        (x as f64).log(b as f64).ceil() as usize
+    }
+
+    #[test]
+    fn test_log_b_ceil() {
+        // tests pass for small values,
+        //    but large values fail because log_b_ceil_float is unstable
+        (1..100).for_each(move |x| {
+            (2..100).for_each(|b| {
+                assert_eq!(
+                    log_b_ceil(x, b),
+                    log_b_ceil_float(x, b),
+                    "log_{:?}({:?})",
+                    b,
+                    x
+                )
+            })
+        });
+    }
+
+    #[test]
+    fn test_num_layers_from_num_leaves() {
+        assert_eq!(num_layers_from_num_leaves(10, 2), 5);
+        assert_eq!(num_layers_from_num_leaves(16, 2), 5);
+        assert_eq!(num_layers_from_num_leaves(8, 2), 4);
+        assert_eq!(num_layers_from_num_leaves(6, 2), 4);
+        assert_eq!(num_layers_from_num_leaves(3, 2), 3);
+    }
+
+    #[test]
+    fn test_layers_from_num_nodes() {
+        assert_eq!(num_layers_from_num_nodes(1, 2), 1);
+        assert_eq!(num_layers_from_num_nodes(2, 2), 2);
+        assert_eq!(num_layers_from_num_nodes(3, 2), 2);
+        assert_eq!(num_layers_from_num_nodes(7, 2), 3);
+        assert_eq!(num_layers_from_num_nodes(8, 2), 4);
+
+        assert_eq!(num_layers_from_num_nodes(2, 3), 2);
+        assert_eq!(num_layers_from_num_nodes(4, 3), 2);
+        assert_eq!(num_layers_from_num_nodes(5, 4), 2);
+        assert_eq!(num_layers_from_num_nodes(13, 3), 3);
+        assert_eq!(num_layers_from_num_nodes(14, 3), 4);
+    }
+
+    #[test]
+    fn test_num_nodes_from_num_layers() {
+        assert_eq!(num_nodes_from_num_layers(1, 2), 1);
+        assert_eq!(num_nodes_from_num_layers(2, 2), 3);
+        assert_eq!(num_nodes_from_num_layers(3, 2), 7);
+        assert_eq!(num_nodes_from_num_layers(4, 2), 15);
+
+        assert_eq!(num_nodes_from_num_layers(1, 3), 1);
+        assert_eq!(num_nodes_from_num_layers(2, 3), 4);
+        assert_eq!(num_nodes_from_num_layers(3, 3), 13);
+        assert_eq!(num_nodes_from_num_layers(4, 3), 40);
+    }
+
+    #[test]
+    fn test_make_b_ary_tree() -> Fallible<()> {
+        let trans = make_b_ary_tree::<i32, L1Distance<i32>>(10, 2)?;
+        let actual = trans.invoke(&vec![1; 10])?;
+        let expect = vec![
+            vec![10],
+            vec![8, 2],
+            vec![4, 4, 2, 0],
+            vec![2, 2, 2, 2, 2, 0, 0, 0],
+            vec![1; 10],
+        ]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<i32>>();
+        assert_eq!(expect, actual);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_noise_b_ary_tree() -> Fallible<()> {
+        let meas =
+            (make_b_ary_tree::<i32, L1Distance<i32>>(10, 2)? >> make_base_geometric(1., None)?)?;
+        println!("noised {:?}", meas.invoke(&vec![1; 10])?);
+
+        Ok(())
+    }
+
+    // #[test]
+    // fn test_identity() -> Fallible<()> {
+    //     let b = 2;
+    //     let meas = (make_b_ary_tree::<i32, L1Distance<i32>>(10, b)?
+    //         >> make_base_geometric(1., None)?)?;
+        
+    //     let post = make_b_ary_tree_consistent::<i32, f64>(b)?;
+
+    //     meas >> post;
+    //     println!("consistent {:?}", meas.invoke(&vec![1; 10])?);
+
+    //     Ok(())
+    // }
 }
