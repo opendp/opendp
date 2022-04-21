9ba6442265bf8c9c25afa8551ffcbe2ab3606d67
Shoeboxam <shoeboxam@gmail.com>
2022-04-21 14:33:01 -0400

    ffi and traits submodule

--- rust/src/meas/discrete_laplace/mod.rs
+++ rust/src/meas/discrete_laplace/mod.rs
@@ -1,123 +1,18 @@
 #[cfg(feature = "ffi")]
 mod ffi;
 
-use std::ops::Mul;
+mod traits;
+pub use traits::*;
 
-use num::{Float, Integer};
+use num::Float;
 
-use crate::core::{Measurement, Metric, SensitivityMetric};
-use crate::dist::{AbsoluteDistance, L1Distance, MaxDivergence};
-use crate::dom::{AllDomain, VectorDomain};
+use crate::core::Measurement;
+use crate::dist::{AbsoluteDistance, MaxDivergence};
 use crate::error::*;
 use crate::meas::make_base_geometric;
-use crate::samplers::SampleTwoSidedGeometric;
-use crate::traits::{
-    AlertingAbs, CheckNull, DistanceConstant, InfAdd, InfCast, RoundCast, TotalOrd,
-};
-use crate::trans::{
-    make_lipschitz_cast, make_lipschitz_mul, GreatestDifference, LipschitzCastDomain,
-    LipschitzMulDomain, LipschitzMulMetric, SameMetric,
-};
+use crate::traits::{InfAdd, InfCast, RoundCast, TotalOrd};
+use crate::trans::{make_lipschitz_cast, make_lipschitz_mul, GreatestDifference, SameMetric};
 
-use super::GeometricDomain;
-
-// Helper trait to obscure trait bounds on Atom
-pub trait BoundedLipschitzMulDomain<I>: LipschitzMulDomain<Atom = Self::BoundedAtom>
-where
-    I: InfCast<Self::BoundedAtom>,
-{
-    type BoundedAtom: Float
-        + AlertingAbs
-        + DistanceConstant<Self::Atom>
-        + DistanceConstant<I>
-        + DefaultGranularity
-        + GreatestDifference<Self::Atom>
-        + InfAdd;
-}
-impl<T, I> BoundedLipschitzMulDomain<I> for T
-where
-    T: LipschitzMulDomain,
-    I: InfCast<Self::Atom>,
-    Self::Atom: Float
-        + AlertingAbs
-        + DistanceConstant<Self::Atom>
-        + DistanceConstant<I>
-        + DefaultGranularity
-        + GreatestDifference<Self::Atom>
-        + InfAdd,
-{
-    type BoundedAtom = Self::Atom;
-}
-
-pub trait DiscreteLaplaceDomain<I>:
-    'static + BoundedLipschitzMulDomain<I> + LipschitzCastDomain<Self::IntegerDomain> + Default
-where
-    I: InfCast<Self::BoundedAtom>,
-{
-    type Metric: SensitivityMetric<Distance = Self::Atom> + Default + LipschitzMulMetric;
-    type IntegerMetric: Metric<Distance = I>;
-    type IntegerDomain: GeometricDomain<Self::Atom, Atom = I, InputMetric = Self::IntegerMetric>
-        + LipschitzCastDomain<Self>
-        + Default;
-}
-
-impl<T, I> DiscreteLaplaceDomain<I> for AllDomain<T>
-where
-    T: 'static + Float + CheckNull + for<'a> Mul<&'a T, Output = T> + RoundCast<I>
-    + AlertingAbs
-    + DistanceConstant<Self::Atom>
-    + DistanceConstant<I>
-    + DefaultGranularity
-    + GreatestDifference<Self::Atom>
-    + InfAdd,
-    I: 'static
-        + RoundCast<T>
-        + CheckNull
-        + Integer
-        + Clone
-        + SampleTwoSidedGeometric<T>
-        + InfCast<T>,
-{
-    type Metric = AbsoluteDistance<T>;
-    type IntegerMetric = AbsoluteDistance<I>;
-    type IntegerDomain = AllDomain<I>;
-}
-
-impl<T, I> DiscreteLaplaceDomain<I> for VectorDomain<AllDomain<T>>
-where
-    T: 'static + Float + CheckNull + for<'a> Mul<&'a T, Output = T> + RoundCast<I>
-    + AlertingAbs
-    + DistanceConstant<Self::Atom>
-    + DistanceConstant<I>
-    + DefaultGranularity
-    + GreatestDifference<Self::Atom>
-    + InfAdd,
-    I: 'static
-        + RoundCast<T>
-        + CheckNull
-        + Integer
-        + Clone
-        + SampleTwoSidedGeometric<T>
-        + InfCast<T>,
-{
-    type Metric = L1Distance<T>;
-    type IntegerMetric = L1Distance<I>;
-    type IntegerDomain = VectorDomain<AllDomain<I>>;
-}
-
-pub trait DefaultGranularity {
-    const GRAN: Self;
-}
-macro_rules! impl_granularity {
-    ($ty:ty) => {
-        impl DefaultGranularity for $ty {
-            // 2^{-14}
-            const GRAN: Self = 0.00006103515625;
-        }
-    };
-}
-impl_granularity!(f32);
-impl_granularity!(f64);
 
 pub fn make_base_discrete_laplace<D, I>(
     scale: D::Atom,
@@ -129,7 +24,6 @@ where
     I: 'static
         + InfCast<D::Atom>
         + RoundCast<D::Atom>
-        + Integer
         + Clone
         + TotalOrd
         + GreatestDifference<D::Atom>
