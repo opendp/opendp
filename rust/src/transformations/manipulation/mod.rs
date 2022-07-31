7c92112594a08c38590186a88eb35b642f9c05d3
Shoeboxam <shoeboxam@gmail.com>
2022-07-31 10:05:08 -0400

    generalize make_row_by_row

--- rust/src/transformations/manipulation/mod.rs
+++ rust/src/transformations/manipulation/mod.rs
@@ -1,65 +1,137 @@
-#[cfg(feature="ffi")]
+#[cfg(feature = "ffi")]
 mod ffi;
 
+use ndarray::Array2;
 use num::One;
 use opendp_derive::bootstrap;
 
+
+use crate::metrics::{SymmetricDistance, IntDistance};
+use crate::domains::{AllDomain, VectorDomain, Array2Domain};
 use crate::core::{Domain, Function, Metric, StabilityMap, Transformation};
 use crate::error::*;
-use crate::traits::{DistanceConstant, CheckNull};
-use crate::domains::{VectorDomain, AllDomain};
-use crate::metrics::{SymmetricDistance, IntDistance};
+use crate::traits::{CheckNull, DistanceConstant};
+use std::convert::TryFrom;
 
+pub trait DatasetDomain: Domain {
+    type RowDomain: Domain;
+    type Row;
+    fn new(row_domain: Self::RowDomain) -> Self;
+}
 
-/// Constructs a [`Transformation`] representing an arbitrary row-by-row transformation.
-pub(crate) fn make_row_by_row<DIA, DOA, M>(
-    atom_input_domain: DIA,
-    atom_output_domain: DOA,
-    atom_function: impl 'static + Fn(&DIA::Carrier) -> DOA::Carrier
-) -> Fallible<Transformation<VectorDomain<DIA>, VectorDomain<DOA>, M, M>>
-    where DIA: Domain, DOA: Domain,
-          DIA::Carrier: 'static,
-          M: Metric<Distance=IntDistance> {
-    Ok(Transformation::new(
-        VectorDomain::new(atom_input_domain),
-        VectorDomain::new(atom_output_domain),
-        Function::new(move |arg: &Vec<DIA::Carrier>|
-            arg.iter().map(&atom_function).collect()),
-        M::default(),
-        M::default(),
-        StabilityMap::new_from_constant(1)))
+impl<D: Domain> DatasetDomain for VectorDomain<D> {
+    type RowDomain = D;
+    type Row = D::Carrier;
+    fn new(row_domain: Self::RowDomain) -> Self {
+        VectorDomain::new(row_domain)
+    }
+}
+
+impl<D: Domain> DatasetDomain for Array2Domain<D> {
+    type RowDomain = D;
+    type Row = Vec<D::Carrier>;
+    fn new(row_domain: Self::RowDomain) -> Self {
+        Array2Domain::new(row_domain)
+    }
+}
+
+pub trait RowByRowDomain<DO: DatasetDomain>: DatasetDomain {
+    fn apply_rows(
+        value: &Self::Carrier,
+        row_function: &impl Fn(&Self::Row) -> Fallible<DO::Row>,
+    ) -> Fallible<DO::Carrier>;
+}
+
+impl<DIA: Domain, DOA: Domain> RowByRowDomain<VectorDomain<DOA>> for VectorDomain<DIA> {
+    fn apply_rows(
+        value: &Self::Carrier,
+        row_function: &impl Fn(&Self::Row) -> Fallible<DOA::Carrier>,
+    ) -> Fallible<Vec<DOA::Carrier>> {
+        value.iter().map(row_function).collect()
+    }
+}
+
+impl<DIA: Domain, DOA: Domain> RowByRowDomain<Array2Domain<DOA>> for Array2Domain<DIA>
+where
+    DIA::Carrier: Clone,
+{
+    fn apply_rows(
+        value: &Self::Carrier,
+        row_function: &impl Fn(&Self::Row) -> Fallible<Vec<DOA::Carrier>>,
+    ) -> Fallible<Array2<DOA::Carrier>> {
+        let shape =
+            <[usize; 2]>::try_from(value.shape()).unwrap_assert("input is always of shape 2");
+
+        let data = (value.rows())
+            .into_iter()
+            .map(|row| row_function(&row.to_vec()))
+            .try_fold(vec![], |mut acc, v| {
+                acc.extend(v?);
+                Fallible::Ok(acc)
+            })?;
+
+        Array2::from_shape_vec(shape, data)
+            .map_err(|_| err!(FailedFunction, "func must preserve the same number of rows"))
+    }
 }
 
 /// Constructs a [`Transformation`] representing an arbitrary row-by-row transformation.
-pub(crate) fn make_row_by_row_fallible<DIA, DOA, M>(
-    atom_input_domain: DIA,
-    atom_output_domain: DOA,
-    atom_function: impl 'static + Fn(&DIA::Carrier) -> Fallible<DOA::Carrier>
-) -> Fallible<Transformation<VectorDomain<DIA>, VectorDomain<DOA>, M, M>>
-    where DIA: Domain, DOA: Domain,
-          DIA::Carrier: 'static,
-          M: Metric<Distance=IntDistance> {
+pub(crate) fn make_row_by_row<DI, DO, M>(
+    input_row_domain: DI::RowDomain,
+    output_row_domain: DO::RowDomain,
+    row_function: impl 'static + Fn(&DI::Row) -> DO::Row,
+) -> Fallible<Transformation<DI, DO, M, M>>
+where
+    DI: RowByRowDomain<DO>,
+    DO: DatasetDomain,
+    M: Metric<Distance=IntDistance>
+{
+    let row_function = move |arg: &DI::Row| Ok(row_function(arg));
     Ok(Transformation::new(
-        VectorDomain::new(atom_input_domain),
-        VectorDomain::new(atom_output_domain),
-        Function::new_fallible(move |arg: &Vec<DIA::Carrier>|
-            arg.iter().map(&atom_function).collect()),
+        DI::new(input_row_domain),
+        DO::new(output_row_domain),
+        Function::new_fallible(move |arg: &DI::Carrier| DI::apply_rows(arg, &row_function)),
         M::default(),
         M::default(),
-        StabilityMap::new_from_constant(1)))
+        StabilityMap::new_from_constant(1),
+    ))
+}
+
+/// Constructs a [`Transformation`] representing an arbitrary row-by-row transformation.
+pub(crate) fn make_row_by_row_fallible<DI, DO, M>(
+    input_row_domain: DI::RowDomain,
+    output_row_domain: DO::RowDomain,
+    row_function: impl 'static + Fn(&DI::Row) -> Fallible<DO::Row>,
+) -> Fallible<Transformation<DI, DO, M, M>>
+where
+    DI: RowByRowDomain<DO>,
+    DO: DatasetDomain,
+    M: Metric<Distance=IntDistance> {
+    Ok(Transformation::new(
+        DI::new(input_row_domain),
+        DO::new(output_row_domain),
+        Function::new_fallible(move |arg: &DI::Carrier| DI::apply_rows(arg, &row_function)),
+        M::default(),
+        M::default(),
+        StabilityMap::new_from_constant(1),
+    ))
 }
 
 /// Constructs a [`Transformation`] representing the identity function.
-pub fn make_identity<D, M>(domain: D, metric: M) -> Fallible<Transformation<D, D, M, M>>
-    where D: Domain, D::Carrier: Clone,
-          M: Metric, M::Distance: DistanceConstant<M::Distance> + One + Clone {
+pub fn make_identity<D, M>(domain: D, metric: M) -> Fallible<Transformation<D, D, M, M>> where
+    D: Domain,
+    D::Carrier: Clone,
+    M: Metric,
+    M::Distance: DistanceConstant<M::Distance> + One + Clone,
+{
     Ok(Transformation::new(
         domain.clone(),
         domain,
         Function::new(|arg: &D::Carrier| arg.clone()),
         metric.clone(),
         metric,
-        StabilityMap::new_from_constant(M::Distance::one())))
+        StabilityMap::new_from_constant(M::Distance::one()),
+    ))
 }
 
 #[bootstrap(features("contrib"))]
@@ -71,13 +143,19 @@ pub fn make_identity<D, M>(domain: D, metric: M) -> Fallible<Transformation<D, D
 /// # Generics
 /// * `TIA` - Atomic Input Type. Type of elements in the input vector
 pub fn make_is_equal<TIA>(
-    value: TIA
-) -> Fallible<Transformation<VectorDomain<AllDomain<TIA>>, VectorDomain<AllDomain<bool>>, SymmetricDistance, SymmetricDistance>>
-    where TIA: 'static + PartialEq + CheckNull {
-    make_row_by_row(
-        AllDomain::new(),
-        AllDomain::new(),
-        move |v| v == &value)
+    value: TIA,
+) -> Fallible<
+    Transformation<
+        VectorDomain<AllDomain<TIA>>,
+        VectorDomain<AllDomain<bool>>,
+        SymmetricDistance,
+        SymmetricDistance,
+    >,
+>
+where
+    TIA: 'static + PartialEq + CheckNull,
+{
+    make_row_by_row(AllDomain::new(), AllDomain::new(), move |v| v == &value)
 }
 
 #[bootstrap(features("contrib"))]
@@ -85,15 +163,24 @@ pub fn make_is_equal<TIA>(
 /// 
 /// # Generics
 /// * `DIA` - Atomic Input Domain. Can be any domain for which the carrier type has a notion of nullity.
-pub fn make_is_null<DIA>() -> Fallible<Transformation<VectorDomain<DIA>, VectorDomain<AllDomain<bool>>, SymmetricDistance, SymmetricDistance>>
-    where DIA: Domain + Default,
-          DIA::Carrier: 'static + CheckNull {
+pub fn make_is_null<DIA>() -> Fallible<
+    Transformation<
+        VectorDomain<DIA>,
+        VectorDomain<AllDomain<bool>>,
+        SymmetricDistance,
+        SymmetricDistance,
+    >,
+>
+where
+    DIA: Domain + Default,
+    DIA::Carrier: 'static + CheckNull,
+{
     make_row_by_row(
         DIA::default(),
         AllDomain::default(),
-        |v| v.is_null())
+        |v: &DIA::Carrier| v.is_null()
+    )
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -101,6 +188,7 @@ mod tests {
     use super::*;
     use crate::metrics::ChangeOneDistance;
     use crate::domains::{AllDomain, InherentNullDomain};
+
 
     #[test]
     fn test_identity() {
@@ -129,4 +217,32 @@ mod tests {
         assert!(is_equal.check(&1, &1)?);
         Ok(())
     }
+}
+
+
+fn make_bin_grid_array2(
+    lower_edges: Vec<f64>,
+    upper_edges: Vec<f64>,
+    bin_count: usize,
+) -> Fallible<
+    Transformation<
+        Array2Domain<AllDomain<f64>>,
+        Array2Domain<AllDomain<usize>>,
+        SymmetricDistance,
+        SymmetricDistance,
+    >,
+> {
+    use crate::traits::RoundCast;
+    
+    let bin_count = bin_count as f64;
+    make_row_by_row_fallible(
+        AllDomain::new(),
+        AllDomain::new(),
+        move |row: &Vec<f64>| {
+            row.iter()
+                .zip(lower_edges.iter().zip(upper_edges.iter()))
+                .map(|(v, (u, l))| usize::round_cast(((v - l) / (u - l) * bin_count).floor()))
+                .collect()
+        },
+    )
 }
