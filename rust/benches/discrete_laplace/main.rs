7b6c64a4470403a4c2a0f44f65aee18a6c2f9c1d
Shoeboxam <shoeboxam@gmail.com>
2022-08-01 11:26:04 -0400

    tune cutoff on scale parameter via benchmarking

---
+++ rust/benches/discrete_laplace/main.rs
@@ -0,0 +1,31 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use opendp::{
+    domains::{AllDomain, VectorDomain},
+    meas::{make_base_discrete_laplace_cks20, make_base_discrete_laplace_linear},
+};
+
+pub fn collect(c: &mut Criterion) {
+
+    (1..20).for_each(|v| {
+        let scale = v as f64;
+        c.bench_function(format!("{} linear", scale).as_str(), |b| {
+            b.iter(|| {
+                let meas =
+                    make_base_discrete_laplace_linear::<VectorDomain<AllDomain<i32>>, _>(scale, None)
+                        .unwrap();
+                meas.invoke(&vec![0; 1000]).unwrap();
+            })
+        });
+
+        c.bench_function(format!("{} cks20", scale).as_str(), |b| {
+            b.iter(|| {
+                let meas =
+                    make_base_discrete_laplace_cks20::<VectorDomain<AllDomain<i32>>, _>(scale).unwrap();
+                meas.invoke(&vec![0; 1000]).unwrap();
+            })
+        });
+    });
+}
+
+criterion_group!(benches, collect);
+criterion_main!(benches);
--- rust/src/meas/discrete_laplace/mod.rs
+++ rust/src/meas/discrete_laplace/mod.rs
@@ -1,22 +1,22 @@
 use crate::{
-    core::{Domain, Measurement, SensitivityMetric, Function},
+    core::{Domain, Function, Measurement, SensitivityMetric},
     domains::{AllDomain, VectorDomain},
     error::Fallible,
     measures::MaxDivergence,
     metrics::{AbsoluteDistance, L1Distance},
+    traits::samplers::SampleDiscreteLaplaceLinear,
     traits::{CheckNull, Float, InfCast, Integer},
-    traits::samplers::SampleDiscreteLaplaceLinear
 };
 
-#[cfg(feature="use-mpfr")]
+#[cfg(feature = "use-mpfr")]
 use az::SaturatingCast;
 
-#[cfg(feature="ffi")]
+#[cfg(feature = "ffi")]
 mod ffi;
 
-#[cfg(feature="use-mpfr")]
+#[cfg(feature = "use-mpfr")]
 mod cks20;
-#[cfg(feature="use-mpfr")]
+#[cfg(feature = "use-mpfr")]
 pub use cks20::*;
 
 mod linear;
@@ -29,7 +29,9 @@ pub trait MappableDomain: Domain {
         func: &impl Fn(&Self::Atom) -> Fallible<Self::Atom>,
     ) -> Fallible<Self::Carrier>;
 
-    fn new_map_function(func: impl Fn(&Self::Atom) -> Fallible<Self::Atom> + 'static) -> Function<Self, Self> {
+    fn new_map_function(
+        func: impl Fn(&Self::Atom) -> Fallible<Self::Atom> + 'static,
+    ) -> Function<Self, Self> {
         Function::new_fallible(move |arg: &Self::Carrier| Self::map_over(arg, &func))
     }
 }
@@ -63,7 +65,7 @@ impl<T: CheckNull> DiscreteLaplaceDomain for VectorDomain<AllDomain<T>> {
     type InputMetric = L1Distance<T>;
 }
 
-#[cfg(feature="use-mpfr")]
+#[cfg(feature = "use-mpfr")]
 pub fn make_base_discrete_laplace<D, QO>(
     scale: QO,
 ) -> Fallible<Measurement<D, D, D::InputMetric, MaxDivergence<QO>>>
@@ -74,14 +76,40 @@ where
     rug::Rational: std::convert::TryFrom<QO>,
     rug::Integer: From<D::Atom> + SaturatingCast<D::Atom>,
 {
-    if scale > QO::exact_int_cast(3)? {
+    // benchmarking results at different levels of σ
+    // src in /rust/benches/discrete_laplace/main.rs
+
+    // execute bench via:
+    //     cargo bench --bench discrete_laplace --features untrusted
+
+    // σ  linear cks20
+    // 1   4.907  9.176
+    // 2   5.614 10.565
+    // 3   6.585 11.592
+    // 4   7.450 10.742
+    // 5   8.320 12.364
+    // 6   9.213 11.722
+    // 7  10.106 11.216
+    // 8  11.061 10.737
+    // 9  11.836 12.884
+    // 10 12.653 12.482
+    // 11 13.605 12.248
+    // 12 14.465 13.320
+    // 13 16.545 11.767
+    // 14 31.647 15.229
+    // 15 25.852 15.177
+    // 16 20.179 11.465
+    // 17 19.120 13.478
+    // 18 19.768 12.982
+    // 19 20.777 12.977
+    if scale > QO::exact_int_cast(10)? {
         make_base_discrete_laplace_cks20(scale)
     } else {
         make_base_discrete_laplace_linear(scale, None)
     }
 }
 
-#[cfg(not(feature="use-mpfr"))]
+#[cfg(not(feature = "use-mpfr"))]
 pub fn make_base_discrete_laplace<D, QO>(
     scale: QO,
 ) -> Fallible<Measurement<D, D, D::InputMetric, MaxDivergence<QO>>>
