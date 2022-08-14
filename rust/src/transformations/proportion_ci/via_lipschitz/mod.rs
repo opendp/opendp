2025fe493d8cdeca3608e1c8c14c3d953c6c1a65
Shoeboxam <shoeboxam@gmail.com>
2022-08-14 13:43:39 -0400

    add tests

--- rust/src/transformations/proportion_ci/via_lipschitz/mod.rs
+++ rust/src/transformations/proportion_ci/via_lipschitz/mod.rs
@@ -100,7 +100,7 @@ pub fn make_lipschitz_sized_proportion_ci_variance<TA: Float>(
     let stability_constant = (strat_sizes.iter())
         .zip(sample_sizes.iter())
         .zip(weights.iter())
-        .map(|((&N, &n), &w)| (w / n).powi(2) * (N - n) / N)
+        .map(|((&N, &n), &w)| w.powi(2) * (N - n) / N / (n - _1 / n))
         .reduce(|l, r| l.max(r))
         .unwrap_assert("there is always at least one partition");
 
@@ -120,4 +120,32 @@ pub fn make_lipschitz_sized_proportion_ci_variance<TA: Float>(
         AbsoluteDistance::default(),
         StabilityMap::new_from_constant(stability_constant),
     ))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // there are integration tests in python
+    #[test]
+    fn test_lipschitz_sized_proportion_ci_mean() -> Fallible<()> {
+        let strat_sizes = vec![100usize; 10];
+        let sample_sizes = vec![10usize; 10];
+        let trans = make_lipschitz_sized_proportion_ci_mean(strat_sizes, sample_sizes)?;
+
+        println!("invoke {:?}", trans.invoke(&vec![5.; 10])?);
+        println!("map {:?}", trans.map(&1.)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_lipschitz_sized_proportion_ci_variance() -> Fallible<()> {
+        let strat_sizes = vec![100usize; 10];
+        let sample_sizes = vec![10usize; 10];
+        let trans = make_lipschitz_sized_proportion_ci_variance(strat_sizes, sample_sizes, 1.)?;
+
+        println!("invoke {:?}", trans.invoke(&vec![5.; 10])?);
+        println!("map {:?}", trans.map(&1.)?);
+        Ok(())
+    }
 }
