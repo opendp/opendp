1724fecbd201574834e8abcab35a58ccd24133ae
Shoeboxam <shoeboxam@gmail.com>
2022-08-14 23:38:04 -0400

    debug/test postprocessors

--- python/test/integration/test_proportion_ci.py
+++ python/test/integration/test_proportion_ci.py
@@ -2,11 +2,12 @@ from opendp.trans import *
 from opendp.meas import *
 from opendp.comb import *
 from opendp.mod import enable_features
+from opendp.typing import *
 
 enable_features("floating-point", "contrib")
 
 
-def test_dp_proportion_cis():
+def test_dp_proportion_cis_population():
     partitions = 2
     sample_size = 1000
 
@@ -42,6 +43,89 @@ def test_dp_proportion_cis():
                 make_base_gaussian(scale_var)
             )
         ])
+    )
+    mean, var = mean_var_meas(data)
+    print("mean:", mean)
+    print("var:", var)
+    print("rho:", mean_var_meas.map(1))
+    print("eps:", make_zCDP_to_approxDP(mean_var_meas).map(1).epsilon(1e-7))
+
+
+def test_postprocess_sized_proportion_ci():
+    partitions = 2
+    sample_size = 1000
+
+    strat_sizes = [sample_size * 2] * partitions
+    sample_sizes = [sample_size] * partitions
+
+    from string import ascii_uppercase
+    partition_idents = [i for i in ascii_uppercase[:partitions]]
+
+    from random import choice
+    from itertools import chain
+    values = [choice((0, 1)) for _ in range(sample_size * partitions)]
+    idents = chain(*([ident] * sample_size for ident in partition_idents))
+
+    data = "\n".join(f"{v},{i}" for v, i in zip(values, idents))
+
+    scale = 10.
+
+    mean_var_meas = (
+        make_split_dataframe(separator=",", col_names=["values", "idents"]) >>
+        make_df_is_equal("values", "1") >>
+        make_filter_by("values", keep_columns=["idents"]) >>
+        make_select_column("idents", TOA=str) >>
+        make_count_by_categories(partition_idents, TOA=float, MO=L2Distance[float]) >>
+        make_base_gaussian(scale, D=VectorDomain[AllDomain[float]]) >>
+        make_postprocess_sized_proportion_ci(
+            strat_sizes, sample_sizes, scale
+        )
+    )
+    mean, var = mean_var_meas(data)
+    print("mean:", mean)
+    print("var:", var)
+    print("rho:", mean_var_meas.map(1))
+    print("eps:", make_zCDP_to_approxDP(mean_var_meas).map(1).epsilon(1e-7))
+
+
+def test_postprocess_proportion_ci():
+    partitions = 2
+    sample_size = 1000
+
+    strat_sizes = [sample_size * 2] * partitions
+    # sample_sizes = [sample_size] * partitions
+
+    from string import ascii_uppercase
+    partition_idents = [i for i in ascii_uppercase[:partitions]]
+
+    from random import choice
+    from itertools import chain
+    values = [choice((0, 1)) for _ in range(sample_size * partitions)]
+    idents = chain(*([ident] * sample_size for ident in partition_idents))
+
+    data = "\n".join(f"{v},{i}" for v, i in zip(values, idents))
+
+    sum_scale = 10.
+    size_scale = 10.
+
+    mean_var_meas = (
+        make_split_dataframe(separator=",", col_names=["values", "idents"]) >>
+        make_basic_composition([
+            (
+                make_df_is_equal("values", "1") >>
+                make_filter_by("values", keep_columns=["idents"]) >>
+                make_select_column("idents", TOA=str) >>
+                make_count_by_categories(partition_idents, TOA=float, MO=L2Distance[float]) >>
+                make_base_gaussian(sum_scale, D=VectorDomain[AllDomain[float]])
+            ),
+            (
+                make_select_column("idents", TOA=str) >>
+                make_count_by_categories(partition_idents, TOA=float, MO=L2Distance[float]) >>
+                make_base_gaussian(size_scale, D=VectorDomain[AllDomain[float]])
+            )
+        ]) >> make_postprocess_proportion_ci(
+            strat_sizes, sum_scale, size_scale
+        )
     )
     mean, var = mean_var_meas(data)
     print("mean:", mean)
--- rust/src/transformations/proportion_ci/via_postprocess/ffi.rs
+++ rust/src/transformations/proportion_ci/via_postprocess/ffi.rs
@@ -1,10 +1,19 @@
-use std::{os::raw::{c_char, c_void}, convert::TryFrom};
+use std::{
+    convert::TryFrom,
+    os::raw::{c_char, c_void},
+};
 
 use crate::{
-    core::{FfiResult, IntoAnyTransformationFfiResultExt},
-    ffi::{any::{AnyObject, AnyTransformation, Downcast}, util::Type},
+    core::{FfiResult, Function, IntoAnyTransformationFfiResultExt, Transformation},
+    domains::VectorDomain,
+    ffi::{
+        any::{AnyDomain, AnyObject, AnyTransformation, Downcast},
+        util::Type,
+    },
     traits::Float,
-    transformations::{make_postprocess_sized_proportion_ci, make_postprocess_proportion_ci},
+    transformations::{
+        make_postprocess, make_postprocess_proportion_ci, make_postprocess_sized_proportion_ci,
+    },
 };
 
 #[no_mangle]
@@ -30,7 +39,6 @@ pub extern "C" fn opendp_trans__make_postprocess_sized_proportion_ci(
     ], (strat_sizes, sample_sizes, scale))
 }
 
-
 #[no_mangle]
 pub extern "C" fn opendp_trans__make_postprocess_proportion_ci(
     strat_sizes: *const AnyObject,
@@ -45,7 +53,33 @@ pub extern "C" fn opendp_trans__make_postprocess_proportion_ci(
     ) -> FfiResult<*mut AnyTransformation> {
         let sum_scale = *try_as_ref!(sum_scale as *const TA);
         let size_scale = *try_as_ref!(size_scale as *const TA);
-        make_postprocess_proportion_ci::<TA>(strat_sizes, sum_scale, size_scale).into_any()
+
+        let Transformation {
+            input_domain,
+            output_domain,
+            function,
+            ..
+        } = try_!(make_postprocess_proportion_ci::<TA>(
+            strat_sizes,
+            sum_scale,
+            size_scale
+        ));
+
+        make_postprocess(
+            VectorDomain::new(AnyDomain::new(input_domain.element_domain)),
+            output_domain,
+            Function::new_fallible(move |arg: &Vec<AnyObject>| {
+                if let [sample_sums, sample_sizes] = &arg[0..2] {
+                    function.eval(&vec![
+                        sample_sums.downcast_ref::<Vec<TA>>()?.clone(),
+                        sample_sizes.downcast_ref::<Vec<TA>>()?.clone(),
+                    ])
+                } else {
+                    fallible!(FailedFunction, "expected an input of [sums, counts]")
+                }
+            }),
+        )
+        .into_any()
     }
     let strat_sizes = try_!(try_as_ref!(strat_sizes).downcast_ref::<Vec<usize>>()).clone();
     let TA = try_!(Type::try_from(TA));
