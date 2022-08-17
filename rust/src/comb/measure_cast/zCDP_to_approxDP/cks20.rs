497c7a627e65c4c7e2717eab6e605e05439b5f7c
Shoeboxam <shoeboxam@gmail.com>
2022-08-16 22:06:28 -0400

    adjust licensing update comments

--- rust/src/comb/measure_cast/zCDP_to_approxDP/cks20.rs
+++ rust/src/comb/measure_cast/zCDP_to_approxDP/cks20.rs
@@ -1,16 +1,39 @@
-// Copyright 2022 OpenDP
+// Copyright (c) 2022 President and Fellows of Harvard College
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright 2020 Thomas Steinke
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 // Algorithm from:
 //     Clément Canonne, Gautam Kamath, Thomas Steinke. The Discrete Gaussian for Differential Privacy. 2020.
@@ -102,6 +125,7 @@ where
         .inf_div(&_2.neg_inf_mul(&rho)?)?
         .inf_add(&_2)?;
 
+    // find alpha
     loop {
         let diff = a_max - a_min;
         if diff <= tol {
@@ -110,7 +134,7 @@ where
 
         let a_mid = a_min + diff / _2;
 
-        // calculate derivative
+        // calculate derivative. inf_* arithmetic is not necessary because any choice of α is valid
         let deriv = (_2 * a_mid - _1) * rho - eps + a_mid.recip().neg().ln_1p();
         //        = (2α - 1)            ρ   - ε   + ln1p(-1/α)
 
