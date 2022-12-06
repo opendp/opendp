a587fb11b106e1eeb65df2cc98649a1eefcd950f
Shoeboxam <shoeboxam@gmail.com>
2022-12-06 14:16:50 -0500

    new nestable helper

--- rust/src/interactive.rs
+++ rust/src/interactive.rs
@@ -78,6 +78,63 @@ impl<Q: 'static, A: 'static> Queryable<Q, A> {
             base: QueryableBase::new(transition),
         }
     }
+
+
+    pub(crate) fn new_nestable<S: 'static, QD: 'static + Clone>(
+        mut state: S,
+        pre_eval: impl Fn(&S, &Q) -> Fallible<QD> + 'static,
+        eval: impl Fn(&mut S, &Q, &QueryableBase) -> Fallible<A> + 'static,
+        pre_child_eval: impl Fn(&S, &DescendantChange<QD>) -> Fallible<QD> + 'static,
+        child_eval: impl Fn(&mut S, &DescendantChange<QD>) -> Fallible<()> + 'static
+    ) -> Self {
+        let mut context: Option<Context> = None;
+        Queryable::new(
+            move |self_: &QueryableBase, query: &dyn Any| {
+                if let Some(query) = query.downcast_ref::<Q>() {
+
+                    let d_mid = pre_eval(&state, &query)?;
+                    
+                    // if there is context, run a pre-commit
+                    if let Some(context) = &mut context {
+                        context.pre_commit(&d_mid)?;
+                    }
+
+                    let answer = eval(&mut state, &query, self_)?;
+
+                    // if there is context, commit the changes
+                    if let Some(context) = &mut context {
+                        context.commit(&d_mid)?;
+                    }
+
+                    return Ok(Box::new(answer) as Box<dyn Any>);
+                }
+
+                if let Some(q) = query.downcast_ref::<Context>() {
+                    if context.is_some() {
+                        return fallible!(FailedFunction, "context has already been set");
+                    }
+                    context.replace(q.clone());
+                    return Ok(Box::new(()) as Box<dyn Any>);
+                }
+
+                // children are always IM's, so new_privacy_loss is bounded by d_mid_i
+                if let Some(query) = query.downcast_ref::<DescendantChange<QD>>() {
+                    let d_temp = pre_child_eval(&state, query)?;
+                    if let Some(context) = &mut context {
+                        context.parent.eval_any(&DescendantChange {
+                            id: context.id,
+                            new_privacy_loss: d_temp,
+                            commit: query.commit,
+                        })?;
+                    };
+                    child_eval(&mut state, query)?;
+                    return Ok(Box::new(()) as Box<dyn Any>)
+                }
+
+                fallible!(FailedFunction, "unrecognized query!")
+            }
+        )
+    }
 }
 
 impl<Q: 'static> Queryable<Q, Box<dyn Any>> {
@@ -92,7 +149,9 @@ impl<Q: 'static> Queryable<Q, Box<dyn Any>> {
 
 
 pub(crate) struct DescendantChange<Q> {
+    #[allow(dead_code)]
     pub id: usize,
+    #[allow(dead_code)]
     pub new_privacy_loss: Q,
     pub commit: bool,
 }
