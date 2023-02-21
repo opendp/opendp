9458cbf10af642b63d47a1aafdbb57861dd036e6
Shoeboxam <shoeboxam@gmail.com>
2023-02-21 02:29:36 -0500

    prettify the test

--- python/test/test_interactive.py
+++ python/test/test_interactive.py
@@ -11,32 +11,37 @@ from opendp.metrics import symmetric_distance, absolute_distance
 from opendp.measures import max_divergence
 
 def test_concurrent_composition():
+    max_influence = 1
     cc_meas = make_concurrent_composition(
         input_domain=vector_domain(all_domain(int)),
         input_metric=symmetric_distance(),
         output_measure=max_divergence(float),
-        d_in=1,
+        d_in=max_influence,
         d_mids=[0.2, 0.3, 0.4, 0.7]
     )
 
     cc_qbl: Queryable = cc_meas([1] * 200)
 
-    print(cc_qbl)
+    print("ConComp IM:", cc_qbl)
     sum_query = make_clamp((0, 10)) >> make_bounded_sum((0, 10)) >> make_base_discrete_laplace(100.)
 
     print("evaluating")
     print(cc_qbl(sum_query))
 
-    exact_sum_cc_qbl = cc_qbl(make_clamp((0, 10)) >> make_bounded_sum((0, 10)) >> make_concurrent_composition(
-        input_domain=all_domain(int),
-        input_metric=absolute_distance(int),
+    noise_query = make_base_discrete_laplace(200.)
+    exact_sum = make_clamp((0, 10)) >> make_bounded_sum((0, 10))
+    print("exact sum:", exact_sum)
+    exact_sum_cc_qbl = cc_qbl(exact_sum >> make_concurrent_composition(
+        input_domain=exact_sum.output_domain,
+        input_metric=exact_sum.output_metric,
         output_measure=max_divergence(float),
-        d_in=10,
+        d_in=exact_sum.map(max_influence),
         d_mids=[0.2, 0.09]
     ))
 
-    noise_query = make_base_discrete_laplace(200.)
-    print(exact_sum_cc_qbl(noise_query))
-    print(cc_qbl(sum_query))
-    print(exact_sum_cc_qbl(noise_query))
+    print("child release:", exact_sum_cc_qbl(noise_query))
+    print("root release: ", cc_qbl(sum_query))
+    print("child release:", exact_sum_cc_qbl(noise_query))
 
+
+test_concurrent_composition()
