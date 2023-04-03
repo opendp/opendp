402d795539a339db1c965b96e844b51369d40c22
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2023-04-03 08:32:16 -0700

    WIP

--- rust/src/beam/serialization.rs
+++ rust/src/beam/serialization.rs
@@ -1,4 +1,5 @@
 use std::any::{Any, type_name};
+use std::marker::PhantomData;
 use crate::error::Fallible;
 use crate::ffi::util::Type;
 
@@ -6,9 +7,19 @@ use crate::ffi::util::Type;
 pub trait Operation<A, R> {
     fn into_closure(self) -> Box<dyn Fn(&A) -> R>;
     fn serialize(&self) -> Serialization;
-    fn deserializer_op(&self) -> fn(&Serialization) -> Fallible<AnyOperation>;
-    // fn deserializer_fn(&self) -> fn(&Serialization) -> Fallible<Box<dyn Any>>;
+    fn deserializer(&self) -> fn(&Serialization) -> Fallible<AnyOperation>;
 }
+// impl<T, A, R> Operation<A, R> for Box<T> where T: Operation<A, R> + ?Sized {
+//     fn into_closure(self) -> Box<dyn Fn(&A) -> R> {
+//         (*self).into_closure()
+//     }
+//     fn serialize(&self) -> Serialization {
+//         todo!()
+//     }
+//     fn deserializer(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
+//         todo!()
+//     }
+// }
 
 pub struct Serialization {
     E: Type,
@@ -32,6 +43,11 @@ pub struct AnyOperation {
 impl AnyOperation {
     pub fn new<A: 'static, R: 'static>(operation: impl Operation<A, R>) -> Self {
         let holder = FnHolder(operation.into_closure());
+        let holder = Box::new(holder) as Box<dyn Any>;
+        Self { holder }
+    }
+    pub fn new_fn<A: 'static, R: 'static>(closure: impl Fn(&A) -> R) -> Self {
+        let holder = FnHolder(Box::new(closure));
         let holder = Box::new(holder) as Box<dyn Any>;
         Self { holder }
     }
@@ -60,8 +76,36 @@ impl<A, R> Operation<A, R> for StructOperation<A, R> {
     fn serialize(&self) -> Serialization {
         (self.serializer)(&self)
     }
-    fn deserializer_op(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
-        unimplemented!()
+    fn deserializer(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
+        self.deserializer
+    }
+}
+
+pub struct EnvOperation<E, A, R> {
+    _a: PhantomData<A>,
+    _r: PhantomData<R>,
+    env: E,
+    f: fn(env: &E, arg: &A) -> R,
+}
+impl<E, A, R> EnvOperation<E, A, R> {
+    pub fn new(env: E, f: fn(env: &E, arg: &A) -> R) -> Self {
+        Self { _a: PhantomData, _r: PhantomData, env, f }
+    }
+}
+impl<E: 'static, A: 'static, R: 'static> Operation<A, R> for EnvOperation<E, A, R> {
+    fn into_closure(self) -> Box<dyn Fn(&A) -> R> {
+        let env = self.env;
+        let f = self.f;
+        let clos = move |arg: &A| -> R {
+            f(&env, arg)
+        };
+        Box::new(clos)
+    }
+    fn serialize(&self) -> Serialization {
+        todo!()
+    }
+    fn deserializer(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
+        todo!()
     }
 }
 
@@ -71,82 +115,35 @@ pub fn go_internal<A, R, O: Operation<A, R>>(arg: Vec<A>, op: O) -> Vec<R> {
 }
 
 pub fn go_external<A: 'static, R: 'static, O: Operation<A, R>>(arg: Vec<A>, op: O) -> Vec<R> {
-    let do_op = true;
-    if do_op {
-        go_external_op(arg, op)
-    } else {
-        go_external_fn(arg, op)
-    }
-}
-
-pub fn go_external_op<A: 'static, R: 'static, O: Operation<A, R>>(arg: Vec<A>, op: O) -> Vec<R> {
-    let deserializer = op.deserializer_op();
+    let deserializer = op.deserializer();
     let serialized = op.serialize();
     let op = (deserializer)(&serialized).unwrap();
     let f = op.into_closure().unwrap();
     arg.iter().map(f).collect()
 }
 
-pub fn go_external_fn<A: 'static, R: 'static, O: Operation<A, R>>(arg: Vec<A>, op: O) -> Vec<R> {
-    // let deserializer = op.deserializer_fn();
-    // let serialized = op.serialize();
-    // let op = (deserializer)(&serialized).unwrap();
-    // let f = op.downcast::<FnHolder<A, R>>().unwrap();
-    // let f = f.0;
-    // arg.iter().map(f).collect()
-    todo!()
-}
-
 pub mod m1 {
-    use std::any::Any;
-    use std::marker::PhantomData;
     use serde::{Deserialize, Serialize};
-    use crate::beam::serialization::{AnyOperation, FnHolder, Operation, Serialization};
+    use crate::beam::serialization::{AnyOperation, EnvOperation, Operation, Serialization, StructOperation};
     use crate::error::Fallible;
     use crate::ffi::util::Type;
     use crate::traits::Number;
 
-    pub struct EnvOperation<E, A, R> {
-        _a: PhantomData<A>,
-        _r: PhantomData<R>,
-        env: E,
-        f: fn(env: &E, arg: &A) -> R,
-    }
-    impl<E: 'static, A: 'static, R: 'static> Operation<A, R> for EnvOperation<E, A, R> {
-        fn into_closure(self) -> Box<dyn Fn(&A) -> R> {
-            let env = self.env;
-            let f = self.f;
-            let clos = move |arg: &A| -> R {
-                f(&env, arg)
-            };
-            Box::new(clos)
-        }
-        fn serialize(&self) -> Serialization {
-            todo!()
-        }
-        fn deserializer_op(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
-            todo!()
-        }
-        // fn deserializer_fn(&self) -> fn(&Serialization) -> Fallible<Box<dyn Any>> {
-        //     todo!()
-        // }
-    }
-
     pub struct MulOperation<T: Number> {
-        env: T
+        x: T
     }
     impl<T: Number + Serialize> Operation<T, T> for MulOperation<T> {
         fn into_closure(self) -> Box<dyn Fn(&T) -> T> {
-            Box::new(move |arg| *arg * self.env)
+            Box::new(move |arg| *arg * self.x)
         }
         fn serialize(&self) -> Serialization {
-            Serialization::new(Type::of::<T>(), Type::of::<T>(), Type::of::<T>(), serde_json::to_string(&self.env).unwrap())
+            Serialization::new(Type::of::<T>(), Type::of::<T>(), Type::of::<T>(), serde_json::to_string(&self.x).unwrap())
         }
-        fn deserializer_op(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
+        fn deserializer(&self) -> fn(&Serialization) -> Fallible<AnyOperation> {
             fn deserializer(serialization: &Serialization) -> Fallible<AnyOperation> {
                 fn monomorphize<'a, T: Number + Serialize + Deserialize<'a>>(serialized_env: &'a str) -> Fallible<AnyOperation> {
                     let env: T = serde_json::from_str(serialized_env).unwrap();
-                    let op = MulOperation { env };
+                    let op = MulOperation { x: env };
                     Ok(AnyOperation::new(op))
                 }
                 dispatch!(monomorphize, [
@@ -155,48 +152,35 @@ pub mod m1 {
             }
             deserializer
         }
-        // fn deserializer_fn(&self) -> fn(&Serialization) -> Fallible<Box<dyn Any>> {
-        //     fn deserializer(serialization: &Serialization) -> Fallible<Box<dyn Any>> {
-        //         fn monomorphize<'a, T: Number + Serialize + Deserialize<'a>>(serialized_env: &'a str) -> Fallible<Box<dyn Any>> {
-        //             let env: T = serde_json::from_str(serialized_env).unwrap();
-        //             let op = MulOperation { env };
-        //             let f = op.into_closure();
-        //             Ok(Box::new(FnHolder(f)))
-        //         }
-        //         dispatch!(monomorphize, [
-        //             (serialization.E, @numbers)
-        //         ], (&serialization.env))
-        //     }
-        //     deserializer
-        // }
     }
 
-    pub fn make_mul<T: Number>(x: T) -> MulOperation<T> {
-        MulOperation { env: x }
+    pub fn make_mul_direct_struct<T: Number>(x: T) -> MulOperation<T> {
+        MulOperation { x }
     }
 
-    // pub fn make_mul<T: Number>(x: T) -> StructOperation<T, T> {
-        // fn make_closure<T: Number>(env: T) -> Box<dyn Fn(&T) -> T> {
-        //     Box::new(move |x| *x * env)
-        // }
-        // let closure = make_closure::<T>(x);
-        // let serializer = Box::new(|s: &StructOperation<T, T>| {
-        //     Serialization::new(Type::of::<T>(), Type::of::<T>(), Type::of::<T>(), serde_json::to_string(&s.env).unwrap())
-        // });
-        // fn deserializer(serialization: &Serialization) -> Fallible<Box<dyn Any>> {
-        //     fn monomorphize<'a, T: Number + Deserialize<'a>>(serialized_env: &'a str) -> Fallible<Box<dyn Any>> {
-        //         let env: T = serde_json::from_str(serialized_env).unwrap();
-        //         let closure = make_closure(env);
-        //         let closure = ConcreteFn(closure);
-        //         Ok(Box::new(closure))
-        //     }
-        //     dispatch!(monomorphize, [
-        //         (serialization.E, @numbers)
-        //     ], (&serialization.env))
-        // }
-        //
-        // StructOperation::new(closure, serializer, deserializer)
+    // pub fn make_mul_direct_trait_obj<T: Number + Serialize>(x: T) -> Box<dyn Operation<T, T>> {
+    //     Box::new(MulOperation { x })
     // }
+
+    pub fn make_mul_env<T: Number>(x: T) -> EnvOperation<T, T, T> {
+        fn make_closure<T: Number>(env: T) -> Box<dyn Fn(&T) -> T> {
+            Box::new(move |x| *x * env)
+        }
+        let closure = make_closure::<T>(x);
+        fn deserializer(serialization: &Serialization) -> Fallible<AnyOperation> {
+            fn monomorphize<'a, T: Number + Deserialize<'a>>(serialized_env: &'a str) -> Fallible<AnyOperation> {
+                let env: T = serde_json::from_str(serialized_env).unwrap();
+                let closure = make_closure(env);
+                let closure = ConcreteFn(closure);
+                Ok(Box::new(closure))
+            }
+            dispatch!(monomorphize, [
+                (serialization.E, @numbers)
+            ], (&serialization.env))
+        }
+
+        StructOperation::new(closure, serializer, deserializer)
+    }
 
     #[cfg(test)]
     pub mod test {
@@ -206,7 +190,8 @@ pub mod m1 {
 
         #[test]
         fn test_mul_internal() -> Fallible<()> {
-            let op = make_mul(3);
+            // let op = make_mul_direct_struct(3);
+            let op = make_mul_direct_struct(3);
             let arg = vec![1, 2, 3];
             let res = go_internal(arg, op);
             assert_eq!(res, vec![3, 6, 9]);
@@ -215,7 +200,7 @@ pub mod m1 {
 
         #[test]
         fn test_mul_external() -> Fallible<()> {
-            let op = make_mul(3);
+            let op = make_mul_direct_struct(3);
             let arg = vec![1, 2, 3];
             let res = go_external(arg, op);
             assert_eq!(res, vec![3, 6, 9]);
