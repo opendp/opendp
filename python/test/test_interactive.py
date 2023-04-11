a9995a0d1ba74c3cfabe0a67d5243e5e79cf49b7
Shoeboxam <shoeboxam@gmail.com>
2023-04-11 13:47:16 -0400

    post-rebase fix

--- python/test/test_interactive.py
+++ python/test/test_interactive.py
@@ -1,8 +1,17 @@
 
+from opendp.measures import max_divergence
+from opendp.metrics import symmetric_distance
+from opendp.domains import *
+from opendp.combinators import make_sequential_composition
+from opendp.measurements import make_base_discrete_laplace
+from opendp.transformations import *
 from opendp.mod import Queryable, enable_features
 enable_features("contrib")
 
+<<<<<<< HEAD
 import opendp.prelude as dp
+=======
+>>>>>>> 754dc56ca5 (post-rebase fix)
 
 def test_sequential_composition():
     max_influence = 1
@@ -17,13 +26,13 @@ def test_sequential_composition():
     sc_qbl: Queryable = sc_meas([1] * 200)
 
     print("SeqComp IM:", sc_qbl)
-    sum_query = dp.t.make_clamp((0, 10)) >> dp.t.make_bounded_sum((0, 10)) >> dp.m.make_base_discrete_laplace(100.)
+    sum_query = dp.t.make_clamp(sc_meas.input_domain, sc_meas.input_metric, (0, 10)) >> dp.t.make_bounded_sum((0, 10)) >> dp.m.make_base_discrete_laplace(100.)
 
     print("evaluating")
     print(sc_qbl(sum_query))
 
     noise_query = dp.m.make_base_discrete_laplace(200.)
-    exact_sum = dp.t.make_clamp((0, 10)) >> dp.t.make_bounded_sum((0, 10))
+    exact_sum = dp.t.make_clamp(sc_meas.input_domain, sc_meas.input_metric, (0, 10)) >> dp.t.make_bounded_sum((0, 10))
     print("exact sum:", exact_sum)
     exact_sum_sc_qbl = sc_qbl(exact_sum >> dp.c.make_sequential_composition(
         input_domain=exact_sum.output_domain,
