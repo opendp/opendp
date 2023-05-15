b8db775df0abb73d7bf9989c8bc44673eee4b408
Michael Shoemate <shoeboxam@gmail.com>
2023-05-15 12:22:24 -0400

    Python `prelude` module (#730)

--- python/test/test_binary_search.py
+++ python/test/test_binary_search.py
@@ -1,19 +1,17 @@
-from opendp.mod import binary_search_param, enable_features, binary_search
-from opendp.transformations import make_bounded_sum, make_clamp, make_sized_bounded_sum, make_sized_bounded_mean
-from opendp.measurements import make_base_laplace
+import opendp.prelude as dp
 
-enable_features('floating-point', 'contrib')
+dp.enable_features('floating-point', 'contrib')
 
 
 def test_binary_search_overflow():
     d_in = 1
     d_out = 1.01
     bounded_sum = (
-        make_clamp(bounds=(0.0, 1.0)) >>
-        make_bounded_sum(bounds=(0.0, 1.0))
+        dp.t.make_clamp(bounds=(0.0, 1.0)) >>
+        dp.t.make_bounded_sum(bounds=(0.0, 1.0))
     )
-    binary_search_param(
-        lambda s: bounded_sum >> make_base_laplace(scale=s),
+    dp.binary_search_param(
+        lambda s: bounded_sum >> dp.m.make_base_laplace(scale=s),
         d_in=d_in,
         d_out=d_out
     )
@@ -22,33 +20,33 @@ def test_stuck():
     epsilon = 1.3
     sens = 500_000.0 * 500_000.0
     bounded_sum = (
-        make_clamp(bounds=(0.0, sens)) >>
-        make_bounded_sum(bounds=(0.0, sens))
+        dp.t.make_clamp(bounds=(0.0, sens)) >>
+        dp.t.make_bounded_sum(bounds=(0.0, sens))
     )
     real_v = sens / epsilon
-    discovered_scale = binary_search_param(
-        lambda s: bounded_sum >> make_base_laplace(scale=s),
+    discovered_scale = dp.binary_search_param(
+        lambda s: bounded_sum >> dp.m.make_base_laplace(scale=s),
         d_in=1,
         bounds=(0.0, real_v * 2.0),
         d_out=(epsilon))
     print(discovered_scale)
     
 def test_binary_search():
-    assert binary_search(lambda x: x <= -5, T=int) == -5
-    assert binary_search(lambda x: x <= 5, T=int) == 5
-    assert binary_search(lambda x: x >= -5, T=int) == -5
-    assert binary_search(lambda x: x >= 5, T=int) == 5
+    assert dp.binary_search(lambda x: x <= -5, T=int) == -5
+    assert dp.binary_search(lambda x: x <= 5, T=int) == 5
+    assert dp.binary_search(lambda x: x >= -5, T=int) == -5
+    assert dp.binary_search(lambda x: x >= 5, T=int) == 5
 
 
 def test_type_inference():
     def chainer(b):
-        return make_sized_bounded_sum(1000, (-b, b))
-    assert binary_search_param(chainer, 2, 100) == 50
+        return dp.t.make_sized_bounded_sum(1000, (-b, b))
+    assert dp.binary_search_param(chainer, 2, 100) == 50
 
     def mean_chainer_n(n):
-        return make_sized_bounded_mean(n, (-20., 20.))
-    assert binary_search_param(mean_chainer_n, 2, 1.) == 41
+        return dp.t.make_sized_bounded_mean(n, (-20., 20.))
+    assert dp.binary_search_param(mean_chainer_n, 2, 1.) == 41
 
     def mean_chainer_b(b):
-        return make_sized_bounded_mean(1000, (-b, b))
-    assert 499.999 < binary_search_param(mean_chainer_b, 2, 1.) < 500.
+        return dp.t.make_sized_bounded_mean(1000, (-b, b))
+    assert 499.999 < dp.binary_search_param(mean_chainer_b, 2, 1.) < 500.
