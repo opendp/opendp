c85fb558f234441ee2603c893a42be428b2f5249
Shoeboxam <shoeboxam@gmail.com>
2023-05-16 16:31:00 -0400

    address review comments
    thanks Andy!

--- python/test/test_interactive.py
+++ python/test/test_interactive.py
@@ -2,20 +2,14 @@
 from opendp.mod import Queryable, enable_features
 enable_features("contrib")
 
-from opendp.transformations import *
-from opendp.measurements import make_base_discrete_laplace
-from opendp.combinators import make_sequential_composition
-
-from opendp.domains import *
-from opendp.metrics import symmetric_distance
-from opendp.measures import max_divergence, fixed_smoothed_max_divergence
+import opendp.prelude as dp
 
 def test_sequential_composition():
     max_influence = 1
-    sc_meas = make_sequential_composition(
-        input_domain=vector_domain(atom_domain(T=int)),
-        input_metric=symmetric_distance(),
-        output_measure=max_divergence(float),
+    sc_meas = dp.c.make_sequential_composition(
+        input_domain=dp.vector_domain(dp.atom_domain(T=int)),
+        input_metric=dp.symmetric_distance(),
+        output_measure=dp.max_divergence(float),
         d_in=max_influence,
         d_mids=[0.2, 0.3, 0.4, 0.7]
     )
@@ -23,18 +17,18 @@ def test_sequential_composition():
     sc_qbl: Queryable = sc_meas([1] * 200)
 
     print("SeqComp IM:", sc_qbl)
-    sum_query = make_clamp((0, 10)) >> make_bounded_sum((0, 10)) >> make_base_discrete_laplace(100.)
+    sum_query = dp.t.make_clamp((0, 10)) >> dp.t.make_bounded_sum((0, 10)) >> dp.t.make_base_discrete_laplace(100.)
 
     print("evaluating")
     print(sc_qbl(sum_query))
 
-    noise_query = make_base_discrete_laplace(200.)
-    exact_sum = make_clamp((0, 10)) >> make_bounded_sum((0, 10))
+    noise_query = dp.c.make_base_discrete_laplace(200.)
+    exact_sum = dp.t.make_clamp((0, 10)) >> dp.t.make_bounded_sum((0, 10))
     print("exact sum:", exact_sum)
-    exact_sum_sc_qbl = sc_qbl(exact_sum >> make_sequential_composition(
+    exact_sum_sc_qbl = sc_qbl(exact_sum >> dp.c.make_sequential_composition(
         input_domain=exact_sum.output_domain,
         input_metric=exact_sum.output_metric,
-        output_measure=max_divergence(float),
+        output_measure=dp.max_divergence(float),
         d_in=exact_sum.map(max_influence),
         d_mids=[0.2, 0.09]
     ))
@@ -46,12 +40,17 @@ def test_sequential_composition():
 
 def test_sequential_composition_approxdp():
     max_influence = 1
-    sc_meas = make_sequential_composition(
-        input_domain=vector_domain(atom_domain(T=int)),
-        input_metric=symmetric_distance(),
-        output_measure=fixed_smoothed_max_divergence(float),
+    sc_meas = dp.c.make_sequential_composition(
+        input_domain=dp.vector_domain(dp.atom_domain(T=int)),
+        input_metric=dp.symmetric_distance(),
+        output_measure=dp.fixed_smoothed_max_divergence(float),
         d_in=max_influence,
         d_mids=[(1., 1e-6), (1., 1e-6)]
     )
 
-    _sc_qbl: Queryable = sc_meas([1] * 200)
+    sc_qbl: Queryable = sc_meas([1] * 200)
+
+    gauss_meas = dp.c.make_fix_delta(dp.c.make_zCDP_to_approxDP(dp.m.make_base_discrete_gaussian(100.)), 1e-6)
+
+    sum_meas = dp.t.make_clamp((0, 10)) >> dp.t.make_bounded_sum((0, 10)) >> gauss_meas
+    sc_qbl(sum_meas)
