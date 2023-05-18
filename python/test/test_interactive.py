0de530bf1e5996da03ed5be19c3ca8aa57294843
Shoeboxam <shoeboxam@gmail.com>
2023-05-18 13:31:17 -0400

    post-rebase fix

--- python/test/test_interactive.py
+++ python/test/test_interactive.py
@@ -1,17 +1,5 @@
-
-from opendp.measures import max_divergence
-from opendp.metrics import symmetric_distance
-from opendp.domains import *
-from opendp.combinators import make_sequential_composition
-from opendp.measurements import make_base_discrete_laplace
-from opendp.transformations import *
-from opendp.mod import Queryable, enable_features
-enable_features("contrib")
-
-<<<<<<< HEAD
 import opendp.prelude as dp
-=======
->>>>>>> 754dc56ca5 (post-rebase fix)
+dp.enable_features("contrib")
 
 def test_sequential_composition():
     max_influence = 1
@@ -23,7 +11,7 @@ def test_sequential_composition():
         d_mids=[0.2, 0.3, 0.4, 0.7]
     )
 
-    sc_qbl: Queryable = sc_meas([1] * 200)
+    sc_qbl: dp.Queryable = sc_meas([1] * 200)
 
     print("SeqComp IM:", sc_qbl)
     sum_query = dp.t.make_clamp(sc_meas.input_domain, sc_meas.input_metric, (0, 10)) >> dp.t.make_bounded_sum((0, 10)) >> dp.m.make_base_discrete_laplace(100.)
@@ -57,7 +45,7 @@ def test_sequential_composition_approxdp():
         d_mids=[(1., 1e-6), (1., 1e-6)]
     )
 
-    sc_qbl: Queryable = sc_meas([1] * 200)
+    sc_qbl: dp.Queryable = sc_meas([1] * 200)
 
     gauss_meas = dp.c.make_fix_delta(dp.c.make_zCDP_to_approxDP(dp.m.make_base_discrete_gaussian(100.)), 1e-6)
 
