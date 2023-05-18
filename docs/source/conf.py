7460bc38297d9437a71afcab36b1982799fe707e
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2023-05-18 11:09:09 -0700

    * Temporarily add dry-run branch for docs testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -149,7 +149,7 @@ smv_tag_whitelist = r'^v.*$'
 # smv_tag_whitelist = rf'(^v\d+\.\d+\.\d+$)|(^v{re.escape(version.split("-")[0])}.+$)'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r'(stable|latest)'
+smv_branch_whitelist = r'(stable|latest|dry-run/0\.7\.x)'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r'origin'
