ee0a3bbfba5c6355856f4834ab3a32e234bdeb01
r_fndv <raphael.de-fondeville@epfl.ch>
2023-05-22 17:20:25 +0200

    last commit before pulling polars integration

--- rust/src/transformations/partitioned_sum/mod.rs
+++ rust/src/transformations/partitioned_sum/mod.rs
@@ -12,6 +12,7 @@ use crate::{
     //transformations::{SizedDataFrame},
     transformations::dataframe::SizedDataFrameDomain,
 };
+use crate::domains::{AllDomain, VectorDomain};
 
 // #[cfg(feature = "ffi")]
 // mod ffi;
@@ -31,80 +32,108 @@ use crate::{
 /// 
 /// # Generics
 /// * `TC` - Type of column names.
-pub fn make_sized_partitioned_sum<CA: Hashable>(
+pub fn make_sized_partitioned_sum<TC: Hashable>(
     input_domain: SizedDataFrameDomain,
-    identifier_column: &str,
-    keep_columns: Vec<&str>,
+    partition_column: &str,
+    sum_column: &str,
+    _bounds: (f64, f64), // TO BE CHECKED
     null_partition: bool,
 ) -> Fallible<
     Transformation<
     SizedDataFrameDomain,
-        ProductDomain<SizedDataFrameDomain>,
+    VectorDomain<AllDomain<f64>>,
         SymmetricDistance,
         ProductMetric<SymmetricDistance>,
     >,
 > {
-    if !input_domain.categories_counts.contains_key(&identifier_column){
+    if !input_domain.categories_counts.contains_key(&partition_column){
         return fallible!(FailedFunction, "Data frame domain does not list the desired colunm as categorical variable.")
     }
 
+    let partition_column_name = partition_column.to_string();
+    let sum_column_name = sum_column.to_string();
+
     //Retreive series index
-    let cat_column_index = input_domain.categories_keys.iter().position(|s| s.name() == identifier_column).expect("Domain does not contain partion keys").clone();
+    let cat_column_index = input_domain.categories_keys.iter().position(|s| s.name() == partition_column_name).expect("Domain does not contain partion keys").clone();
 
     // Create vector of partition keys
     let partion_keys: Vec<&str> = input_domain.categories_keys[cat_column_index].utf8().unwrap().into_no_null_iter().collect();
-    let partition_indexes: HashMap<&str, usize> = partion_keys
-                .iter()
-                .cloned()
-                .enumerate()
-                .map(|(i, k)| (k, i))
-                .collect();
-
-
+    
     // Defined output sizes 
     let partion_size = partion_keys.len();
-    let true_partitions = partion_size + 1;
     let output_partitions = partion_size + if null_partition { 1 } else { 0 };
     let d_output_partitions = IntDistance::exact_int_cast(output_partitions)?;
 
     // Create Product<SizedDataFrameDomain>
-    let mut vector_partition_domains = vec![SizedDataFrameDomain::Default(); output_partitions];
-    for i in 0..output_partitions {
-        let mut partition_counts: Vec<usize> = vec![0; partion_size];
-        if i < partion_size {
-        partition_counts[i] = input_domain.categories_counts.get(&identifier_column).unwrap()[i].clone();
-        }
-        vector_partition_domains[i].add_categorical_colunm(&input_domain.categories_keys[cat_column_index].clone(), partition_counts).unwrap();
-    }
-    let product_df_domain = ProductDomain::new(vector_partition_domains);
+    // let mut vector_partition_domains = vec![SizedDataFrameDomain::Default(); output_partitions];
+    // for i in 0..output_partitions {
+    //     let mut partition_counts: Vec<usize> = vec![0; partion_size];
+    //     if i < partion_size {
+    //     partition_counts[i] = input_domain.categories_counts.get(&partition_column_name).unwrap()[i].clone();
+    //     }
+    //     vector_partition_domains[i].add_categorical_colunm(&input_domain.categories_keys[cat_column_index].clone(), partition_counts).unwrap();
+    // }
+    //let product_df_domain = ProductDomain::new(vector_partition_domains);
 
     Ok(Transformation::new(
         input_domain,
-        product_df_domain,
+        VectorDomain::new(AllDomain::new()),
         Function::new_fallible(move |data: &DataFrame| {
             
             // Check if col names exists
-
-            // Turn partition column into categorical
-
-            // Create partition
-            let mut sums = data.clone()
-                        .lazy()
-                        .groupby([&identifier_column.to_string()])
-                        .agg([sum(&sum_column.to_string())])
-                        .collect()?;
-
-
-
-            if !null_partition {
-                partitioned_data.pop(); // Remove last position if no unknown class
+            if !data.get_column_names().contains(&partition_column_name.as_str()) {
+                return fallible!(FailedFunction, "Dataframe does not contains the desired partition colunm.")
+            }
+            if !data.get_column_names().contains(&sum_column_name.as_str()) {
+                return fallible!(FailedFunction, "Dataframe does not contains the column to be summed.")
             }
 
-            Ok(partitioned_data)
+            // Create partition
+            let mut cat_sums = data.clone()
+                        .lazy()
+                        .groupby([partition_column_name.as_str()])
+                        .agg([sum(&sum_column_name.as_str())])
+                        .collect()
+                        .unwrap();
+
+
+            // Remove unknown classes
+            let sums = cat_sums.filter(
+                &cat_sums
+                .column(&partition_column_name.as_str())
+                .unwrap()
+                .is_in(&input_domain.categories_keys[cat_column_index])
+                .unwrap())
+                .unwrap();
+
+            if null_partition {
+                let mask = &cat_sums
+                .column(&partition_column_name.as_str())
+                .unwrap()
+                .is_in(&input_domain.categories_keys[cat_column_index])
+                .unwrap();
+                let unknown_sum: f64 = cat_sums
+                                                .filter(&!mask)
+                                                .unwrap()
+                                                .column("sum")
+                                                .unwrap()
+                                                .sum()
+                                                .unwrap();
+
+                let df_unkown = df!(&partition_column_name.as_str() => &["unknown"],
+                                                "sum" => &[unknown_sum]).unwrap();
+
+                let sums = sums.vstack(&df_unkown).unwrap();
+
+            } 
+
+            let vec_sums: Vec<f64> = sums.column("sum").unwrap().f64().unwrap().into_no_null_iter().collect();
+
+            Ok(vec_sums)
         }),
         SymmetricDistance::default(),
-        ProductMetric::new(SymmetricDistance::default()),
-        StabilityMap::new(move |d_in: &IntDistance| (*d_in, *d_in.min(&d_output_partitions))),
+        ProductMetric::new(SymmetricDistance::default()), // TO DO !!!
+        StabilityMap::new(move |d_in: &IntDistance| (*d_in, *d_in.min(&d_output_partitions))), // TO DO !!!
     ))
 }
 
