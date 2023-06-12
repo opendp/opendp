4336d19b33fcc9b16297df8592a3d05bda6e6a9d
Shoeboxam <shoeboxam@gmail.com>
2023-06-11 23:52:44 -0400

    csv scanner

--- python/src/opendp/_convert.py
+++ python/src/opendp/_convert.py
@@ -25,6 +25,7 @@ ATOM_MAP = {
     'i64': ctypes.c_int64,
     'usize': ctypes.c_size_t,
     'bool': ctypes.c_bool,
+    'char': ctypes.c_char,
     'AnyMeasurementPtr': Measurement,
     'AnyTransformationPtr': Transformation,
 }
@@ -81,6 +82,10 @@ def py_to_c(value: Any, c_type, type_name: Union[RuntimeType, str] = None):
 
         rust_type = str(type_name)
         if rust_type in ATOM_MAP:
+
+            if rust_type == "char":
+                value = value.encode()
+            
             if rust_type in INT_SIZES:
                 check_c_int_cast(value, rust_type)
             return ctypes.byref(ATOM_MAP[rust_type](value))
