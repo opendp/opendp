a617a93ea1e5060561ae40f2c2620bd783df2b65
Shoeboxam <shoeboxam@gmail.com>
2023-06-12 10:51:17 -0400

    Lp metric

--- rust/src/transformations/count/mod.rs
+++ rust/src/transformations/count/mod.rs
@@ -10,7 +10,7 @@ use opendp_derive::bootstrap;
 use crate::core::{Function, Metric, MetricSpace, StabilityMap, Transformation};
 use crate::domains::{AtomDomain, MapDomain, VectorDomain};
 use crate::error::*;
-use crate::metrics::{AbsoluteDistance, LpDistance, SymmetricDistance};
+use crate::metrics::{AbsoluteDistance, SymmetricDistance, Lp};
 use crate::traits::{CollectionSize, Float, Hashable, Number, Primitive};
 
 #[bootstrap(features("contrib"), generics(TO(default = "int")))]
@@ -89,7 +89,7 @@ where
 pub trait CountByCategoriesConstant<QO> {
     fn get_stability_constant() -> QO;
 }
-impl<const P: usize, Q: One> CountByCategoriesConstant<Q> for LpDistance<P, Q> {
+impl<const P: usize, Q: One> CountByCategoriesConstant<Q> for Lp<P, AbsoluteDistance<Q>> {
     fn get_stability_constant() -> Q {
         Q::one()
     }
@@ -186,7 +186,7 @@ where
 pub trait CountByConstant<QO> {
     fn get_stability_constant() -> Fallible<QO>;
 }
-impl<const P: usize, Q: One> CountByConstant<Q> for LpDistance<P, Q> {
+impl<const P: usize, Q: One> CountByConstant<Q> for Lp<P, AbsoluteDistance<Q>> {
     fn get_stability_constant() -> Fallible<Q> {
         if P == 0 {
             return fallible!(MakeTransformation, "P must be positive");
