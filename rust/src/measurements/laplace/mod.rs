a8bc65f6369300925d78ed6587886acbda59cc47
Shoeboxam <shoeboxam@gmail.com>
2023-06-13 14:03:43 -0400

    review comments

--- rust/src/measurements/laplace/mod.rs
+++ rust/src/measurements/laplace/mod.rs
@@ -102,3 +102,28 @@ where
 {
     D::make_laplace(input_domain, input_metric, scale)
 }
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use num::{One, Zero};
+
+    #[test]
+    fn test_all() -> Fallible<()> {
+        macro_rules! test_laplace_with_ty {
+            ($($ty:ty),+) => {$(
+                let meas = make_laplace(AtomDomain::<$ty>::default(), Default::default(), 1.)?;
+                meas.invoke(&<$ty>::zero())?;
+                meas.map(&<$ty>::one())?;
+
+                let meas = make_laplace(VectorDomain::new(AtomDomain::<$ty>::default()), Default::default(), 1.)?;
+                meas.invoke(&vec![<$ty>::zero()])?;
+                meas.map(&<$ty>::one())?;
+            )+}
+        }
+        test_laplace_with_ty!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, f32, f64);
+        Ok(())
+    }
+}
