5659d3d0866d7dc80f549c1e9f23022aa6858a7f
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-06-20 22:52:48 +0200

    bounds and size

--- rust/src/transformations/polars/make_sum/mod.rs
+++ rust/src/transformations/polars/make_sum/mod.rs
@@ -3,20 +3,51 @@ use crate::domains::{DatasetMetric, LazyFrameDomain};
 use crate::metrics::{IntDistance, L1Distance, SymmetricDistance};
 use polars::prelude::*;
 
+/// Polars operator to compute sum of a serie in a LazyFrame
+///
+/// # Arguments
+/// * `input_domain` - ExprDomain
+/// * `input_metric` - The metric space under which neighboring LazyFrames are compared
 pub fn make_sum_expr(
     input_domain: ExprDomain,
     input_metric: SymmetricDistance,
 ) -> Fallible<Transformation<ExprDomain, ExprDomain, SymmetricDistance, L1Distance<f64>>> {
-    // Output domain is the same as input domain (TODO: no ? same lfd, context and active_column)
+    // Output domain has the same series as input domain but QUESTION maybe lose margins ?
     let output_domain = input_domain.clone();
 
-    // For StabilityMap: Compute ideal_sensitivity and relaxation
+    // VERIFY: active_column is the column that was selected with make_col ?
     let active_column = input_domain.clone().active_column;
+
+    // Verify active_column in dataframe (maybe not relevant as already done on make_col)
+    if input_domain
+        .lazy_frame_domain
+        .column(active_column.clone())
+        .is_none()
+    {
+        return fallible!(MakeTransformation, "{} is not in dataframe", col_name);
+    }
+
+    // For StabilityMap: Compute ideal_sensitivity and relaxation //
+    // Verify active_column has margin with counts (to compute size for stability map)
+    let active_column_margin = input_domain
+        .clone()
+        .lazy_frame_domain
+        .margins
+        .into_iter()
+        .filter(|(s, _)| s.contains(active_column.clone()))
+        .collect();
+    let lazy_frame_size = active_column_margin.sum(); // get size of df
+
     // TODO: get bounds from dataframe SeriesDomain of active_column
-    // TODO: get size of df
+    // ASK: Otherwise are the bounds given as argument of the transformation ?
+    let bounds = input_domain
+        .lazy_frame_domain
+        .col(active_column.clone())
+        .bounds();
 
     // Compute ideal sensitivity
-    let (lower, upper) = bounds;
+    let lower = bounds.clone().lower();
+    let upper = bounds.clone().upper();
     let ideal_sensitivity = upper.inf_sub(&lower)?;
 
     // Compute sensitivity correction for bit approximation
@@ -24,11 +55,10 @@ pub fn make_sum_expr(
     let _2 = T::exact_int_cast(2)?;
 
     // Formula is: n^2 / 2^(k - 1) max(|L|, U)
-    let error = size
-        .inf_mul(&size)?
+    let error = lazy_frame_size
+        .inf_mul(&lazy_frame_size)?
         .inf_div(&_2.inf_pow(&mantissa_bits)?)?
         .inf_mul(&lower.alerting_abs()?.total_max(upper)?)?;
-
     let relaxation = error.inf_add(&error)?;
 
     Transformation::new(
@@ -114,31 +144,32 @@ mod test_make_col {
     }
 
     #[test]
-    fn test_make_sum_expr_no_bounds() -> Fallible<()> {
-        // Does not have bounds
-        let frame_domain = LazyFrameDomain::new(vec![SeriesDomain::new(
-            "C",
-            AtomDomain::<String>::default(),
-        )])
-        .unwrap();
+    fn test_make_sum_expr_no_active_column() -> Fallible<()> {
+        // copied from make_col
+        let (expr_domain, _) = get_test_data();
 
-        let expr_domain = ExprDomain {
-            lazy_frame_domain: frame_domain,
-            context: Context::Select,
-            active_column: "C",
-        };
-
-        let lazy_frame = df!(
-            "C" => &["x", "y", "y"],)
-        .unwrap()
-        .lazy();
-
+        // Get resulting ExpressionDomain
         let error_res = make_sum(expr_domain, SymmetricDistance::default())
+            .map(|v| v.input_domain.clone())
             .unwrap_err()
             .variant;
-        let expected_error_kind = ErrorVariant::MakeTransformation; // missing bounds (?)
+        let expected_error_kind = ErrorVariant::MakeTransformation;
 
         assert_eq!(error_res, expected_error_kind);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_make_sum_expr_no_counts() -> Fallible<()> {
+        // TODO: does not have counts (don't know size)
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_make_sum_expr_no_bounds() -> Fallible<()> {
+        // TODO: does not have bounds (don't know snesibility)
 
         Ok(())
     }
