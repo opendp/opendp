c82eebedf6161ebd017c55cb5d2cb4f13383e0ff
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-06-28 13:55:09 +0200

    moving to measuremnt

--- rust/src/transformations/polars/mod.rs
+++ rust/src/transformations/polars/mod.rs
@@ -7,8 +7,3 @@ pub use scan_csv::*;
 mod make_sum;
 #[cfg(feature = "contrib")]
 pub use make_sum::*;
-
-#[cfg(feature = "contrib")]
-mod make_laplace;
-#[cfg(feature = "contrib")]
-pub use make_laplace::*;
