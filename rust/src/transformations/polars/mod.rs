b3531d165829222b3a44364f2756e034fe380b48
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-06-28 11:18:00 +0200

    fic conflict

--- rust/src/transformations/polars/mod.rs
+++ rust/src/transformations/polars/mod.rs
@@ -4,12 +4,11 @@ mod scan_csv;
 pub use scan_csv::*;
 
 #[cfg(feature = "contrib")]
-<<<<<<< HEAD
 mod make_sum;
 #[cfg(feature = "contrib")]
 pub use make_sum::*;
-=======
+
+#[cfg(feature = "contrib")]
 mod make_laplace;
 #[cfg(feature = "contrib")]
 pub use make_laplace::*;
->>>>>>> 58a0cfb3 (first pass taking R code)
