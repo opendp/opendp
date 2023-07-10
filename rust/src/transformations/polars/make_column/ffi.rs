ce6c825da375a028a0d5a1de72bada1d9b0a3d53
Shoeboxam <shoeboxam@gmail.com>
2023-07-10 18:41:57 -0500

    add `make_column` and `make_unpack`

---
+++ rust/src/transformations/polars/make_column/ffi.rs
@@ -0,0 +1,32 @@
+use std::ffi::c_char;
+
+use crate::{
+    core::FfiResult,
+    domains::DataFrameDomain,
+    ffi::{any::{AnyDomain, AnyMetric, AnyTransformation, Downcast, IntoAnyFunctionExt}, util::into_string},
+};
+
+use super::make_column;
+
+#[no_mangle]
+pub extern "C" fn opendp_transformations__make_column(
+    input_domain: *const AnyDomain,
+    input_metric: *const AnyMetric,
+    column_name: *mut c_char
+) -> FfiResult<*mut AnyTransformation> {
+    let input_domain = try_!(try_as_ref!(input_domain).downcast_ref::<DataFrameDomain>()).clone();
+    let input_metric = try_as_ref!(input_metric);
+    let column_name = try_!(into_string(column_name));
+
+    let (input_domain, output_domain, function, input_metric, output_metric, stability_map) =
+        try_!(make_column(input_domain, input_metric.clone(), column_name)).decompose();
+
+    AnyTransformation::new(
+        AnyDomain::new(input_domain),
+        AnyDomain::new(output_domain),
+        function.into_any(),
+        input_metric,
+        output_metric,
+        stability_map
+    ).into()
+}
