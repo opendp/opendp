1dc927b9d423ab10a85347849f9eb46c76dd2a1a
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2023-07-11 16:50:31 -0700

    Restore remote whitelist

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -155,7 +155,7 @@ smv_tag_whitelist = r'^v.*$'
 smv_branch_whitelist = r'(stable|beta|nightly)'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = None  # None to prevent duplicates if you have branches locally
+smv_remote_whitelist = r'origin'
 
 # Pattern for released versions
 smv_released_pattern = r'^tags/v\d+\.\d+\.\d+$'
