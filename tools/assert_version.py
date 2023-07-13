08704594c49709d84385bb72c82eac0091954543
Andrew Vyrros <6577271+andrewvyrros@users.noreply.github.com>
2023-07-12 17:42:23 -0700

    Factor out utils

--- tools/assert_version.py
+++ tools/assert_version.py
@@ -1,73 +1,38 @@
 import configparser
-import semver
-import toml
+import tomlkit
+
+from utils import *
 
 # all version numbers should be:
-version = open("VERSION", 'r').read().strip()
-version = semver.Version.parse(version)
-assert version.prerelease != "dev", "Please update the repository with the current version."
-print("Checking if all version numbers are synchronized at", version)
-
-# From channel_tool.py
-def get_python_version(version):
-    # Python (PEP 440) has several annoying quirks that make it not quite compatible with semantic versioning:
-    # 1. Python doesn't allow arbitrary tags, only (a|b|rc|post|dev). (You can use (alpha|beta|c|pre|preview|rev|r),
-    #    but they'll be mapped to (a|b|rc|rc|rc|post|post) respectively.)
-    #    So "1.2.3-nightly.456" will fail, and "1.2.3-alpha.456" gets mapped to "1.2.3a456" (see #2).
-    # 2. Python doesn't allow separators between the main version and the tag, nor within the tag.
-    #    So "1.2.3-a.456" gets mapped to "1.2.3a456"
-    # 3. HOWEVER, Python treats tags "post" and "dev" differently, and in these cases uses a "." separator between
-    #    the main version and the tag (but still doesn't allow separators within the tag).
-    #    So "1.2.3-dev.456" gets mapped to "1.2.3.dev456".
-    # 4. Python requires that all tags have a numeric suffix, and will assume 0 if none is present.
-    #    So "1.2.3-dev" gets mapped to "1.2.3.dev0" (by #3 & #4).
-    # We don't use all these variations, only (dev|nightly|beta), but if that ever changes, hopefully we won't
-    # have to look at this whole mess again.
-    tag_to_py_tag = {
-        "nightly": "a",
-        "beta": "b",
-        "c": "rc",
-        "pre": "rc",
-        "preview": "rc",
-        "rev": "post",
-        "r": "post",
-    }
-    if version.prerelease is not None:
-        split = version.prerelease.split(".", 2)
-        tag = split[0]
-        py_tag = tag_to_py_tag.get(tag, tag)
-        date = split[1] if len(split) >= 2 else None
-        counter = split[2] if len(split) >= 3 else None
-        py_n = f"{date}{counter:>03}" if date and counter else (date if date else "0")
-        py_separator = "." if py_tag in ("post", "dev") else ""
-    else:
-        py_tag = None
-        py_n = None
-        py_separator = None
-    # semver can't represent the rendered Python version, so we generate a string.
-    if py_tag is not None:
-        return f"{version.major}.{version.minor}.{version.patch}{py_separator}{py_tag}{py_n}"
-    else:
-        return str(version)
-
+version = get_version()
 python_version = get_python_version(version)
-config = configparser.RawConfigParser()
-config.read('python/setup.cfg')
-assert config['metadata']['version'] == str(python_version), \
-    "python/setup.cfg package version is incorrect"
+
+assert "dev" not in version.prerelease, "Please configure the channel with a non-dev version."
+print("Checking if all version numbers are synchronized at", version, python_version)
 
 # check that opendp crate version is set properly
 # cargo doesn't like build metadata in dependency references, so we strip that for those.
 stripped_version = version.replace(build=None)
-opendp_toml = toml.load('rust/Cargo.toml')
+opendp_toml = tomlkit.load(open('rust/Cargo.toml'))
 assert opendp_toml['workspace']['package']['version'] == version, \
     "rust/Cargo.toml workspace version is incorrect"
 assert opendp_toml['dependencies']['opendp_derive']['version'] == version, \
     "rust/Cargo.toml dependency opendp_derive version is incorrect"
 assert opendp_toml['build-dependencies']['opendp_tooling']['version'] == version, \
     "rust/Cargo.toml build-dependency opendp_tooling version is incorrect"
-opendp_derive_toml = toml.load('rust/opendp_derive/Cargo.toml')
+opendp_derive_toml = tomlkit.load(open('rust/opendp_derive/Cargo.toml'))
 assert opendp_derive_toml['dependencies']['opendp_tooling']['version'] == version, \
     "rust/Cargo.toml dependency opendp_derive version is incorrect"
+
+config = configparser.RawConfigParser()
+config.read('python/setup.cfg')
+assert config['metadata']['version'] == str(python_version), \
+    "python/setup.cfg package version is incorrect"
+
+binder_requirements = open('.binder/requirements.txt').readlines()
+for line in binder_requirements:
+    if line.startswith("opendp=="):
+        assert line == f"opendp=={python_version}\n", \
+            ".binder/requirements.txt opendp dependency is incorrect"
 
 print("All version numbers are synchronized")
--- tools/channel_tool.py
+++ tools/channel_tool.py
@@ -3,81 +3,11 @@ import configparser
 import datetime
 import io
 import re
-import subprocess
-import sys
-import time
 import zoneinfo
 
-import semver
 import tomlkit
 
-
-def log(message, command=False):
-    prefix = "$" if command else "#"
-    print(f"{prefix} {message}", file=sys.stderr)
-
-
-def run_command(description, cmd, capture_output=False, shell=True):
-    if description:
-        log(description)
-    printed_cmd = " ".join(cmd) if type(cmd) == list else cmd
-    log(printed_cmd, command=True)
-    stdout = subprocess.PIPE if capture_output else None
-    completed_process = subprocess.run(cmd, stdout=stdout, shell=shell, check=True, encoding="utf-8")
-    return completed_process.stdout.rstrip() if capture_output else None
-
-
-def get_version(version_str=None):
-    if not version_str:
-        with open("VERSION") as f:
-            version_str = f.read().strip()
-    return semver.Version.parse(version_str)
-
-
-def get_python_version(version):
-    # Python (PEP 440) has several annoying quirks that make it not quite compatible with semantic versioning:
-    # 1. Python doesn't allow arbitrary tags, only (a|b|rc|post|dev). (You can use (alpha|beta|c|pre|preview|rev|r),
-    #    but they'll be mapped to (a|b|rc|rc|rc|post|post) respectively.)
-    #    So "1.2.3-nightly.456" will fail, and "1.2.3-alpha.456" gets mapped to "1.2.3a456" (see #2).
-    # 2. Python doesn't allow separators between the main version and the tag, nor within the tag.
-    #    So "1.2.3-a.456" gets mapped to "1.2.3a456"
-    # 3. HOWEVER, Python treats tags "post" and "dev" differently, and in these cases uses a "." separator between
-    #    the main version and the tag (but still doesn't allow separators within the tag).
-    #    So "1.2.3-dev.456" gets mapped to "1.2.3.dev456".
-    # 4. Python requires that all tags have a numeric suffix, and will assume 0 if none is present.
-    #    So "1.2.3-dev" gets mapped to "1.2.3.dev0" (by #3 & #4).
-    # We don't use all these variations, only (dev|nightly|beta), but if that ever changes, hopefully we won't
-    # have to look at this whole mess again.
-    tag_to_py_tag = {
-        "nightly": "a",
-        "beta": "b",
-        "c": "rc",
-        "pre": "rc",
-        "preview": "rc",
-        "rev": "post",
-        "r": "post",
-    }
-    if version.prerelease is not None:
-        split = version.prerelease.split(".", 2)
-        tag = split[0]
-        py_tag = tag_to_py_tag.get(tag, tag)
-        date = split[1] if len(split) >= 2 else None
-        counter = split[2] if len(split) >= 3 else None
-        py_n = f"{date}{counter:>03}" if date and counter else (date if date else "0")
-        py_separator = "." if py_tag in ("post", "dev") else ""
-    else:
-        py_tag = None
-        py_n = None
-        py_separator = None
-    # semver can't represent the rendered Python version, so we generate a string.
-    if py_tag is not None:
-        return f"{version.major}.{version.minor}.{version.patch}{py_separator}{py_tag}{py_n}"
-    else:
-        return str(version)
-
-
-def get_current_branch():
-    return run_command(f"Determining current branch", "git branch --show-current", capture_output=True)
+from utils import *
 
 
 def ensure_branch(branch):
@@ -123,16 +53,6 @@ def date(args):
     print(date.isoformat())
 
 
-def update_file(path, load, munge, dump, binary=False):
-    log(f"Updating {path}")
-    b = "b" if binary else ""
-    with open(path, f"r{b}") as f:
-        data = load(f)
-    new_data = munge(data)
-    with open(path, f"w{b}") as f:
-        dump(new_data, f)
-
-
 def infer_counter(version, date, args):
     if args.counter:
         return args.counter
@@ -146,6 +66,16 @@ def infer_counter(version, date, args):
     if not version_date == date.strftime('%Y%m%d'):
         return 1
     return int(version_counter) + 1
+
+
+def update_file(path, load, munge, dump, binary=False):
+    log(f"Updating {path}")
+    b = "b" if binary else ""
+    with open(path, f"r{b}") as f:
+        data = load(f)
+    new_data = munge(data)
+    with open(path, f"w{b}") as f:
+        dump(new_data, f)
 
 
 def update_version(version):
@@ -207,15 +137,6 @@ def configure(args):
         # stable has no tag
         version = version.finalize_version()
     update_version(version)
-
-
-def infer_channel(version):
-    if version.prerelease is None:
-        return "stable"
-    channel = version.prerelease.split(".", 1)[0]
-    if channel not in ("dev", "nightly", "beta"):
-        raise Exception(f"Unable to infer channel from version {version}")
-    return channel
 
 
 def first_match(lines, pattern):
