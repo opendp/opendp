4837ef533423f3ad8e3e5d86fec6e6e6c1dd58e4
Shoeboxam <shoeboxam@gmail.com>
2023-07-19 10:16:40 -0400

    update tests

--- python/test/integration/test_quantile.py
+++ python/test/integration/test_quantile.py
@@ -1,22 +1,19 @@
 from opendp.mod import enable_features
+import opendp.prelude as dp
 
 enable_features("floating-point", "contrib")
 
 
 def test_quantile_score_candidates():
-    from opendp.transformations import make_quantile_score_candidates
-    from opendp.measurements import part_base_discrete_exponential
-    from opendp.domains import vector_domain, atom_domain
-    from opendp.metrics import symmetric_distance
 
-    input_domain = vector_domain(atom_domain(T=int))
-    input_metric = symmetric_distance()
+    input_domain = dp.vector_domain(dp.atom_domain(T=int))
+    input_metric = dp.symmetric_distance()
     candidates = [20, 33, 40, 50, 72, 100]
-    quant_trans = make_quantile_score_candidates(input_domain, input_metric, candidates, alpha=0.5)
+    quant_trans = dp.t.make_quantile_score_candidates(input_domain, input_metric, candidates, alpha=0.5)
 
     print(quant_trans(list(range(100))))
 
-    expo_meas = part_base_discrete_exponential(1000., "min")
+    expo_meas = dp.m.then_base_discrete_exponential(1000., "min")
 
     quantile_meas = quant_trans >> expo_meas
     idx = quantile_meas(list(range(100)))
