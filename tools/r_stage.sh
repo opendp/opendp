f575be09fe02f9d0ccabed75f6e20eae62793fc7
Shoeboxam <shoeboxam@gmail.com>
2023-08-25 01:11:48 -0400

    golf r_stage

--- tools/r_stage.sh
+++ tools/r_stage.sh
@@ -13,25 +13,6 @@ function usage() {
   echo "Usage: $(basename "$0") [-c]" >&2
 }
 
-CLEAN=false
-DOCS=false
-VENDOR=false
-SOURCE=false
-AUTHORS=false
-while getopts ":cdvsa" OPT; do
-  case "$OPT" in
-  c) CLEAN=true ;;
-  d) DOCS=true ;;
-  v) VENDOR=true ;;
-  s) SOURCE=true ;;
-  a) authors=true ;;
-  *) usage && exit 1 ;;
-  esac
-done
-
-shift $((OPTIND - 1))
-if (($# != 0)); then usage && exit 1; fi
-
 function log() {
   local FORMAT="$1"
   shift
@@ -47,7 +28,8 @@ function run() {
 }
 
 function clean() {
-  log "Clean staging files"
+  log "***** CLEAN *****"
+
   run rm -f R/opendp/src/libopendp.a
   if [ -f "R/opendp/src/rust/Cargo.toml" ]; then
     run cargo clean --manifest-path R/opendp/src/rust/Cargo.toml
@@ -68,6 +50,8 @@ function clean() {
 
 function docs() {
   clean
+
+  log "***** DOCS *****"
   # We don't directly expose any APIs from compiled code, 
   # so we don't actually have to build the binary in order to build docs.
   # To avoid the overhead of building the binary, 
@@ -98,24 +82,8 @@ function docs() {
   log "R package docs are ready in R/opendp/docs/index.html"
 }
 
-function vendor_tar() {
-  log "Vendor dependencies"
-  run cargo vendor --manifest-path rust/Cargo.toml
-
-  log "Tar dependencies into: R/opendp/src/vendor.tar.xz"
-  tar --create --xz --no-xattrs --file=R/opendp/src/vendor.tar.xz vendor
-}
-
-function source_tar() {
-  log "Tar lib sources into:  R/opendp/src/source.tar.xz"
-  mkdir -p R/opendp/src/rust
-  [ -d rust/target ] && mv rust/target target
-  # tar everything because R CMD build ignores arbitrary file patterns like .*old (like threshold...)
-  tar --create --xz --no-xattrs --file=R/opendp/src/source.tar.xz rust
-  [ -d target ] && mv target rust/target
-}
-
 function binary_tar() {
+  log "***** BINARY *****"
   if [[ -f "rust/target/debug/libopendp.a" ]]; then
     log "    Detected debug library, using it to simulate precompiled binaries"
     mkdir -p binary/$(uname -m)/
@@ -128,7 +96,27 @@ function binary_tar() {
   fi
 }
 
+function source_tar() {
+  log "***** SOURCE *****"
+  log "Tar lib sources into:  R/opendp/src/source.tar.xz"
+  mkdir -p R/opendp/src/rust
+  [ -d rust/target ] && mv rust/target target
+  # tar everything because R CMD build ignores arbitrary file patterns like .*old (like threshold...)
+  tar --create --xz --no-xattrs --file=R/opendp/src/source.tar.xz rust
+  [ -d target ] && mv target rust/target
+}
+
+function vendor_tar() {
+  log "***** VENDOR *****"
+  log "Vendor dependencies"
+  run cargo vendor --manifest-path rust/Cargo.toml
+
+  log "Tar dependencies into: R/opendp/src/vendor.tar.xz"
+  tar --create --xz --no-xattrs --file=R/opendp/src/vendor.tar.xz vendor
+}
+
 function notes() {
+  log "***** NOTES *****"
   if [[ ! -d "vendor" ]] && [[ -f "R/opendp/src/vendor.tar.xz" ]]; then
     mkdir vendor
     tar --extract --xz -f R/opendp/src/vendor.tar.xz -C ./vendor
@@ -138,25 +126,28 @@ function notes() {
   run Rscript tools/update_notes.R
 }
 
-if [[ $CLEAN == true ]]; then
-  log "***** CLEAN *****"
+if (($# == 0)); then
   clean
-elif [[ $DOCS == true ]]; then
-  log "***** DOCS *****"
-  docs
-elif [[ $VENDOR == true ]]; then
-  log "***** VENDOR *****"
-  vendor_tar
-elif [[ $SOURCE == true ]]; then
-  log "***** SOURCE *****"
-  source_tar
-else
-  log "***** STAGE *****"
-  clean
-  vendor_tar
-  source_tar
   binary_tar
+  source_tar
+  vendor_tar
   notes
 
   echo "R package is staged. Run R CMD build R/opendp to build the package."
+  exit 0
 fi
+
+while getopts ":cdbsvna" OPT; do
+  case "$OPT" in
+  c) clean ;;
+  d) docs ;;
+  b) binary_tar ;;
+  s) source_tar ;;
+  v) vendor_tar ;;
+  n) notes ;;
+  *) usage && exit 1 ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+if (($# != 0)); then usage && exit 1; fi
