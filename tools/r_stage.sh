0e39304affb5e21e10f0a55ed5403a9319cc1956
Shoeboxam <shoeboxam@gmail.com>
2023-08-25 01:35:20 -0400

    try to fix early return

--- tools/r_stage.sh
+++ tools/r_stage.sh
@@ -48,6 +48,50 @@ function clean() {
   rm -rf R/opendp/docs
 }
 
+function binary_tar() {
+  log "***** BINARY *****"
+  if [[ -f "rust/target/debug/libopendp.a" ]]; then
+    log "    Detected debug library, using it to simulate precompiled binaries"
+    mkdir -p binary/$(uname -m)/
+    cp rust/target/debug/libopendp.a binary/$(uname -m)/
+  fi
+
+  if [[ -d "binary" ]]; then
+    log "Tar binaries into:     R/opendp/src/binary.tar.xz"
+    tar --create --xz --no-xattrs --file=R/opendp/src/binary.tar.xz binary
+  fi
+}
+
+function source_tar() {
+  log "***** SOURCE *****"
+  log "Tar lib sources into:  R/opendp/src/source.tar.xz"
+
+  [ -d "rust/target" ] && mv rust/target target
+  # tar everything because R CMD build ignores arbitrary file patterns like .*old (like threshold...)
+  tar --create --xz --no-xattrs --file=R/opendp/src/source.tar.xz rust
+  [ -d "target" ] && mv target rust/target
+}
+
+function vendor_tar() {
+  log "***** VENDOR *****"
+  log "Vendor dependencies"
+  run cargo vendor --manifest-path rust/Cargo.toml
+
+  log "Tar dependencies into: R/opendp/src/vendor.tar.xz"
+  tar --create --xz --no-xattrs --file=R/opendp/src/vendor.tar.xz vendor
+}
+
+function notes() {
+  log "***** NOTES *****"
+  if [[ ! -d "vendor" ]] && [[ -f "R/opendp/src/vendor.tar.xz" ]]; then
+    mkdir vendor
+    tar --extract --xz -f R/opendp/src/vendor.tar.xz -C ./vendor
+  fi
+
+  log "Prepare inst/AUTHORS and LICENSE.note"
+  run Rscript tools/update_notes.R
+}
+
 function docs() {
   clean
 
@@ -82,49 +126,6 @@ function docs() {
   log "R package docs are ready in R/opendp/docs/index.html"
 }
 
-function binary_tar() {
-  log "***** BINARY *****"
-  if [[ -f "rust/target/debug/libopendp.a" ]]; then
-    log "    Detected debug library, using it to simulate precompiled binaries"
-    mkdir -p binary/$(uname -m)/
-    cp rust/target/debug/libopendp.a binary/$(uname -m)/
-  fi
-
-  if [[ -d "binary" ]]; then
-    log "Tar binaries into:     R/opendp/src/binary.tar.xz"
-    tar --create --xz --no-xattrs --file=R/opendp/src/binary.tar.xz binary
-  fi
-}
-
-function source_tar() {
-  log "***** SOURCE *****"
-  log "Tar lib sources into:  R/opendp/src/source.tar.xz"
-  [[ -d "rust/target" ]] && mv rust/target target
-  # tar everything because R CMD build ignores arbitrary file patterns like .*old (like threshold...)
-  tar --create --xz --no-xattrs --file=R/opendp/src/source.tar.xz rust
-  [[ -d "target" ]] && mv target rust/target
-}
-
-function vendor_tar() {
-  log "***** VENDOR *****"
-  log "Vendor dependencies"
-  run cargo vendor --manifest-path rust/Cargo.toml
-
-  log "Tar dependencies into: R/opendp/src/vendor.tar.xz"
-  tar --create --xz --no-xattrs --file=R/opendp/src/vendor.tar.xz vendor
-}
-
-function notes() {
-  log "***** NOTES *****"
-  if [[ ! -d "vendor" ]] && [[ -f "R/opendp/src/vendor.tar.xz" ]]; then
-    mkdir vendor
-    tar --extract --xz -f R/opendp/src/vendor.tar.xz -C ./vendor
-  fi
-
-  log "Prepare inst/AUTHORS and LICENSE.note"
-  run Rscript tools/update_notes.R
-}
-
 if (($# == 0)); then
   clean
   binary_tar
@@ -136,14 +137,14 @@ if (($# == 0)); then
   exit 0
 fi
 
-while getopts ":dcbsvna" OPT; do
+while getopts ":cbsvnd" OPT; do
   case "$OPT" in
-    d) docs ;;
     c) clean ;;
     b) binary_tar ;;
     s) source_tar ;;
     v) vendor_tar ;;
     n) notes ;;
+    d) docs ;;
     *) usage && exit 1 ;;
   esac
 done
