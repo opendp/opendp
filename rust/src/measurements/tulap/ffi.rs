e9deec78393cb71066a190094291d2fc1c41b1d3
Shoeboxam <shoeboxam@gmail.com>
2024-01-24 18:20:08 -0500

    work with Aishwarya

--- rust/src/measurements/tulap/ffi.rs
+++ rust/src/measurements/tulap/ffi.rs
@@ -1,10 +1,10 @@
 use std::ffi::c_double;
 
 use crate::core::{FfiResult, IntoAnyMeasurementFfiResultExt};
-use crate::domains::{AtomDomain, VectorDomain};
+use crate::domains::AtomDomain;
 use crate::err;
 use crate::ffi::any::{AnyDomain, AnyMeasurement, AnyMetric, Downcast};
-use crate::metrics::{AbsoluteDistance, PartitionDistance};
+use crate::metrics::AbsoluteDistance;
 
 use super::make_tulap;
 
@@ -15,10 +15,8 @@ pub extern "C" fn opendp_measurements__make_tulap(
     epsilon: c_double,
     delta: c_double,
 ) -> FfiResult<*mut AnyMeasurement> {
-    let input_domain =
-        try_!(try_as_ref!(input_domain).downcast_ref::<VectorDomain<AtomDomain<f64>>>());
-    let input_metric =
-        try_!(try_as_ref!(input_metric).downcast_ref::<PartitionDistance<AbsoluteDistance<f64>>>());
+    let input_domain = try_!(try_as_ref!(input_domain).downcast_ref::<AtomDomain<f64>>());
+    let input_metric = try_!(try_as_ref!(input_metric).downcast_ref::<AbsoluteDistance<f64>>());
     make_tulap(input_domain.clone(), input_metric.clone(), epsilon, delta)
         .into_any()
         .into()
