bbae684af3ac7ff65de7f50fc32c7497b21df095
mccalluc <mccalluc@users.noreply.github.com>
2024-01-30 08:35:09 -0500

    fix links

--- python/src/opendp/mod.py
+++ python/src/opendp/mod.py
@@ -1,6 +1,6 @@
 '''
 The ``mod`` module provides the classes which implement the
-`OpenDP Programming Framework <../../user/programming-framework/index.html>`_,
+`OpenDP Programming Framework <../../api/user-guide/programming-framework/index.html>`_,
 as well as utilities for enabling features and finding parameter values.
 
 The classes here correspond to other top-level modules: For example,
@@ -22,7 +22,7 @@ class Measurement(ctypes.POINTER(AnyMeasurement)): # type: ignore[misc]
     The function releases a differentially-private release.
     The privacy relation maps from an input metric to an output measure.
 
-    See the `Measurement <../../user/programming-framework/core-structures.html#measurement>`_
+    See the `Measurement <../../api/user-guide/programming-framework/core-structures.html#measurement>`_
     section in the Programming Framework docs for more context.
 
     Functions for creating measurements are in :py:mod:`opendp.measurements`.
@@ -196,7 +196,7 @@ class Transformation(ctypes.POINTER(AnyTransformation)): # type: ignore[misc]
     The function maps from an input domain to an output domain.
     The stability relation maps from an input metric to an output metric.
 
-    See the `Transformation <../../user/programming-framework/core-structures.html#transformation>`_
+    See the `Transformation <../../api/user-guide/programming-framework/core-structures.html#transformation>`_
     section in the Programming Framework docs for more context.
 
     Functions for creating transformations are in :py:mod:`opendp.transformations`.
@@ -421,7 +421,7 @@ class Queryable(object):
 
 class Function(ctypes.POINTER(AnyFunction)): # type: ignore[misc]
     '''
-    See the `Function <../../user/programming-framework/supporting-elements.html#function>`_
+    See the `Function <../../api/user-guide/programming-framework/supporting-elements.html#function>`_
     section in the Programming Framework docs for more context.
     '''
     _type_ = AnyFunction
@@ -445,7 +445,7 @@ class Function(ctypes.POINTER(AnyFunction)): # type: ignore[misc]
 
 class Domain(ctypes.POINTER(AnyDomain)): # type: ignore[misc]
     '''
-    See the `Domain <../../user/programming-framework/supporting-elements.html#domain>`_
+    See the `Domain <../../api/user-guide/programming-framework/supporting-elements.html#domain>`_
     section in the Programming Framework docs for more context.
 
     Functions for creating domains are in :py:mod:`opendp.domains`.
@@ -494,7 +494,7 @@ class Domain(ctypes.POINTER(AnyDomain)): # type: ignore[misc]
 
 class Metric(ctypes.POINTER(AnyMetric)): # type: ignore[misc]
     '''
-    See the `Metric <../../user/programming-framework/supporting-elements.html#metric>`_
+    See the `Metric <../../api/user-guide/programming-framework/supporting-elements.html#metric>`_
     section in the Programming Framework docs for more context.
 
     Functions for creating metrics are in :py:mod:`opendp.metrics`.
@@ -534,7 +534,7 @@ class Metric(ctypes.POINTER(AnyMetric)): # type: ignore[misc]
 
 class Measure(ctypes.POINTER(AnyMeasure)): # type: ignore[misc]
     '''
-    See the `Measure <../../user/programming-framework/supporting-elements.html#measure>`_
+    See the `Measure <../../api/user-guide/programming-framework/supporting-elements.html#measure>`_
     section in the Programming Framework docs for more context.
 
     Functions for creating measures are in :py:mod:`opendp.measures`.
