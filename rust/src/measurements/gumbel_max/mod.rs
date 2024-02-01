dae238603d9f3fd75a1465e671246eb1037b89e1
Michael Shoemate <shoeboxam@gmail.com>
2024-02-01 13:50:50 -0500

    remove floating-point flag from RNM Gumbel (#1195)

--- rust/src/measurements/gumbel_max/mod.rs
+++ rust/src/measurements/gumbel_max/mod.rs
@@ -25,7 +25,7 @@ pub enum Optimize {
 }
 
 #[bootstrap(
-    features("contrib", "floating-point"),
+    features("contrib"),
     arguments(optimize(c_type = "char *", rust_type = "String")),
     generics(TIA(suppress))
 )]
