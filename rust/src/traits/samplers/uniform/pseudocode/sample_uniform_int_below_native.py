ed7d30b5d058e7673cda7f66d06ba50e681c42d9
Chuck McCallum <mccalluc@users.noreply.github.com>
2024-02-06 12:18:19 -0500

    fix typos and run flake8 on pseudocode (#1208)
    * fix typos and run flake8 on pseudocode
    
    * fix names in pseudocode

--- rust/src/traits/samplers/uniform/pseudocode/sample_uniform_int_below_native.py
+++ rust/src/traits/samplers/uniform/pseudocode/sample_uniform_int_below_native.py
@@ -2,6 +2,6 @@
 # returns a single bit with some probability of success
 def sample_uniform_int_below(upper, T) -> int:
     while True:
-        sample = T.sample_uniform_int():
+        sample = T.sample_uniform_int()
         if sample < T.MAX - T.MAX % upper:
             return sample % upper
