184271cda1563344a08c3fd9d11570ac2928406f
mccalluc <mccalluc@users.noreply.github.com>
2024-02-09 15:19:14 -0500

    Run all the R files in docs/source; exit on first failure

--- docs/source/quickstart.R
+++ docs/source/quickstart.R
@@ -4,4 +4,4 @@ enable_features("contrib")
 # 2-use
 space <- c(atom_domain(.T = "f64"), absolute_distance(.T = "f64"))
 base_laplace <- space |> then_base_laplace(1.)
-dp_agg <- meas(arg = 23.4)
+dp_agg <- base_laplace(arg = 23.4)
