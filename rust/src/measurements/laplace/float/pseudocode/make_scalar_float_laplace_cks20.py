d90f6cec4bd4b043a22d782fc60a5d5fcf5c3802
Shoeboxam <shoeboxam@gmail.com>
2024-02-26 15:08:28 -0500

    prove float Laplace mechanisms

---
+++ rust/src/measurements/laplace/float/pseudocode/make_scalar_float_laplace_cks20.py
@@ -0,0 +1,22 @@
+# type: ignore
+def make_scalar_float_laplace_cks20(input_domain, input_metric, scale: QO, k):
+    if scale.is_sign_negative():
+        raise ValueError("scale must not be negative")
+    
+    k, relaxation = get_discretization_consts(k)
+
+    if scale.is_zero():
+        def function(x: T):
+            return x
+    else:
+        def function(x: T):
+            return sample_discrete_laplace_Z2k(x, scale, k)
+    
+    return Measurement(
+        input_domain,
+        function,
+        input_metric,
+        MaxDivergence(QO),
+        privacy_map=laplace_map(scale, relaxation=relaxation)
+    )
+        
