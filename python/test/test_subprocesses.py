7bb1c95f228eba1b948669569e3797118d13e521
Chuck McCallum <mccallucc@gmail.com>
2024-03-11 11:45:44 -0400

    move type checking

--- python/test/test_subprocesses.py
+++ python/test/test_subprocesses.py
@@ -1,9 +1,11 @@
 import subprocess
 import pytest
 
+tests = {
+    'linting': 'cd .. && flake8 . --count --show-source --statistics',
+    'type checking': 'mypy .'
+}
 
-@pytest.mark.parametrize("cmd", [
-    'cd .. && flake8 . --count --show-source --statistics'
-])
+@pytest.mark.parametrize("cmd", tests.values(), ids=tests.keys())
 def test_subprocess(cmd):
     subprocess.run(cmd, shell=True, check=True)
