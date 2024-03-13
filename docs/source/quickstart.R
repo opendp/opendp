da73eebe3b5ee04d11f53adfe38bb663d17111e7
Shoeboxam <shoeboxam@gmail.com>
2024-03-13 12:16:05 -0400

    these files had been (re)moved

--- docs/source/quickstart.R
+++
@@ -1,7 +0,0 @@
-# 1-library
-library(opendp)
-enable_features("contrib")
-# 2-use
-space <- c(atom_domain(.T = "f64"), absolute_distance(.T = "f64"))
-laplace_mechanism <- space |> then_laplace(scale = 1.)
-dp_agg <- laplace_mechanism(arg = 23.4)
