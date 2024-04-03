ce8f4cf9b56f20a1b5eda71fdce6c713c5e8feb4
Chuck McCallum <mccallucc@gmail.com>
2024-04-03 17:14:55 -0400

    more re-exports

--- python/src/opendp/extras/__init__.py
+++ python/src/opendp/extras/__init__.py
@@ -1,4 +1,7 @@
 '''
 The ``extras`` module is a namespace for functionality that requires extra, optional Python dependencies.
 '''
-from opendp.extras.domains import np_array2_domain
+from .domains import np_array2_domain
+from .make_np_clamp import make_np_clamp
+from .make_np_pca import make_private_np_pca, make_private_np_mean
+from .sklearn import PCA
