5a02fa923b082473843aaa73eabe3b2b2f4d8659
Chuck McCallum <mccallucc@gmail.com>
2024-04-23 15:19:54 -0400

    move out error test

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -194,25 +194,3 @@ impl<T, E: Debug> ExplainUnwrap for Result<T, E> {
         self.unwrap()
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_error_from_conversion_error() {
-        let e: Error = ConversionError::OutOfBounds.into();
-        assert_eq!(e.variant, ErrorVariant::FailedCast);
-    }
-
-    #[cfg(feature = "polars")]
-    #[test]
-    fn test_error_from_polars_error() {
-        let e: Error = PolarsError::ColumnNotFound("A".into()).into();
-        assert_eq!(e.variant, ErrorVariant::FailedFunction);
-        assert_eq!(
-            e.message,
-            Some("ColumnNotFound(ErrString(\"A\"))".to_string())
-        );
-    }
-}
