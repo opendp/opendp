4155879788d28612ee4e43dd50cd1c17e45e66b2
Pauline Maury-Laribière <36953262+PaulineMauryL@users.noreply.github.com>
2024-04-26 21:28:34 +0200

    Polars: add `make_expr_sum` (#819)
    * add make_sum_expr constructor
    
    * respond to review
    
    ---------
    
    Co-authored-by: Ania Banaszak <banaszak.ab@gmail.com>
    Co-authored-by: Shoeboxam <shoeboxam@gmail.com>

--- rust/src/transformations/make_stable_expr/ffi.rs
+++ rust/src/transformations/make_stable_expr/ffi.rs
@@ -4,7 +4,7 @@ use crate::{
     core::{FfiResult, IntoAnyTransformationFfiResultExt},
     domains::ExprDomain,
     ffi::any::{AnyDomain, AnyMetric, AnyObject, AnyTransformation, Downcast},
-    metrics::{PartitionDistance, SymmetricDistance},
+    metrics::{L1Distance, PartitionDistance, SymmetricDistance},
 };
 
 use super::make_stable_expr;
@@ -21,7 +21,8 @@ pub extern "C" fn opendp_transformations__make_stable_expr(
             .clone();
 
     let expr = try_!(try_as_ref!(expr).downcast_ref::<Expr>()).clone();
-    make_stable_expr(input_domain, input_metric, expr)
+    // TODO: dispatch to different output types
+    make_stable_expr::<_, L1Distance<f64>>(input_domain, input_metric, expr)
         .into_any()
         .into()
 }
