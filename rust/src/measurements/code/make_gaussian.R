c431745a74f75e75d0f5f5e8532d23c8b647986b
Chuck McCallum <mccallucc@gmail.com>
2024-05-01 11:09:10 -0400

    unindent

--- rust/src/measurements/code/make_gaussian.R
+++ rust/src/measurements/code/make_gaussian.R
@@ -1,9 +1,9 @@
 library(opendp)
 enable_features("contrib")
 gaussian <- make_gaussian(
-                          atom_domain(.T = f64),
-                          absolute_distance(.T = f64),
-                          scale = 1.0)
+  atom_domain(.T = f64),
+  absolute_distance(.T = f64),
+  scale = 1.0)
 gaussian(arg = 100.0)
 
 # Alternatively:
