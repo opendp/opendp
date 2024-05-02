02aa0530d67766f6c10c9f2d7845d8f9e6674b42
Chuck McCallum <mccallucc@gmail.com>
2024-05-02 17:18:44 -0400

    simplified

--- rust/src/measurements/code/make_private_lazyframe.py
+++ rust/src/measurements/code/make_private_lazyframe.py
@@ -2,27 +2,42 @@ import opendp.prelude as dp
 import polars as pl
 dp.enable_features("contrib")
 
+
 lf_domain = dp.lazyframe_domain([
-    dp.series_domain("grades", dp.atom_domain(T=dp.f64)),
-    dp.series_domain("classes", dp.atom_domain(T=dp.i32))])
-lf = pl.LazyFrame([
-    pl.Series("grades", [1.0] * 50, dtype=pl.Float64),
-    pl.Series("classes", [1, 2, 3, 4, 5] * 10, dtype=pl.Int32)])
+    dp.series_domain("pet_count", dp.atom_domain(T=dp.i32)),
+    dp.series_domain("grade", dp.atom_domain(T=dp.i32))])
 
-lf_domain = dp.with_margin(lf_domain, by=["classes"], public_info="keys", max_partition_length=50)
+lf_domain_with_margin = dp.with_margin(
+    lf_domain,
+    by=["grade"],
+    public_info="keys",
+    max_partition_length=50)
 
+plan = (
+    pl.LazyFrame(schema={'pet_count': pl.Int32, 'grade': pl.Int32})
+    .group_by("grade")
+    .agg(pl.col("pet_count").dp.sum((0, 10), scale=1.0)))
 
-expr = pl.col("grades").dp.sum((1.0, 2.0), scale=0.0)
-plan = pl.DataFrame(None, lf.schema, orient="row").lazy().group_by("classes").agg(expr)
+make_private = dp.m.make_private_lazyframe(
+    input_domain=lf_domain_with_margin,
+    input_metric=dp.symmetric_distance(),
+    output_measure=dp.max_divergence(T=float),
+    lazyframe=plan,
+    global_scale=1.0)
 
-m_lf = dp.m.make_private_lazyframe(
-        lf_domain, dp.symmetric_distance(), dp.max_divergence(T=float), plan, 0.0
-    )
-
-df_exp = pl.DataFrame([
-    pl.Series("classes", list(range(1, 6)), dtype=pl.Int32),
-    pl.Series("grades", [10.0] * 5),
-])
-df_act = m_lf(lf).sort("classes").collect()
-
-print('50?', lf.select(expr).collect()["grades"][0])
+df = pl.from_records(
+    [
+        [0, 0], # No kindergarteners with pets.
+        [0, 0],
+        [0, 0],
+        [1, 1], # Each first grader has 1 pet.
+        [1, 1],
+        [1, 1],
+        [1, 2], # One second grader has chickens!
+        [1, 2],
+        [9, 2]
+    ],
+    schema=['pet_count', 'grade'])
+lf = pl.LazyFrame(df)
+results = make_private(lf).sort("grade").collect()
+print(results)
