8b7e5a5d7e96e90442c3ae19249666171ad5b5e9
Shoeboxam <shoeboxam@gmail.com>
2024-05-03 22:44:16 -0500

    respond to review/refactor to allow arbitrary expressions

--- rust/src/transformations/make_stable_expr/expr_fill_null/mod.rs
+++ rust/src/transformations/make_stable_expr/expr_fill_null/mod.rs
@@ -1,10 +1,8 @@
-use polars::prelude::*;
 use polars_plan::dsl::{Expr, FunctionExpr};
 
-use crate::core::{Domain, ExprFunction, Function, MetricSpace, StabilityMap, Transformation};
-use crate::domains::{ExprDomain, OuterMetric, SeriesDomain};
+use crate::core::{Function, MetricSpace, StabilityMap, Transformation};
+use crate::domains::{ExprContext, ExprDomain, OuterMetric};
 use crate::error::*;
-use crate::traits::Primitive;
 use crate::transformations::DatasetMetric;
 
 use super::StableExpr;
@@ -29,141 +27,82 @@ where
     (ExprDomain, M): MetricSpace,
     Expr: StableExpr<M, M>,
 {
-    let Some((input, constant)) = match_fill_null(&expr)? else {
-        return fallible!(MakeTransformation, "expected fill_null expression");
-    };
-
-    let t_prior = input
-        .clone()
-        .make_stable(input_domain.clone(), input_metric.clone())?;
-    let (middle_domain, middle_metric) = t_prior.output_space();
-
-    let mut output_domain = middle_domain.clone();
-    let series_domain = output_domain.active_series_mut()?;
-    series_domain.nullable = false;
-
-    let dtype = series_domain.field.dtype.clone();
-
-    use DataType::*;
-    let constant = match &dtype {
-        UInt32 => reconcile_domain::<u32>(constant, series_domain),
-        UInt64 => reconcile_domain::<u64>(constant, series_domain),
-        Int8 => reconcile_domain::<i8>(constant, series_domain),
-        Int16 => reconcile_domain::<i16>(constant, series_domain),
-        Int32 => reconcile_domain::<i32>(constant, series_domain),
-        Int64 => reconcile_domain::<i64>(constant, series_domain),
-        Float32 => reconcile_domain::<f32>(constant, series_domain),
-        Float64 => reconcile_domain::<f64>(constant, series_domain),
-        Boolean => reconcile_domain::<bool>(constant, series_domain),
-        String => reconcile_domain::<str>(constant, series_domain),
-        UInt8 | UInt16 => {
-            fallible!(
-                MakeTransformation,
-                "u8 and 16 are not supported, please use u32 or u64 instead"
-            )
-        }
-        dtype => fallible!(MakeTransformation, "unsupported dtype: {}", dtype),
-    }?;
-
-    t_prior
-        >> Transformation::new(
-            middle_domain.clone(),
-            output_domain,
-            Function::then_expr(move |expr| {
-                let mut expr = expr.fill_null(constant.clone());
-
-                // Update the super type of the fill_null function
-                // This is necessary because it initializes to Unknown,
-                // and Unknown dtype panics when serialized for FFI.
-                let Expr::Function {
-                    function: FunctionExpr::FillNull { super_type },
-                    ..
-                } = &mut expr
-                else {
-                    unreachable!();
-                };
-                *super_type = dtype.clone();
-                expr
-            }),
-            middle_metric.clone(),
-            middle_metric,
-            StabilityMap::new(Clone::clone),
-        )?
-}
-
-pub fn match_fill_null(expr: &Expr) -> Fallible<Option<(&Expr, &LiteralValue)>> {
     let Expr::Function {
         input,
         function: FunctionExpr::FillNull { .. },
         ..
     } = expr
     else {
-        return Ok(None);
+        return fallible!(MakeTransformation, "expected fill_null expression");
     };
 
-    let Ok([input, constant]) = <&[_; 2]>::try_from(input.as_slice()) else {
+    let Ok([data, fill]) = <[_; 2]>::try_from(input) else {
         return fallible!(MakeTransformation, "fill_null expects 2 arguments");
     };
 
-    let Expr::Literal(constant) = constant else {
+    let ExprDomain {
+        frame_domain,
+        context,
+    } = input_domain.clone();
+    let rr_domain = ExprDomain::new(frame_domain, ExprContext::RowByRow);
+
+    let t_data = data
+        .clone()
+        .make_stable(rr_domain.clone(), input_metric.clone())?;
+    let t_fill = fill
+        .clone()
+        .make_stable(rr_domain.clone(), input_metric.clone())?;
+
+    let (data_domain, data_metric) = t_data.output_space();
+    let (fill_domain, fill_metric) = t_fill.output_space();
+
+    if data_metric != fill_metric {
         return fallible!(
             MakeTransformation,
-            "fill_null expects a literal as second argument"
-        );
-    };
-    Ok(Some((input, constant)))
-}
-
-fn reconcile_domain<T: TryGetValue + ?Sized>(
-    constant: &LiteralValue,
-    series_domain: &SeriesDomain,
-) -> Fallible<Expr>
-where
-    T::Owned: Primitive + Literal,
-{
-    let constant = constant
-        .to_any_value()
-        .ok_or_else(|| err!(MakeTransformation))?
-        .strict_cast(&series_domain.field.dtype)?;
-    let constant = T::try_get_value(constant)?;
-
-    if !series_domain.atom_domain::<T::Owned>()?.member(&constant)? {
-        return fallible!(
-            MakeTransformation,
-            "constant {:?} is not in domain {:?}",
-            constant,
-            series_domain
+            "interior metrics on the input and fill expressions must match: {:?} != {:?}",
+            data_metric,
+            fill_metric
         );
     }
-    Ok(lit(constant))
-}
 
-trait TryGetValue: ToOwned {
-    fn try_get_value(value: AnyValue) -> Fallible<Self::Owned>;
-}
+    if fill_domain.active_series()?.nullable {
+        return fallible!(MakeTransformation, "fill expression must not be nullable");
+    }
 
-macro_rules! impl_try_get_value {
-    ($ty:ty, $ident:ident) => {
-        impl TryGetValue for $ty {
-            fn try_get_value(value: AnyValue) -> Fallible<Self::Owned> {
-                if let AnyValue::$ident(v) = value {
-                    Ok(v.to_owned())
-                } else {
-                    fallible!(MakeTransformation, "expected {}", stringify!($ty))
-                }
-            }
-        }
-    };
+    let mut output_domain = data_domain.clone();
+    // fill_null should not change the output context-- just require that its input is row-by-row
+    output_domain.context = context;
+
+    let series_domain = output_domain.active_series_mut()?;
+    series_domain.drop_bounds().ok();
+    series_domain.nullable = false;
+
+    let dtype = series_domain.field.dtype.clone();
+
+    Transformation::new(
+        input_domain,
+        output_domain,
+        Function::new_fallible(move |arg| {
+            let expr_data = t_data.invoke(arg)?.1;
+            let expr_fill = t_fill.invoke(arg)?.1;
+
+            let mut expr_impute = expr_data.fill_null(expr_fill);
+
+            // Update the super type of the fill_null function
+            // This is necessary because it initializes to Unknown,
+            // and Unknown dtype panics when serialized for FFI.
+            let Expr::Function {
+                function: FunctionExpr::FillNull { super_type },
+                ..
+            } = &mut expr_impute
+            else {
+                unreachable!();
+            };
+            *super_type = dtype.clone();
+            Ok((arg.0.clone(), expr_impute))
+        }),
+        input_metric.clone(),
+        input_metric,
+        StabilityMap::new(Clone::clone),
+    )
 }
-impl_try_get_value!(u8, UInt8);
-impl_try_get_value!(u16, UInt16);
-impl_try_get_value!(u32, UInt32);
-impl_try_get_value!(u64, UInt64);
-impl_try_get_value!(i8, Int8);
-impl_try_get_value!(i16, Int16);
-impl_try_get_value!(i32, Int32);
-impl_try_get_value!(i64, Int64);
-impl_try_get_value!(f32, Float32);
-impl_try_get_value!(f64, Float64);
-impl_try_get_value!(bool, Boolean);
-impl_try_get_value!(str, String);
