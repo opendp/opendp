e5a0c3bb86cc7d3aadcc149db221963685f338c3
Chuck McCallum <mccallucc@gmail.com>
2024-06-06 10:25:43 -0400

    honest-but-curious TODOs

--- rust/src/transformations/manipulation/mod.rs
+++ rust/src/transformations/manipulation/mod.rs
@@ -135,7 +135,8 @@ where
     generics(D(suppress), M(suppress))
 )]
 /// Make a Transformation representing the identity function.
-///
+/// Requires `honest-but-curious`: TODO
+/// 
 /// WARNING: In Python, this function does not ensure that the domain and metric form a valid metric space.
 /// However, if the domain and metric do not form a valid metric space,
 /// then the resulting Transformation won't be chainable with any valid Transformation,
