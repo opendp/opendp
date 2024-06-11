2133682e7b7f36d3cab744f4584e0b98c9aace61
Shoeboxam <shoeboxam@gmail.com>
2024-06-10 23:34:41 -0400

    fix test

--- rust/src/measurements/make_private_lazyframe/postprocess/test.rs
+++ rust/src/measurements/make_private_lazyframe/postprocess/test.rs
@@ -1,9 +1,9 @@
 use polars::{df, lazy::frame::IntoLazy};
-use polars_plan::dsl::len;
+use polars_plan::dsl::col;
 
 use crate::{
     core::PrivacyNamespaceHelper,
-    domains::{AtomDomain, SeriesDomain},
+    domains::{AtomDomain, Margin, SeriesDomain},
     measurements::make_private_lazyframe,
     measures::MaxDivergence,
     metrics::SymmetricDistance,
@@ -13,14 +13,20 @@ use super::*;
 
 #[test]
 fn test_make_private_lazyframe_sort() -> Fallible<()> {
-    let lf_domain = LazyFrameDomain::new(vec![SeriesDomain::new(
-        "A",
-        AtomDomain::new_closed((0, 1))?,
-    )])?;
-    let lf = df!("A" => [0, 1, 1])?.lazy();
+    let s1 = SeriesDomain::new("A", AtomDomain::new_closed((0, 1))?);
+    let s2 = SeriesDomain::new("B", AtomDomain::<bool>::default());
+
+    let lf_domain = LazyFrameDomain::new(vec![s1, s2])?.with_margin(
+        &["B"],
+        Margin::new()
+            .with_public_lengths()
+            .with_max_partition_length(5),
+    )?;
+    let lf = df!("A" => [0, 1, 1], "B" => [true, false, true])?.lazy();
 
     let query = lf
-        .select([len().dp().laplace(Some(1.))])
+        .group_by([col("B")])
+        .agg([col("A").dp().sum((0, 1), Some(1.))])
         .sort("len", Default::default());
 
     make_private_lazyframe(
