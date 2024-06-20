1b511ffb4195bf9f58ecc1d0308854f106be8a61
Chuck McCallum <mccallucc@gmail.com>
2024-06-20 16:54:04 -0400

    script is getting better

--- docs/tools/nb-to-rst.py
+++ docs/tools/nb-to-rst.py
@@ -1,14 +1,82 @@
 import argparse
 import subprocess
 from pathlib import Path
+import re
+
+
+def get_rst(nb_path):
+    return subprocess.check_output(
+        ['jupyter', 'nbconvert', nb_path, '--to', 'rst', '--stdout'],
+        text=True
+    )
+
+
+def unindent(text):
+    return '\n'.join(line[4:] for line in text.split('\n'))
+
+
+def doctest(text):
+    # Not perfect! Closing parens could be in the first column.
+    return '\n'.join(
+        (f'... {line}' if line[0] == ' ' else f'>>> {line}')
+            if line else ''
+        for line in text.split('\n')
+    )
+
+
+def reindent(text):
+    return '\n'.join(' ' * 4 + line for line in text.split('\n'))
+
+
+def convert_block(match):
+    '''
+    Given a match object, with subexpressions for the input and output lines,
+    return a python code block containing a doctest.
+    '''
+    input = unindent(match.group(1).strip())
+    output = unindent((match.group(2) or '').strip())
+    double_indent_input = reindent(reindent(doctest(input)))
+    double_indent_output = reindent(reindent(output))
+    return f'''.. tab-set::
+
+    .. tab-item:: Python
+        :sync: python
+
+        .. code:: python
+
+{double_indent_input}
+{double_indent_output}
+
+'''
+
+
+def convert_blocks(rst):
+    '''
+    Converts `ipython3` and `parsed-literal` blocks into doctests.
+    This is not perfect because cells can combine multiple statements:
+    We'll need to finish it by hand.
+    '''
+    # TODO
+    pattern = r'\.\. code:: ipython3\n(.*?)(?:^\.\. parsed-literal::\n(.*?))?^(?=\w)'
+    rst, count = re.subn(pattern, convert_block, rst, flags=re.MULTILINE|re.DOTALL)
+    print(f"Converted {count} code blocks")
+    return rst
+
+
+# TODO
+# def extract_code(rst, parent_path):
+#     '''
+#     Extracts code into standalone files
+#     '''
+#     return rst
+
 
 
 def convert(nb_path):
-    rst_lines = subprocess.check_output(
-        ['jupyter', 'nbconvert', nb_path, '--to', 'rst', '--stdout'],
-        text=True
-    ).splitlines()
-    print(rst_lines[:10])
+    rst = get_rst(nb_path)
+    rst = convert_blocks(rst)
+    # rst = extract_code(rst)
+    return rst
 
 
 def main():
@@ -16,7 +84,10 @@ def main():
         description="Wraps nbconvert, also converting code cells to tabbed doctests")
     parser.add_argument("nb_path", help="Notebook to convert", type=Path)
     args = parser.parse_args()
-    convert(args.nb_path)
+    rst = convert(args.nb_path)
+    rst_path = args.nb_path.parent / f"{args.nb_path.stem}.rst"
+    rst_path.write_text(rst)
+    print(f"Wrote to: {rst_path}")
 
 
 if __name__ == "__main__":
