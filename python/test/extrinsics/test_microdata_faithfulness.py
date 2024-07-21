d8764b4b010e66b7866e0b5e39537387e739c2cd
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-07-21 01:14:45 -0400

    fix test dependencies

--- python/test/extrinsics/test_microdata_faithfulness.py
+++ python/test/extrinsics/test_microdata_faithfulness.py
@@ -10,26 +10,29 @@ dp.enable_features("contrib", "floating-point")
 def test_microdataa_faithfulness():
     from opendp._extrinsics.microdata.faithfulness import make_faithfulness
 
-    # TODO: add optional_dependency("igraph")
-    with optional_dependency("numpy"), optional_dependency("scipy"), optional_dependency("sklearn"):
+    pl = pytest.importorskip("polars")
+    # private
+    dataset = pl.LazyFrame({"A": pl.Series([100, 1, 300], dtype=pl.Int32),
+                            "B": pl.Series([4, 4, 6], dtype=pl.Int32)})
 
-        pl = pytest.importorskip("polars")
-        # private
-        dataset = pl.LazyFrame({"A": pl.Series([100, 1, 300], dtype=pl.Int32),
-                                "B": pl.Series([4, 4, 6], dtype=pl.Int32)})
+    # public
+    reference_dataset = pl.DataFrame({"A": pl.Series([1, 2, 3], dtype=pl.Int32),
+                                      "B": pl.Series([4, 5, 6], dtype=pl.Int32)})
 
-        # public
-        reference_dataset = pl.DataFrame({"A": pl.Series([1, 2, 3], dtype=pl.Int32),
-                                          "B": pl.Series([4, 5, 6], dtype=pl.Int32)})
+    lf_domain = dp.lazyframe_domain(
+        [dp.series_domain("A", dp.option_domain(dp.atom_domain(T=dp.i32))),
+         dp.series_domain("B", dp.atom_domain(T=dp.i32))]
+    )
 
-        lf_domain = dp.lazyframe_domain(
-            [dp.series_domain("A", dp.option_domain(dp.atom_domain(T=dp.i32))),
-             dp.series_domain("B", dp.atom_domain(T=dp.i32))
-             ])
+    # exact match
+    def similarity_record_fn(x, y):
+        return 2*(x != y).any()
 
-        # exact match
-        def similarity_record_fn(x, y):
-            return 2*(x != y).any()
+    with (optional_dependency("numpy"),
+          optional_dependency("polars"),
+          optional_dependency("scipy"),
+          optional_dependency("sklearn"),
+          optional_dependency("igraph")):
 
         trans1 = make_faithfulness(lf_domain,
                                    dp.symmetric_distance(),
