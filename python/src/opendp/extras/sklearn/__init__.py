18cf6dcee0d2ecf31630adf6ede0b51d7d57b48f
Chuck McCallum <mccallucc@gmail.com>
2024-07-22 16:07:26 -0400

    sklearn module docs

--- python/src/opendp/extras/sklearn/__init__.py
+++ python/src/opendp/extras/sklearn/__init__.py
@@ -1,1 +1,14 @@
+'''
+This module requires extra installs: ``pip install opendp[sklearn]``
+
+For convenience, all the functions of this module are also available from :py:mod:`opendp.prelude`.
+We suggest importing under the conventional name ``dp``:
+
+.. code:: python
+
+    >>> import opendp.prelude as dp
+
+The methods of this module will then be accessible at ``dp.sklearn``.    
+'''
+
 from .pca import PCA
