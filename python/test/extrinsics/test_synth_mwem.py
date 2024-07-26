2a6240a7b93af3829a3a73582378e71eb3a63e91
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-07-26 13:44:30 -0400

    fix test dependency

--- python/test/extrinsics/test_synth_mwem.py
+++ python/test/extrinsics/test_synth_mwem.py
@@ -1,5 +1,3 @@
-import pytest
-
 import opendp.prelude as dp
 from opendp._extrinsics.synth.mwem import Schema, MWEMSynthesizerTrainer
 
@@ -14,28 +12,29 @@ def test_mwem_learning():
     cols = list("abcd")
     lower, upper = 1, 9
 
-    np = pytest.importorskip("numpy")
-    pl = pytest.importorskip("polars")
-    sklearn_datasets = pytest.importorskip("sklearn.datasets")
+    with (optional_dependency("numpy"), optional_dependency("polars"),
+          optional_dependency("sklearn"), optional_dependency("tqdm")):
+        import numpy as np
+        import polars as pl
+        from sklearn.datasets import make_blobs
 
-    data_array = (sklearn_datasets.make_blobs(num_records, 4, random_state=42)[0]
-                  .round()
-                  .clip(lower, upper)
-                  .astype(np.int32))
-    real_df = pl.LazyFrame(data_array, schema={col: pl.Int32 for col in cols})
+        data_array = (make_blobs(num_records, 4, random_state=42)[0]
+                      .round()
+                      .clip(lower, upper)
+                      .astype(np.int32))
+        real_df = pl.LazyFrame(data_array, schema={col: pl.Int32 for col in cols})
 
-    lf_domain = dp.lazyframe_domain(
-        [dp.series_domain(col, dp.atom_domain(T=dp.i32, bounds=(lower, upper)))
-         for col in cols])
+        lf_domain = dp.lazyframe_domain(
+            [dp.series_domain(col, dp.atom_domain(T=dp.i32, bounds=(lower, upper)))
+             for col in cols])
 
-    lf_domain_with_margin = dp.with_margin(lf_domain,
-                                           by=cols,
-                                           public_info="keys")
+        lf_domain_with_margin = dp.with_margin(lf_domain,
+                                               by=cols,
+                                               public_info="keys")
 
-    real_schema = Schema(bounds={col: (lower, upper) for col in cols},
-                         size=num_records)
+        real_schema = Schema(bounds={col: (lower, upper) for col in cols},
+                             size=num_records)
 
-    with optional_dependency("numpy"), optional_dependency("polars"), optional_dependency("tqdm"):
         mwem_meas = MWEMSynthesizerTrainer.make(lf_domain_with_margin, dp.symmetric_distance(),
                                                 epsilon=10, schema=real_schema,
                                                 epsilon_split=0.5, num_queries=1000,
@@ -46,5 +45,6 @@ def test_mwem_learning():
 
         synth_df = mwem_synth.sample(num_records)
 
-    max_error_in_mean = np.max(np.abs(real_df.mean().collect() - synth_df.mean()))
-    assert max_error_in_mean < 1
+        max_error_in_mean = np.max(np.abs(real_df.mean().collect() - synth_df.mean()))
+
+        assert max_error_in_mean < 1
