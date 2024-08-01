95f8b537d07cc8163f2c66bca890f36d3d53596f
Chuck McCallum <mccallucc@gmail.com>
2024-08-01 17:51:25 -0400

    One more note

--- python/src/opendp/extras/sklearn/__init__.py
+++ python/src/opendp/extras/sklearn/__init__.py
@@ -8,7 +8,8 @@ We suggest importing under the conventional name ``dp``:
 
     >>> import opendp.prelude as dp
 
-The methods of this module will then be accessible at ``dp.sklearn``.    
+The methods of this module will then be accessible at ``dp.sklearn``.
+Submodule organization will follow the conventions of `scikit-learn <https://scikit-learn.org/stable/api/index.html>`_.
 '''
 
 import opendp.extras.sklearn.decomposition
