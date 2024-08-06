5b4cb42c831b054245f98bbc02a6d124bb468e7a
shlomihod <6306135+shlomihod@users.noreply.github.com>
2024-08-05 20:31:39 -0400

    fix module name typo

--- python/test/extras/test_make_private_selection.py
+++ python/test/extras/test_make_private_selection.py
@@ -4,26 +4,29 @@ from ..helpers import optional_dependency
 
 
 def test_private_selection_threshold():
-    from opendp._extrinsics._make_private_selection import make_private_selection_threshold
+    from opendp.extras._make_private_selection import (
+        make_select_private_candidate_above_threshold,
+    )
 
     dp.enable_features("contrib", "floating-point")
 
-    bounds = (0., 100.)
+    bounds = (0.0, 100.0)
     range_ = bounds[1] - bounds[0]
     epsilon = 1.0
     threshold = 23
 
-    input_domain = dp.vector_domain(dp.atom_domain(T=float))
-    input_metric = dp.symmetric_distance()
+    space = dp.vector_domain(dp.atom_domain(T=float)), dp.symmetric_distance()
 
     # score
-    count = (dp.t.make_count(input_domain, input_metric)
-             >> dp.m.then_geometric(scale=2 / epsilon))
+    count = space >> dp.t.then_count() >> dp.m.then_laplace(scale=2 / epsilon)
 
     # output
-    sum_ = (dp.t.make_clamp(input_domain, input_metric, bounds)
-            >> dp.t.then_sum()
-            >> dp.m.then_laplace(scale=2 * range_ / epsilon))
+    sum_ = (
+        space
+        >> dp.t.then_clamp(bounds)
+        >> dp.t.then_sum()
+        >> dp.m.then_laplace(scale=2 * range_ / epsilon)
+    )
 
     mech_with_score = dp.c.make_basic_composition([count, sum_])
 
@@ -31,9 +34,9 @@ def test_private_selection_threshold():
 
         import numpy as np
 
-        meas_pst = make_private_selection_threshold(mech_with_score,
-                                                    threshold=threshold,
-                                                    stop_probability=0)
+        meas_pst = make_select_private_candidate_above_threshold(
+            mech_with_score, threshold=threshold, stop_probability=0
+        )
 
         data = np.random.default_rng(seed=42).normal(10, 5, 20)
 
