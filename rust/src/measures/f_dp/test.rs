eba6b382ca3c23f60a7ede1a116478392320b34a
Michael Shoemate <shoeboxam@gmail.com>
2024-08-06 15:04:27 -0400

    ffi for f-DP tradeoff curves (#1828)

---
+++ rust/src/measures/f_dp/test.rs
@@ -0,0 +1,128 @@
+use super::*;
+
+#[test]
+fn test_privacy_profile() -> Fallible<()> {
+    let pure_dp_privacy_profile_delta = move |epsilon: f64| {
+        let pure_epsilon = 1.0;
+        if epsilon > pure_epsilon {
+            return Ok(0.0);
+        }
+
+        if epsilon < (pure_epsilon.exp() - 1.0).ln() {
+            return Ok(1.0);
+        }
+
+        Ok(pure_epsilon.exp() - epsilon.exp())
+    };
+    let profile = PrivacyProfile::new(move |epsilon| pure_dp_privacy_profile_delta(epsilon));
+
+    //assert!(profile.delta(profile.epsilon(0.0)?)? == 0.0);
+    println!("epsilon(0.0) {}", profile.epsilon(0.0)?);
+    println!("epsilon(1.0) {}", profile.epsilon(1.0)?);
+
+    let epsilons = (0..100)
+        .map(|i| i as f64 / 100.0)
+        .map(|a| profile.epsilon(a))
+        .collect::<Fallible<Vec<f64>>>()?;
+
+    println!("{epsilons:?}");
+
+    //println!("{}", profile.epsilon(1.0)?);
+    //println!("{}", profile.epsilon(2.0)?);
+    //println!("{}", profile.epsilon(100.0)?);
+
+    Ok(())
+}
+
+#[test]
+fn test_beta() -> Fallible<()> {
+    let pure_dp_privacy_profile_delta = move |epsilon: f64| {
+        let pure_epsilon = 1.0;
+        if epsilon >= pure_epsilon {
+            // eps >= 1.0
+            return Ok(0.0);
+        }
+
+        if epsilon <= (pure_epsilon.exp() - 1.0).ln() {
+            // eps <= 0.54132...
+            return Ok(1.0);
+        }
+
+        Ok(pure_epsilon.exp() - epsilon.exp())
+    };
+    let profile = PrivacyProfile::new(move |epsilon| pure_dp_privacy_profile_delta(epsilon));
+
+    //println!("{}", pure_dp_privacy_profile_delta(0.9999999999999999)?);
+    //println!("{}", profile.delta(0.9999999999999999)?);
+
+    println!("{}", profile.epsilon(0.0)?);
+    println!("{}", profile.epsilon(1.0)?);
+    println!("{}", profile.beta(0.0)?);
+
+    let betas = (0..100)
+        .map(|i| i as f64 / 100.0)
+        .map(|a| profile.beta(a))
+        .collect::<Fallible<Vec<f64>>>()?;
+
+    println!("{betas:?}");
+
+    Ok(())
+}
+
+#[test]
+fn test_all() -> Fallible<()> {
+    let pure_dp_privacy_profile_delta = move |epsilon: f64| {
+        let pure_epsilon = 1.0;
+        if epsilon > pure_epsilon {
+            return Ok(0.0);
+        }
+
+        if epsilon < (pure_epsilon.exp() - 1.0).ln() {
+            return Ok(1.0);
+        }
+
+        Ok(pure_epsilon.exp() - epsilon.exp())
+    };
+    let profile = PrivacyProfile::new(move |epsilon| pure_dp_privacy_profile_delta(epsilon));
+
+    // Posterior
+    let posterior_curve = profile.posterior_curve(0.5);
+    println!("posterior(prior=0.5, alpha=0) = {}", posterior_curve(0.0)?);
+    println!(
+        "posterior(prior=0.5, alpha=0.5) = {}",
+        posterior_curve(0.5)?
+    );
+    println!("posterior(prior=0.5, alpha=1) = {}", posterior_curve(1.0)?);
+
+    // Relative risk
+
+    let relative_risk_curve = profile.relative_risk_curve(0.5);
+    println!(
+        "relative_risk(prior=0.5, alpha=0) = {}",
+        relative_risk_curve(0.0)?
+    );
+    println!(
+        "relative_risk(prior=0.5, alpha=0.5) = {}",
+        relative_risk_curve(0.5)?
+    );
+    println!(
+        "relative_risk(prior=0.5, alpha=1) = {}",
+        relative_risk_curve(1.0)?
+    );
+
+    let posts = (0..100)
+        .map(|i| i as f64 / 100.0)
+        .map(|a| posterior_curve(a))
+        .collect::<Fallible<Vec<f64>>>()?;
+
+    println!("{posts:?}");
+
+    let rrisks = (0..100)
+        .map(|i| i as f64 / 100.0)
+        .map(|a| relative_risk_curve(a))
+        .collect::<Fallible<Vec<f64>>>()?;
+
+    println!("{rrisks:?}");
+
+    Ok(())
+}
