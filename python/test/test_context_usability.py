7ead05e7bd80f8fd242a7ea5db153e10bc9ae1f7
Chuck McCallum <mccallucc@gmail.com>
2024-08-09 07:37:53 -0400

    reproducer for max_num_partitions; Should understand correct behavior before merge

--- python/test/test_context_usability.py
+++ python/test/test_context_usability.py
@@ -83,3 +83,43 @@ def test_query_dir():
     query_dir = dir(context.query())
     assert 'count' in query_dir
     assert 'laplace' in query_dir
+
+
+def test_priv_map_fails():
+    pl = pytest.importorskip('polars')
+    # partitions? NB_REGIONS = 3
+    NB_ROWS = 6
+
+    df = pl.DataFrame(
+        {
+            "region": [1, 1, 2, 2, 3, 3],
+            "income": [1000.0, 1000.0, 2000.0, 2000.0, 3000.0, 3000.0]
+        },
+        schema={"region": pl.Int64, "income": pl.Float64}
+    )
+    lf = df.lazy()
+
+    # Minimal domain
+    lf_domain = dp.lazyframe_domain([
+        dp.series_domain("region", dp.atom_domain(T=dp.i64)),
+        dp.series_domain("income", dp.atom_domain(T=dp.f64))
+    ])
+    lf_domain = dp.with_margin(lf_domain, by=[], public_info="lengths", max_partition_length=NB_ROWS)
+
+    # Group-by query: average income per region, add noïse with scale of 1.0
+    income_lower_bound, income_upper_bound = 1_000, 100_000
+    plan = lf.group_by("region").agg([
+        pl.col("income").dp.mean(bounds=(income_lower_bound, income_upper_bound), scale=1.0)
+    ]).sort("income")
+
+    # Bad domain: add a margin that does not include max_num_partitions
+    bad_domain = dp.with_margin(lf_domain, by=["region"], public_info="lengths", max_partition_length=NB_ROWS)
+
+    # Construct and collect a measurement with bad_domain works
+    # TODO: Should it fail here?
+    bad_meas = dp.m.make_private_lazyframe(bad_domain, dp.symmetric_distance(), dp.max_divergence(T=float), plan)
+    _ = bad_meas(lf).collect()
+
+    # Privacy map fails: This is expected?
+    with pytest.raises(dp.OpenDPException, match='max_num_partitions must be known when the metric is not sensitive to ordering'):
+        bad_meas.map(1)
