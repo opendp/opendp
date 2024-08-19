2eea4cc18e52861a0c0d81ce4da4753a6bbf7546
Chuck McCallum <mccallucc@gmail.com>
2024-08-19 17:12:10 -0400

    ignore deprecation warnings during tests

--- python/test/__init__.py
+++ python/test/__init__.py
@@ -1,6 +1,5 @@
 import opendp.prelude as dp
 
-
 # We don't have tests that require a feature *not* to be enabled,
 # so more stable just to enable everything at the start.
 dp.enable_features('floating-point', 'contrib', 'honest-but-curious')
