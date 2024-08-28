3b3bb3a7002b4bf00cdd930b5a76e0928ef90556
Chuck McCallum <mccallucc@gmail.com>
2024-08-28 14:16:52 -0400

    python codegen test works

--- rust/opendp_tooling/src/codegen/test.rs
+++ rust/opendp_tooling/src/codegen/test.rs
@@ -42,7 +42,34 @@ fn test_python_code_generation() {
         has_ffi: true,
     };
 
-    let actual_code = generate_function("fake_module", &function, &HashMap::new(), &HashMap::new());
-    let expected_code = "TODO";
+    let typemap: HashMap<String, String> =
+        serde_json::from_str(&include_str!("python_typemap.json")).unwrap();
+    let actual_code = generate_function("fake_module", &function, &typemap, &HashMap::new());
+    let expected_code = "
+def fake_function(
+    fake_argument = 99.9
+):
+    r\"\"\"fake description
+
+    :param fake_argument: fake description
+    :return: fake description
+    :raises TypeError: if an argument's type differs from the expected type
+    :raises UnknownTypeException: if a type argument fails to parse
+    \"\"\"
+    assert_features(\"fake_feature\")
+
+    # No type arguments to standardize.
+    # Convert arguments to c types.
+    c_fake_argument = py_to_c(fake_argument, c_type=ctypes.c_double, type_name=f64)
+
+    # Call library function.
+    lib_function = lib.opendp_fake_module__fake_function
+    lib_function.argtypes = [ctypes.c_double]
+    lib_function.restype = ctypes.c_double
+
+    output = c_to_py(lib_function(c_fake_argument))
+
+    return output
+";
     assert_eq!(actual_code, expected_code);
 }
