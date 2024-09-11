4252dbd4b6810687cffeace5e319feffd7f67a06
Chuck McCallum <mccalluc@users.noreply.github.com>
2024-09-11 12:29:29 -0400

    Handle windows name convention
    Co-authored-by: Michael Shoemate <shoeboxam@gmail.com>

--- python/src/opendp/_lib.py
+++ python/src/opendp/_lib.py
@@ -29,7 +29,7 @@ def _load_library():
         lib_dir_file_names = [
             p for p in lib_dir.iterdir()
             if p.suffix in {".so", ".dylib", ".dll", ".pyd"}
-            and p.stem == 'libopendp']
+            and p.stem.replace("lib", "") == "opendp"]
         if len(lib_dir_file_names) != 1:
             raise Exception(f"Expected exactly one binary to be present in {lib_dir}. Got: {lib_dir_file_names}")
         
