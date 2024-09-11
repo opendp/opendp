6aeb53bd4eddff4d7ffe0aa6a90a69480d192b97
Shoeboxam <shoeboxam@gmail.com>
2024-09-11 02:25:01 -0400

    respond to review

--- rust/src/combinators/measure_cast/zCDP_to_approxDP/cdp_delta/mod.rs
+++ rust/src/combinators/measure_cast/zCDP_to_approxDP/cdp_delta/mod.rs
@@ -1,3 +1,47 @@
+// Copyright (c) 2022 President and Fellows of Harvard College
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright 2020 Thomas Steinke
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+// Algorithm from:
+//     Clément Canonne, Gautam Kamath, Thomas Steinke. The Discrete Gaussian for Differential Privacy. 2020.
+//     https://arxiv.org/abs/2004.00010
+//
+// This file is derived from the following implementation by Thomas Steinke:
+//     https://github.com/IBM/discrete-gaussian-differential-privacy/blob/cb190d2a990a78eff6e21159203bc888e095f01b/cdp2adp.py#L74-L102
+
 use std::ops::Neg;
 
 use num::Zero;
@@ -10,6 +54,10 @@ use crate::{
 #[cfg(test)]
 pub(crate) mod test;
 
+/// # Proof Definition
+///
+/// For any possible setting of $\rho$ and $\epsilon$, $\texttt{cdp\_delta}$ either returns an error,
+/// or a $\delta$ such that any $\rho$-differentially private measurement is also $(\epsilon, \delta)$-differentially private.
 pub(crate) fn cdp_delta(rho: f64, eps: f64) -> Fallible<f64> {
     if rho.is_sign_negative() {
         return fallible!(FailedMap, "rho ({}) must be non-negative", rho);
