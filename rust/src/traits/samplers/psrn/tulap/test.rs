c538a837ef29818b1d2cde66df36532a5db630f6
Shoeboxam <shoeboxam@gmail.com>
2024-09-11 12:35:57 -0400

    post-rebase fixes

--- rust/src/traits/samplers/psrn/tulap/test.rs
+++ rust/src/traits/samplers/psrn/tulap/test.rs
@@ -4,12 +4,11 @@ use super::*;
 
 #[test]
 fn test_sample_tulap_interval_progression() -> Fallible<()> {
-    let tulap_rv = TulapRV::new(
+    let mut tulap = PartialSample::new(TulapRV::new(
         RBig::ZERO,
         FBig::ONE.with_precision(50).value(),
         RBig::try_from(1e-6).unwrap(),
-    )?;
-    let mut tulap = PartialSample::new(tulap_rv);
+    )?);
     let (l, r) = assert_ordered_progression(&mut tulap, 20);
     let (l, r) = (l.to_f64().value(), r.to_f64().value());
     println!("{l:?}, {r:?}, {}", tulap.refinements);
@@ -18,15 +17,13 @@ fn test_sample_tulap_interval_progression() -> Fallible<()> {
 
 #[test]
 fn test_tulap_psrn() -> Fallible<()> {
+    let tulap = TulapRV::new(
+        RBig::ZERO,
+        FBig::ONE.with_precision(50).value(),
+        RBig::try_from(1e-6).unwrap(),
+    )?;
     let samples = (0..1000)
-        .map(|_| {
-            PartialSample::new(TulapRV::new(
-                RBig::ZERO,
-                FBig::ONE.with_precision(50).value(),
-                RBig::try_from(1e-6).unwrap(),
-            )?)
-            .value()
-        })
+        .map(|_| PartialSample::new(tulap.clone()).value())
         .collect::<Fallible<Vec<f64>>>()?;
     println!("{:?}", samples);
     Ok(())
