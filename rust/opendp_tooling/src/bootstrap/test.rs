a2ac6510bd7cef3d7754f03c03781132fa0c299c
Chuck McCallum <mccallucc@gmail.com>
2024-09-18 09:25:08 -0400

    add missing param to test

--- rust/opendp_tooling/src/bootstrap/test.rs
+++ rust/opendp_tooling/src/bootstrap/test.rs
@@ -5,12 +5,13 @@ use super::docstring::BootstrapDocstring;
 
 #[test]
 fn test_docstring_description_from_attrs() {
+    let name = "fake_name".to_string();
     let attrs = vec![];
     let output = ReturnType::Default;
     let path = None;
     let features = vec!["feature_1".to_string(), "feature_2".to_string()];
 
-    let result = BootstrapDocstring::from_attrs(attrs, &output, path, features);
+    let result = BootstrapDocstring::from_attrs(&name, attrs, &output, path, features);
 
     let docstring = result.expect("from_attrs failed");
     let description = docstring.description.unwrap();
