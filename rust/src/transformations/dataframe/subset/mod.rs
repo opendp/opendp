a9931ccbafb9d6df559f61303523e4daaaea96b6
Chuck McCallum <mccallucc@gmail.com>
2024-10-01 09:33:46 -0400

    no retroactive deprecation: "0.11.1" -> "0.12.0"

--- rust/src/transformations/dataframe/subset/mod.rs
+++ rust/src/transformations/dataframe/subset/mod.rs
@@ -12,7 +12,7 @@ use super::{DataFrame, DataFrameDomain};
 #[cfg(feature = "ffi")]
 mod ffi;
 
-#[deprecated(note = "Use Polars instead", since = "0.11.1")]
+#[deprecated(note = "Use Polars instead", since = "0.12.0")]
 #[bootstrap(features("contrib"))]
 /// Make a Transformation that subsets a dataframe by a boolean column.
 ///
