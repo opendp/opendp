971a9e3ff006843d70bbd4a27f51189dc80dd5d7
Michael Shoemate <shoeboxam@gmail.com>
2024-10-09 16:23:43 -0400

    R package maintenance (#2082)
    Close #2081

--- rust/src/metrics/ffi.rs
+++ rust/src/metrics/ffi.rs
@@ -265,7 +265,7 @@ impl Metric for UserDistance {
 /// # Why honest-but-curious?
 /// Your definition of `d` must satisfy the requirements of a pseudo-metric:
 /// 1. for any $x$, $d(x, x) = 0$
-/// 2. for any $x, y$, $d(x, y) \ge 0 (non-negativity)
+/// 2. for any $x, y$, $d(x, y) \ge 0$ (non-negativity)
 /// 3. for any $x, y$, $d(x, y) = d(y, x)$ (symmetry)
 /// 4. for any $x, y, z$, $d(x, z) \le d(x, y) + d(y, z)$ (triangle inequality)
 #[no_mangle]
