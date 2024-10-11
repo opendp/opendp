8d0ddcf84ef284013cb2d2111dd96aac6cf06d9e
Tudor Cebere <tudorcebere@gmail.com>
2024-10-11 12:39:34 -0400

    fmt

--- rust/src/combinators/measure_cast/bounded_range_to_zCDP/mod.rs
+++ rust/src/combinators/measure_cast/bounded_range_to_zCDP/mod.rs
@@ -1,10 +1,9 @@
 use crate::{
     core::{Domain, Measurement, Metric, MetricSpace, PrivacyMap},
     error::Fallible,
-    measures::{ZeroConcentratedDivergence, BoundedRange},
+    measures::{BoundedRange, ZeroConcentratedDivergence},
     traits::Float,
 };
-
 
 /// Constructs a new output measurement where the output measure
 /// is casted from `MaxDivergence<QO>` to `BoundedRange<QO>`.
@@ -19,9 +18,6 @@ use crate::{
 /// * `QO` - Output distance type. One of `f32` or `f64`.
 /// For more details, see: https://differentialprivacy.org/exponential-mechanism-bounded-range/
 
-
-
-
 pub fn make_bounded_range_to_zCDP<DI, TO, MI, QO>(
     m: Measurement<DI, TO, MI, BoundedRange<QO>>,
 ) -> Fallible<Measurement<DI, TO, MI, ZeroConcentratedDivergence<QO>>>
@@ -33,13 +29,22 @@ where
 {
     let privacy_map: PrivacyMap<MI, BoundedRange<QO>> = m.privacy_map.clone();
     // For Mike: How to make this sane? I just want an 8
-    let constant: QO = QO::one() + QO::one() + QO::one() + QO::one() + QO::one() + QO::one() + QO::one() + QO::one();
+    let constant: QO = QO::one()
+        + QO::one()
+        + QO::one()
+        + QO::one()
+        + QO::one()
+        + QO::one()
+        + QO::one()
+        + QO::one();
 
     m.with_map(
         m.input_metric.clone(),
         ZeroConcentratedDivergence::default(),
         PrivacyMap::new_fallible(move |d_in: &MI::Distance| {
-            privacy_map.eval(d_in).map(|br_eps| br_eps.powi(2) / constant)
+            privacy_map
+                .eval(d_in)
+                .map(|br_eps| br_eps.powi(2) / constant)
         }),
     )
 }
