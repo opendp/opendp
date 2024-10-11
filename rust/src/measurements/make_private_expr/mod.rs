0a708ce420a2d50e22e7f1083478bbe35c6d7d51
Shoeboxam <shoeboxam@gmail.com>
2024-10-11 17:00:07 -0400

    Polars: reuse margin descriptors

--- rust/src/measurements/make_private_expr/mod.rs
+++ rust/src/measurements/make_private_expr/mod.rs
@@ -221,11 +221,7 @@ where
 pub(crate) fn approximate_c_stability<MI: UnboundedMetric, MO: Metric>(
     trans: &Transformation<ExprDomain, ExprDomain, PartitionDistance<MI>, MO>,
 ) -> Fallible<MO::Distance> {
-    let margin = trans
-        .input_domain
-        .active_margin()
-        .cloned()
-        .unwrap_or_default();
+    let margin = trans.input_domain.active_margin()?;
 
     let d_in = match margin.public_info {
         // smallest valid dataset distance is 2 in bounded-DP
