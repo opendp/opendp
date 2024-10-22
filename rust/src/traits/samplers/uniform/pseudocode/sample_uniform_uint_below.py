38d381a288c2128f60c97570c843206187ec3481
Shoeboxam <shoeboxam@gmail.com>
2024-10-21 22:19:28 -0400

    review: sample_uniform_uint_below

--- rust/src/traits/samplers/uniform/pseudocode/sample_uniform_uint_below.py
+++ rust/src/traits/samplers/uniform/pseudocode/sample_uniform_uint_below.py
@@ -1,8 +1,8 @@
-# type: ignore
-def sample_uniform_uint_below(upper: T) -> T:
-    threshold = T.MAX - T.MAX % upper
+# type: ignore 
+def sample_uniform_uint_below(upper: T) -> T: 
+    threshold = T.MAX - T.MAX % upper 
 
-    while True:
-        sample = sample_from_uniform_bytes()
-        if sample < threshold:
-            return sample % upper
+    while True: 
+        sample = sample_from_uniform_bytes() 
+        if sample < threshold: 
+            return sample % upper 
