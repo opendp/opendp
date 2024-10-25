59080c0cb6949f375b976220845ea4d51c2cc099
Shoeboxam <shoeboxam@gmail.com>
2024-10-25 10:14:47 -0400

    update to latest proof template, polish

--- rust/src/measurements/laplace/integer/pseudocode/make_vector_integer_laplace.py
+++ rust/src/measurements/laplace/integer/pseudocode/make_vector_integer_laplace.py
@@ -1,17 +1,20 @@
 # type: ignore
-def make_vector_integer_laplace(input_domain, input_metric, scale: QO):
+def make_vector_integer_laplace(
+    input_domain: VectorDomain[AtomDomain[T]], input_metric: L1Distance[T], scale: f64
+):
     if scale.is_sign_negative():
         raise ValueError("scale must not be negative")
-    
+
     # conversion to rational will fail if scale is null
     r_scale = RBig.try_from(scale)
 
-    if scale == 0.:
-        def function(x: list[T]):
+    if scale == 0.0:
+        def function(x: Vec[T]):
             return x
+
     else:
-        def function(x: list[T]):
-            release = [IBig(x_i) + sample_discrete_laplace(r_scale) for x_i in x]
+        def function(x: Vec[T]):
+            release = [IBig.from_(x_i) + sample_discrete_laplace(r_scale) for x_i in x]
             # postprocessing
             return [T.saturating_cast(r_i) for r_i in release]
 
@@ -19,6 +22,6 @@ def make_vector_integer_laplace(input_domain, input_metric, scale: QO):
         input_domain=input_domain,
         function=function,
         input_metric=input_metric,
-        output_measure=MaxDivergence(QO),
-        privacy_map=laplace_map(scale, relaxation=0.)
+        output_measure=MaxDivergence(),
+        privacy_map=laplace_puredp_map(scale, relaxation=0.0),
     )
