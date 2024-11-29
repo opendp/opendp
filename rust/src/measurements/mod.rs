9a56d272b6d14af1b2ffce488ad77d3774d91b09
Shoeboxam <shoeboxam@gmail.com>
2024-11-29 16:53:40 -0500

    respond to review 11/29

--- rust/src/measurements/mod.rs
+++ rust/src/measurements/mod.rs
@@ -3,21 +3,6 @@
 //! The different [`crate::core::Measurement`] implementations in this module are accessed by calling the appropriate constructor function.
 //! Constructors are named in the form `make_xxx()`, where `xxx` indicates what the resulting `Measurement` does.
 
-#[cfg(feature = "contrib")]
-mod gumbel_max;
-#[cfg(feature = "contrib")]
-pub use gumbel_max::*;
-
-#[cfg(feature = "contrib")]
-mod exponential_max;
-#[cfg(feature = "contrib")]
-pub use exponential_max::*;
-
-#[cfg(feature = "contrib")]
-mod laplace;
-#[cfg(feature = "contrib")]
-pub use laplace::*;
-
 #[cfg(all(feature = "contrib", feature = "polars"))]
 mod make_private_expr;
 #[cfg(all(feature = "contrib", feature = "polars"))]
@@ -62,6 +47,11 @@ pub use randomized_response_bitvec::*;
 mod canonical_noise;
 #[cfg(feature = "contrib")]
 pub use canonical_noise::*;
+
+#[cfg(feature = "contrib")]
+mod randomized_response_bitvec;
+#[cfg(feature = "contrib")]
+pub use randomized_response_bitvec::*;
 
 #[cfg(all(feature = "floating-point", feature = "contrib"))]
 mod alp;
