a1fd0a8b03ee840d941d8bbaf45a1a5dc3319be5
Shoeboxam <shoeboxam@gmail.com>
2024-12-01 22:20:03 -0500

    respond to review 12/1

--- rust/src/traits/samplers/cks20/pseudocode/sample_bernoulli_exp1.py
+++ rust/src/traits/samplers/cks20/pseudocode/sample_bernoulli_exp1.py
@@ -2,7 +2,7 @@
 def sample_bernoulli_exp1(x) -> bool: 
     k = 1 
     while True: 
-        if sample_bernoulli_rational(x / k, false): # |\label{line:B_i}| 
+        if sample_bernoulli_rational(x / k): # |\label{line:B_i}| 
             k += 1 
         else:  
             return is_odd(k) # |\label{line:K}| 
