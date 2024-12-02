ab94fd744515c676ab2e9969f39e175d791de883
Shoeboxam <shoeboxam@gmail.com>
2024-12-01 19:34:57 -0500

    respond to review 12/1

--- rust/src/traits/samplers/cks20/pseudocode/sample_bernoulli_exp.py
+++ rust/src/traits/samplers/cks20/pseudocode/sample_bernoulli_exp.py
@@ -1,6 +1,6 @@
 # type: ignore 
 def sample_bernoulli_exp(x) -> bool: 
-    while x >= 1: 
+    while x > 1: 
         if sample_bernoulli_exp1(1):  # |\label{line:B_i}| 
             x -= 1 
         else:  
