f7c236c828afcf1ce42c40a49d6db1aed8cf8c20
Chuck McCallum <mccallucc@gmail.com>
2024-12-11 14:28:07 -0500

    get all errors, and not just the first

--- python/test/test_ast.py
+++ python/test/test_ast.py
@@ -6,11 +6,11 @@ from typing import NamedTuple
 import pytest
 
 
-def ends_with_ellipsis(node):
+def ast_ends_with_ellipsis(node):
     '''
-    >>> ends_with_ellipsis(ast.parse('def nope(): pass').body[0])
+    >>> ast_ends_with_ellipsis(ast.parse('def nope(): pass').body[0])
     False
-    >>> ends_with_ellipsis(ast.parse('def yeah(): ...').body[0])
+    >>> ast_ends_with_ellipsis(ast.parse('def yeah(): ...').body[0])
     True
 
     Need body[0] because parse wraps these in a "Module".
@@ -20,11 +20,11 @@ def ends_with_ellipsis(node):
     return last_node_value_value == Ellipsis
 
 
-def has_return(tree):
+def ast_has_return(tree):
     '''
-    >>> has_return(ast.parse('def nope(): pass'))
+    >>> ast_has_return(ast.parse('def nope(): pass'))
     False
-    >>> has_return(ast.parse('def yeah(): return 1'))
+    >>> ast_has_return(ast.parse('def yeah(): return 1'))
     True
     '''
     for node in ast.walk(tree):
@@ -33,9 +33,9 @@ def has_return(tree):
     return False
 
 
-def return_type(tree):
+def ast_return_type(tree):
     '''
-    >>> return_type(ast.parse('def f() -> list[int]: return [1]').body[0])
+    >>> ast_return_type(ast.parse('def f() -> list[int]: return [1]').body[0])
     'list[int]'
 
     Need body[0] because parse wraps these in a "Module".
@@ -67,7 +67,7 @@ for code_path in src_dir_path.glob('**/*.py'):
     for node in ast.walk(tree):
         if not isinstance(node, ast.FunctionDef):
             continue
-        if ends_with_ellipsis(node):
+        if ast_ends_with_ellipsis(node):
             continue
         if node.name.startswith('_'):
             is_public = False
@@ -88,8 +88,9 @@ assert len(functions) > 100
 @pytest.mark.parametrize("file,name,node,is_public", functions)  # type: ignore[attr-defined]
 def test_public_function_docs(file, name, node, is_public):
     where = f'In {file}, line {node.lineno}, def {name}'
+    errors = []
 
-    # First, check the docstring in isolation:
+    ### First, check the docstring in isolation:
     docstring = ast.get_docstring(node)
     if is_public:
         assert docstring is not None, f'{where}, add docstring or make private'
@@ -99,64 +100,56 @@ def test_public_function_docs(file, name, node, is_public):
 
     directives = set(re.findall(r'^\s*(\:\w+:?)', docstring, re.MULTILINE))
     unknown_directives = directives - {':param', ':rtype:', ':type', ':raises', ':example:', ':return:'}
-    assert not unknown_directives, (
-        f'{where} has unknown directives: {", ".join(unknown_directives)}'
-    )
+    if unknown_directives:
+        errors.append(f'unknown directives: {", ".join(unknown_directives)}')
 
-    param_dict = dict(re.findall(r':param (\w+):(.*)', docstring))
+    doc_arg_dict = dict(re.findall(r':param (\w+):(.*)', docstring))
     # TODO: Has 68 failures; Enable and fill in the docs
-    # k_missing_v = [k for k, v in param_dict.items() if not v.strip()]
-    # assert not k_missing_v, (
-    #     f'{where} has params missing descriptions: {", ".join(k_missing_v)}'
-    # )
+    k_missing_v = [k for k, v in doc_arg_dict.items() if not v.strip()]
+    if k_missing_v:
+        errors.append(f'params missing descriptions: {", ".join(k_missing_v)}')
 
-    # Then, compare the docstring to the AST:
-    args = (
+    ### Then, compare the docstring to the AST:
+    ast_args = (
         node.args.posonlyargs
         + node.args.args
         + node.args.kwonlyargs
     )
     if node.args.kwarg is not None:
-        args.append(node.args.kwarg)
+        ast_args.append(node.args.kwarg)
 
     # TODO: For "self" and "cls", confirm that it really is a method.
-    arg_names = {arg.arg for arg in args} - {'self'} - {'cls'}
-    if param_dict or is_public:
+    ast_arg_names = {arg.arg for arg in ast_args} - {'self'} - {'cls'}
+    if doc_arg_dict or is_public:
         # Private functions don't need to document params,
         # but if they do, they should be consistent with signature.
-        assert param_dict.keys() == arg_names, (
-            f'{where}, docstring params ({", ".join(param_dict.keys())}) '
-            f'!= function signature ({", ".join(arg_names)})'
-        )
-
-    if is_public:
-        has_return_statement = has_return(node)
-        has_return_directive = ':return:' in docstring
-        # TODO: Has 142 failures; Enable and fill in the docs.
-        # if has_return_statement:
-        #     assert has_return_directive, (
-        #         f'{where}, function has return statement, '
-        #         'but no :return: in docstring'
-        #     )
-        if has_return_directive:
-            assert has_return_statement, (
-                f'{where}, function has :return: directive, '
-                'but no return statement'
+        if doc_arg_dict.keys() != ast_arg_names:
+            errors.append(
+                f'docstring params ({", ".join(doc_arg_dict.keys())}) '
+                f'!= function signature ({", ".join(ast_arg_names)})'
             )
+
+    has_return_statement = ast_has_return(node)
+    has_return_directive = ':return:' in docstring
+    # TODO: Has ## failures; Enable and fill in the docs.
+    if has_return_statement and not has_return_directive:
+        errors.append('return statement, but no :return: in docstring')
+    if has_return_directive and not has_return_statement:
+        errors.append(':return: directive, but no return statement')
 
     rtype_match = re.search(r':rtype:(.*)', docstring)
     if rtype_match:
         doc_rtype = rtype_match.group(1).strip().replace('"', "'")
-        sig_rtype = return_type(node)
+        ast_rtype = ast_return_type(node)
         # We trust mypy to check that the annotation is consistent with the actual return,
         # so we just check that the annotation is consistent with docstring.
-        if sig_rtype is None:
-            assert False, (
-                f'{where}, to match :rtype:, add "-> {doc_rtype}"'
-            )
-        # If it's a single character, probably a typevar, so just skip.
-        elif len(sig_rtype) > 1:
-            assert doc_rtype == sig_rtype, (
-                f'{where}, update :rtype: from "{doc_rtype}" to "{sig_rtype}"'
-            )
- 
+        if ast_rtype is None:
+            errors.append(f'to match :rtype:, add "-> {doc_rtype}"')
+        # If it's a single character, probably a typevar, which we won't try to resolve.
+        elif len(ast_rtype) > 1:
+            if doc_rtype != ast_rtype:
+                errors.append(f'update :rtype: from "{doc_rtype}" to "{ast_rtype}"')
+    
+    if errors:
+        errors_str = '; '.join(f'({i+1}) {e}' for i, e in enumerate(errors))
+        raise AssertionError(f'{where}: {errors_str}')
