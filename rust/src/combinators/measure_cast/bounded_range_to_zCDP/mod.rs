995deab65d66a0cf5c1bd07fbb8acba6fa3c4d7b
Shoeboxam <shoeboxam@gmail.com>
2024-12-23 14:59:52 -0500

    first pass

--- rust/src/combinators/measure_cast/bounded_range_to_zCDP/mod.rs
+++ rust/src/combinators/measure_cast/bounded_range_to_zCDP/mod.rs
@@ -1,12 +1,16 @@
+use dashu::ibig;
+
 use crate::{
     core::{Domain, Measurement, Metric, MetricSpace, PrivacyMap},
     error::Fallible,
     measures::{BoundedRange, ZeroConcentratedDivergence},
-    traits::Float,
+    traits::{InfDiv, InfPowI},
 };
 
 /// Constructs a new output measurement where the output measure
-/// is casted from `MaxDivergence<QO>` to `BoundedRange<QO>`.
+/// is casted from `BoundedRange` to `ZeroConcentratedDivergence`.
+///
+/// For more details, see: https://differentialprivacy.org/exponential-mechanism-bounded-range/
 ///
 /// # Arguments
 /// * `meas` - a measurement with a privacy measure to be casted
@@ -15,25 +19,21 @@ use crate::{
 /// * `DI` - Input Domain
 /// * `DO` - Output Domain
 /// * `MI` - Input Metric
-/// * `QO` - Output distance type. One of `f32` or `f64`.
-/// For more details, see: https://differentialprivacy.org/exponential-mechanism-bounded-range/
-
-pub fn bounded_range_to_zCDP<DI, TO, MI, QO>(
-    m: Measurement<DI, TO, MI, BoundedRange>,
+pub fn make_bounded_range_to_zCDP<DI, TO, MI>(
+    meas: Measurement<DI, TO, MI, BoundedRange>,
 ) -> Fallible<Measurement<DI, TO, MI, ZeroConcentratedDivergence>>
 where
     DI: Domain,
     MI: 'static + Metric,
-    QO: Float,
     (DI, MI): MetricSpace,
 {
-    let privacy_map: PrivacyMap<MI, BoundedRange> = m.privacy_map.clone();
+    let privacy_map: PrivacyMap<MI, BoundedRange> = meas.privacy_map.clone();
 
-    m.with_map(
-        m.input_metric.clone(),
+    meas.with_map(
+        meas.input_metric.clone(),
         ZeroConcentratedDivergence::default(),
         PrivacyMap::new_fallible(move |d_in: &MI::Distance| {
-            privacy_map.eval(d_in).map(|br_eps| br_eps.powi(2) / 8.0)
+            privacy_map.eval(d_in)?.inf_powi(ibig!(2))?.inf_div(&8.0)
         }),
     )
 }
