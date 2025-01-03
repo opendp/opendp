63066643900b5a43957e8420f04ca9b0db3572dc
Chuck McCallum <mccallucc@gmail.com>
2025-01-03 12:27:10 -0500

    make mypy happy

--- python/src/opendp/extras/sklearn/decomposition/__init__.py
+++ python/src/opendp/extras/sklearn/decomposition/__init__.py
@@ -165,13 +165,17 @@ class PCA():
         n_changes: int = 1,
         whiten: bool = False,
     ) -> None:
-        # Error if constructor called without depency installed:
+        # Error if constructor called without dependency:
         import_optional_dependency('sklearn.decomposition')
         
         # The zero-argument form of super() does not work,
         # (I believe) because the type argument is determined lexically,
         # and it doesn't see our redefinition.
-        super(PCA, self).__init__(
+        # Instead, make the type explicit.
+        #
+        # Also because of the class redefinition, mypy has a lot of complaints,
+        # and so there's a lot of "type: ignore" below.
+        super(PCA, self).__init__(  # type: ignore[call-arg]
             n_components or n_features,
             whiten=whiten,
         )
@@ -184,6 +188,18 @@ class PCA():
     @property
     def n_features(self):
         return self.n_features_in_
+
+    # This isn't strictly necessary, since we just call the superclass method,
+    # but this lets us document a frequently used method,
+    # and avoids a number of mypy warnings.
+    def fit(self, X, y=None):
+        '''
+        Fit the model with X.
+
+        :param X: Training data, where ``n_samples`` is the number of samples and ``n_features`` is the number of features.
+        :param y: Ignored
+        '''
+        return super(PCA, self).fit(X) # type: ignore[misc]
 
     # this overrides the scikit-learn method to instead use the opendp-core constructor
     def _fit(self, X):
@@ -203,9 +219,9 @@ class PCA():
         input_metric = dp.symmetric_distance()
 
         n_estimated_components = (
-            self.n_components
-            if isinstance(self.n_components, int)
-            else self.n_features_in_
+            self.n_components  # type: ignore[attr-defined]
+            if isinstance(self.n_components, int)  # type: ignore[attr-defined]
+            else self.n_features_in_  # type: ignore[attr-defined]
         )
 
         return make_private_pca(
@@ -225,7 +241,7 @@ class PCA():
         self.mean_, S, Vt = values
         U = Vt.T
         n_samples, n_features = self.n_samples, self.n_features_in_
-        n_components = self.n_components
+        n_components = self.n_components  # type: ignore[attr-defined]
 
         # CODE BELOW THIS POINT IS FROM SKLEARN
         # flip eigenvectors' sign to enforce deterministic output
@@ -278,26 +294,8 @@ class PCA():
 
 
 _decomposition = import_optional_dependency('sklearn.decomposition', False)
-# if _decomposition is not None:
-#     class _SKLPCA(_decomposition.PCA): # type: ignore[name-defined]
-#         '''
-#         :meta private:
-#         '''
-#         pass
-# else: # pragma: no cover
-#     class _SKLPCA(object): # type: ignore[no-redef]
-#         '''
-#         :meta private:
-#         '''
-#         def __init__(*args, **kwargs):
-#             raise ImportError(
-#                 "The optional install scikit-learn is required for this functionality"
-#             )
-# # Redefine PCA, adding _SKLPCA as base class:
-# PCA = type('PCA', (_SKLPCA,), dict(PCA.__dict__)) 
-
 if _decomposition is not None:
-    PCA = type('PCA', (_decomposition.PCA,), dict(PCA.__dict__))
+    PCA = type('PCA', (_decomposition.PCA,), dict(PCA.__dict__))  # type: ignore[assignment,misc]
 
         
 def _smaller(v):
