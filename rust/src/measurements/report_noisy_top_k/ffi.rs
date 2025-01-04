10758f23854bf57b13c7bfd4b6402dac6dc2b10a
Shoeboxam <shoeboxam@gmail.com>
2025-01-04 12:24:47 -0500

    feat: make_report_noisy_top_k

--- rust/src/measurements/report_noisy_top_k/ffi.rs
+++ rust/src/measurements/report_noisy_top_k/ffi.rs
@@ -10,17 +10,18 @@ use crate::{
         any::{AnyDomain, AnyMeasure, AnyMeasurement, AnyMetric, Downcast},
         util::to_str,
     },
-    measurements::{Optimize, make_report_noisy_max, report_noisy_top_k::SelectionMeasure},
+    measurements::{Optimize, make_report_noisy_top_k, report_noisy_top_k::SelectionMeasure},
     measures::{MaxDivergence, RangeDivergence},
     metrics::LInfDistance,
     traits::{CastInternalRational, CheckNull, DistanceConstant, Number},
 };
 
 #[unsafe(no_mangle)]
-pub extern "C" fn opendp_measurements__make_noisy_max(
+pub extern "C" fn opendp_measurements__make_noisy_top_k(
     input_domain: *const AnyDomain,
     input_metric: *const AnyMetric,
     output_measure: *const AnyMeasure,
+    k: u32,
     scale: f64,
     optimize: *const c_char,
 ) -> FfiResult<*mut AnyMeasurement> {
@@ -29,6 +30,7 @@ pub extern "C" fn opendp_measurements__make_noisy_max(
     let output_measure = try_as_ref!(output_measure);
     let TIA_ = try_!(input_domain.type_.get_atom());
     let MO = output_measure.type_.clone();
+    let k = k as usize;
 
     let optimize = try_!(Optimize::try_from(try_!(to_str(optimize))));
 
@@ -36,6 +38,7 @@ pub extern "C" fn opendp_measurements__make_noisy_max(
         input_domain: &AnyDomain,
         input_metric: &AnyMetric,
         output_measure: &AnyMeasure,
+        k: usize,
         scale: f64,
         optimize: Optimize,
     ) -> Fallible<AnyMeasurement>
@@ -50,10 +53,11 @@ pub extern "C" fn opendp_measurements__make_noisy_max(
             .clone();
         let input_metric = input_metric.downcast_ref::<LInfDistance<TIA>>()?.clone();
         let output_measure = output_measure.downcast_ref::<MO>()?.clone();
-        make_report_noisy_max::<MO, TIA>(
+        make_report_noisy_top_k::<MO, TIA>(
             input_domain,
             input_metric,
             output_measure,
+            k,
             scale,
             optimize,
         )
@@ -66,7 +70,14 @@ pub extern "C" fn opendp_measurements__make_noisy_max(
             (MO, [MaxDivergence, RangeDivergence]),
             (TIA_, [u32, u64, i32, i64, usize, f32, f64])
         ],
-        (input_domain, input_metric, output_measure, scale, optimize)
+        (
+            input_domain,
+            input_metric,
+            output_measure,
+            k,
+            scale,
+            optimize
+        )
     )
     .into()
 }
