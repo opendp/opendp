198728b0b1b9d0060c9fe86bab750498f7aa02b1
Chuck McCallum <mccallucc@gmail.com>
2025-01-09 12:03:37 -0500

    california pums works

--- python/src/opendp/extras/examples/__init__.py
+++ python/src/opendp/extras/examples/__init__.py
@@ -8,3 +8,26 @@ We suggest importing under the conventional name ``dp``:
 
 The methods of this module will then be accessible at ``dp.examples``.    
 '''
+
+def _http_get(url: str) -> str:
+    '''
+    Normally would use the requests library for this, but we want to avoid extra dependencies.
+    '''
+    from urllib.parse import urlparse
+    import http.client
+    parsed = urlparse(url)
+    conn = http.client.HTTPSConnection(parsed.hostname, port=parsed.port)
+    conn.request("GET", parsed.path)
+    response = conn.getresponse()
+    assert response.status == 200
+    body = response.read().decode()
+    conn.close()
+    return body
+
+_california_pums = None
+
+def get_california_pums() -> str:
+    global _california_pums
+    if _california_pums is None:
+        _california_pums = _http_get('https://raw.githubusercontent.com/opendp/opendp/main/docs/source/data/PUMS_california_demographics_1000/data.csv')
+    return _california_pums
--- python/test/extras/test_examples.py
+++ python/test/extras/test_examples.py
@@ -3,6 +3,8 @@ import re
 
 import pytest
 
+import opendp.prelude as dp
+
 @pytest.mark.parametrize("ext", ['ipynb', 'rst'])
 @pytest.mark.parametrize("cmd", ['wget', 'curl'])
 def test_no_shell_http(ext, cmd):
@@ -11,7 +13,10 @@ def test_no_shell_http(ext, cmd):
     mistakes = []
     for path in paths:
         for n, line in enumerate(path.read_text().splitlines()):
-            match = re.search(fr'\b{cmd}\s+\S+', line)
+            match = re.search(fr'\b{cmd}\W+\w\S+', line)
             if match:
                 mistakes.append(f'"{match.group(0)}" on line {n} of {path}')
     assert len(mistakes) == 0, "\n".join(mistakes)
+
+def test_california_pums():
+    assert dp.examples.get_california_pums().startswith('59,1,9,1,0,1')
