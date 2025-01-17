c2fe17eec322ca86a1fac39fc19cac8bd4065873
Chuck McCallum <mccallucc@gmail.com>
2025-01-17 15:26:13 -0500

    failing example: CategoricalDomain

--- python/test/test_serialization.py
+++ python/test/test_serialization.py
@@ -1,8 +1,19 @@
+import pytest
+
 import opendp.prelude as dp
 
 
-def test_atom_domain_serialization():
-    domain = dp.atom_domain(bounds=(0, 10))
-    serialized = dp.serialize(domain)
+@pytest.mark.parametrize(
+    "_,dp_obj",  # Unused parameter gives us a readable test name.
+    [
+        (str(obj), obj)
+        for obj in [
+            dp.atom_domain(bounds=(0, 10)),
+            dp.categorical_domain(['A', 'B', 'C']),
+        ]
+    ],
+)
+def test_atom_domain_serialization(_, dp_obj):
+    serialized = dp.serialize(dp_obj)
     deserialized = dp.deserialize(serialized)
-    assert domain == deserialized
+    assert dp_obj == deserialized
