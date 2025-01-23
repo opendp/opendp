b3e265b2f24fe4b8fb4739983c2c8be1a0190b49
Chuck McCallum <mccallucc@gmail.com>
2025-01-23 14:25:20 -0500

    fix use of private name in test

--- python/test/test_supporting_elements.py
+++ python/test/test_supporting_elements.py
@@ -1,4 +1,5 @@
 import pytest
+import opendp  # So we can access the private _PRIMITIVE_TYPES
 import opendp.prelude as dp
 
 
@@ -18,7 +19,7 @@ def test_partition_distance():
     assert trans.map((3, 4, 3)) == (3, 4, 3)
 
 
-@pytest.mark.parametrize("ty", dp.typing._PRIMITIVE_TYPES)
+@pytest.mark.parametrize("ty", opendp.typing._PRIMITIVE_TYPES)
 def test_atom_domain(ty):
     # Checks that all primitive types are construct-able over FFI.
     # This ensures that all Polars dtypes can be used in debug builds.
