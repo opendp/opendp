b5dff25d12dcded96e4ebedefe2d907f4af72172
Chuck McCallum <mccallucc@gmail.com>
2025-01-24 15:50:07 -0500

    move block of polars tests to bottom; my guess is that the query should not serialize?

--- python/test/test_serialization.py
+++ python/test/test_serialization.py
@@ -55,47 +55,6 @@ def test_serializable(_readable_name, dp_obj):
     assert serialized == dp.serialize(deserialized)
 
 
-# Would normally put the conditional inside the test,
-# but since we need polars at test collection time,
-# in this case it needs to wrap the test.
-pl = import_optional_dependency('polars', raise_error=False)
-if pl is not None:
-    lf = pl.LazyFrame(schema={"A": pl.Int32, "B": pl.String})
-    lf_domain = dp.lazyframe_domain([
-        dp.series_domain("A", dp.atom_domain(T="i32")), 
-        dp.series_domain("B", dp.atom_domain(T=str))
-    ])
-    lf_domain_with_margin = dp.with_margin(lf_domain, by=[], max_partition_length=1000)
-
-    @pytest.mark.parametrize(
-        "_readable_name,dp_obj",
-        [
-            (str(obj), obj)
-            for obj in [
-                lf_domain,
-                lf_domain_with_margin,
-                dp.m.make_private_lazyframe(
-                    lf_domain_with_margin,
-                    dp.symmetric_distance(),
-                    dp.max_divergence(),
-                    lf.select([dp.len(), pl.col("A").dp.sum((0, 1))]),
-                    global_scale=1.0
-                ),
-                dp.m.make_private_expr(
-                    dp.wild_expr_domain([], by=[]),
-                    dp.partition_distance(dp.symmetric_distance()),
-                    dp.max_divergence(),
-                    dp.len(scale=1.0)
-                ),
-            ]
-        ],
-    )
-    def test_serializable_polars(_readable_name, dp_obj):
-        serialized = dp.serialize(dp_obj)
-        deserialized = dp.deserialize(serialized)
-        assert serialized == dp.serialize(deserialized)
-
-
 @pytest.mark.parametrize(
     "_readable_name,dp_obj",
     [
@@ -108,26 +67,6 @@ if pl is not None:
 )
 def test_not_ever_serializable(_readable_name, dp_obj):
     with pytest.raises(Exception, match=r"OpenDP JSON Encoder does not handle"):
-        dp.serialize(dp_obj)
-
-
-@pytest.mark.parametrize(
-    "_readable_name,dp_obj",
-    [
-        (str(obj), obj)
-        for obj in [
-            dp.Context.compositor(
-                data=[5.0] * 100,
-                privacy_unit=dp.unit_of(contributions=1),
-                privacy_loss=dp.loss_of(epsilon=1.0),
-                split_evenly_over=1,
-            ),
-            dp.Queryable('value', 'query_type'),
-        ]
-    ],
-)
-def test_not_currently_serializable(_readable_name, dp_obj):
-    with pytest.raises(Exception, match=r"OpenDP JSON Encoder currently does not handle"):
         dp.serialize(dp_obj)
 
 
@@ -158,3 +97,69 @@ def test_version_mismatch_warning():
     })
     with pytest.warns(UserWarning, match=re.escape('(bad-version) != this version')):
         dp.deserialize(bad_serialized)
+
+
+
+# Would normally put the conditional inside the test,
+# but since we need polars at test collection time,
+# in this case it needs to wrap the tests.
+pl = import_optional_dependency('polars', raise_error=False)
+if pl is not None:
+    lf = pl.LazyFrame(schema={"A": pl.Int32, "B": pl.String})
+    lf_domain = dp.lazyframe_domain([
+        dp.series_domain("A", dp.atom_domain(T="i32")), 
+        dp.series_domain("B", dp.atom_domain(T=str))
+    ])
+    lf_domain_with_margin = dp.with_margin(lf_domain, by=[], max_partition_length=1000)
+
+    context = dp.Context.compositor(
+        data=pl.LazyFrame({"age": [1, 2, 3]}),
+        privacy_unit=dp.unit_of(contributions=1),
+        privacy_loss=dp.loss_of(epsilon=1.0),
+        split_evenly_over=10,
+    )
+    query = context.query().select(dp.len())
+
+    @pytest.mark.parametrize(
+        "_readable_name,dp_obj",
+        [
+            (str(obj), obj)
+            for obj in [
+                lf_domain,
+                lf_domain_with_margin,
+                dp.m.make_private_lazyframe(
+                    lf_domain_with_margin,
+                    dp.symmetric_distance(),
+                    dp.max_divergence(),
+                    lf.select([dp.len(), pl.col("A").dp.sum((0, 1))]),
+                    global_scale=1.0
+                ),
+                dp.m.make_private_expr(
+                    dp.wild_expr_domain([], by=[]),
+                    dp.partition_distance(dp.symmetric_distance()),
+                    dp.max_divergence(),
+                    dp.len(scale=1.0)
+                ),
+                query, # TODO: Should not be serializable, because it has internal state.
+            ]
+        ],
+    )
+    def test_serializable_polars(_readable_name, dp_obj):
+        serialized = dp.serialize(dp_obj)
+        deserialized = dp.deserialize(serialized)
+        assert serialized == dp.serialize(deserialized)
+
+
+    @pytest.mark.parametrize(
+        "_readable_name,dp_obj",
+        [
+            (str(obj), obj)
+            for obj in [
+                context,
+                dp.Queryable('value', 'query_type'),
+            ]
+        ],
+    )
+    def test_not_currently_serializable(_readable_name, dp_obj):
+        with pytest.raises(Exception, match=r"OpenDP JSON Encoder currently does not handle"):
+            dp.serialize(dp_obj)
