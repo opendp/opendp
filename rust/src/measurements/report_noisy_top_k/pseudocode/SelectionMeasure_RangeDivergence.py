29bb36cfa611c60adb6734c913c378c32bdf7f5f
Shoeboxam <shoeboxam@gmail.com>
2025-02-19 15:01:02 -0500

    rewrite proof stack

---
+++ rust/src/measurements/report_noisy_top_k/pseudocode/SelectionMeasure_RangeDivergence.py
@@ -0,0 +1,18 @@
+# type: ignore
+class RangeDivergence(SelectionMeasure):
+    ONE_SHOT = True
+    RV = GumbelRV
+
+    @staticmethod
+    def random_variable(shift: FBig, scale: FBig) -> GumbelRV:
+        return GumbelRV(shift=shift, scale=scale)
+    
+    @staticmethod
+    def privacy_map(d_in: f64, scale: f64, k: usize) -> f64:
+        if d_in < 0:
+            raise ValueError("input distance must be non-negative")
+
+        if scale.is_zero():
+            return f64.INFINITY
+
+        return d_in.inf_div(scale).inf_mul(f64.inf_cast(k))
