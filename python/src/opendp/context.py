e5f7caf8ac6f0d8bbdf837091c970c9f9abed303
Chuck McCallum <mccallucc@gmail.com>
2025-02-19 15:07:02 -0500

    fix typo

--- python/src/opendp/context.py
+++ python/src/opendp/context.py
@@ -390,8 +390,8 @@ class Context(object):
         
         new_plan = pl.LazyFrame.deserialize(io.BytesIO(serialized_plan))
         new_query = self.query()
-	if not isinstance(new_query, LazyFrameQuery):
-		raise ValueError("deserialize_polars_plan may only be used when data is a LazyFrame")
+        if not isinstance(new_query, LazyFrameQuery):
+            raise ValueError("deserialize_polars_plan may only be used when data is a LazyFrame")
 
         new_query.polars_plan = new_plan
         return new_query
