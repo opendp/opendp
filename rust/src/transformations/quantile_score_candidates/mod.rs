2da3b934370771cabfcc4b8907cea6730928ff69
Shoeboxam <shoeboxam@gmail.com>
2025-04-13 14:47:19 -0400

    chore: consolidate discrete quantile scorers

--- rust/src/transformations/quantile_score_candidates/mod.rs
+++ rust/src/transformations/quantile_score_candidates/mod.rs
@@ -1,15 +1,14 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, iter::zip};
 
 use dashu::{integer::UBig, rational::RBig};
-use num::NumCast;
-use opendp_derive::bootstrap;
+use opendp_derive::{bootstrap, proven};
 
 use crate::{
     core::{Function, MetricSpace, StabilityMap, Transformation},
     domains::{AtomDomain, VectorDomain},
     error::Fallible,
     metrics::{IntDistance, LInfDistance},
-    traits::{ExactIntCast, Integer, Number, RoundCast},
+    traits::{AlertingMul, ExactIntCast, InfDiv, Integer, Number, RoundCast},
 };
 
 use super::traits::UnboundedMetric;
@@ -17,13 +16,15 @@ use super::traits::UnboundedMetric;
 #[cfg(feature = "ffi")]
 mod ffi;
 
+#[cfg(test)]
+mod test;
+
 #[bootstrap(
     features("contrib"),
     generics(MI(suppress), TIA(suppress)),
     derived_types(TIA = "$get_atom(get_type(input_domain))")
 )]
 /// Makes a Transformation that scores how similar each candidate is to the given `alpha`-quantile on the input dataset.
-///
 ///
 /// # Arguments
 /// * `input_domain` - Uses a smaller sensitivity when the size of vectors in the input domain is known.
@@ -42,27 +43,39 @@ pub fn make_quantile_score_candidates<MI: UnboundedMetric, TIA: Number>(
 ) -> Fallible<
     Transformation<
         VectorDomain<AtomDomain<TIA>>,
-        VectorDomain<AtomDomain<usize>>,
+        VectorDomain<AtomDomain<u64>>,
         MI,
-        LInfDistance<usize>,
+        LInfDistance<u64>,
     >,
 >
 where
     (VectorDomain<AtomDomain<TIA>>, MI): MetricSpace,
 {
     if input_domain.element_domain.nan() {
-        return fallible!(MakeTransformation, "input must be non-null");
+        return fallible!(
+            MakeTransformation,
+            "input_domain members must have non-nan elements"
+        );
     }
 
-    validate_candidates(&candidates)?;
+    check_candidates(&candidates)?;
 
-    let (alpha_num, alpha_den, size_limit) = score_candidates_constants(input_domain.size, alpha)?;
+    let (alpha_num, alpha_den, size_limit) = score_candidates_constants(
+        input_domain.size.map(u64::exact_int_cast).transpose()?,
+        alpha,
+    )?;
 
-    Transformation::<_, VectorDomain<AtomDomain<usize>>, _, _>::new(
+    Transformation::<_, VectorDomain<AtomDomain<u64>>, _, _>::new(
         input_domain.clone(),
         VectorDomain::default().with_size(candidates.len()),
         Function::new(move |arg: &Vec<TIA>| {
-            compute_score(arg.clone(), &candidates, alpha_num, alpha_den, size_limit)
+            Vec::from_iter(score_candidates(
+                arg.iter().cloned(),
+                candidates.clone(),
+                alpha_num,
+                alpha_den,
+                size_limit,
+            ))
         }),
         input_metric,
         LInfDistance::default(),
@@ -74,32 +87,49 @@ where
     )
 }
 
-pub(crate) fn validate_candidates<T: Number>(candidates: &Vec<T>) -> Fallible<()> {
+/// # Proof Definition
+/// Raises an error if:
+/// * `candidates` is empty
+/// * `candidates` is not strictly increasing
+/// * `candidates` is not totally ordered
+#[proven(proof_path = "transformations/quantile_score_candidates/check_candidates.tex")]
+pub(crate) fn check_candidates<T: Number>(candidates: &Vec<T>) -> Fallible<()> {
     if candidates.is_empty() {
         return fallible!(MakeTransformation, "candidates must be non-empty");
     }
     if candidates.windows(2).any(|w| {
         w[0].partial_cmp(&w[1])
+            // NaN always evaluates to false
             .map(|c| c != Ordering::Less)
             .unwrap_or(true)
     }) {
         return fallible!(
             MakeTransformation,
-            "candidates must be non-null and increasing"
+            "candidates must be non-null and strictly increasing"
         );
     }
     Ok(())
 }
 
-pub(crate) fn score_candidates_constants<T: Integer>(
-    size: Option<T>,
+/// Returns the following constants:
+/// * `alpha_num`
+/// * `alpha_den`
+/// * `size_limit`
+///
+/// The constants are chosen to ensure that `alpha_num / alpha_den` is approximately equal to `alpha`,
+/// in a way that minimizes `alpha_den` to maximize `size_limit`.
+///
+/// # Proof Definition
+/// The constants follow the following properties:
+/// * $alpha_num / alpha_den \in [0, 1]$
+/// * $size_limit \cdot alpha_den < 2^{64}$
+///
+/// An error is raised if these properties cannot be met.
+#[proven(proof_path = "transformations/quantile_score_candidates/score_candidates_constants.tex")]
+pub(crate) fn score_candidates_constants(
+    size: Option<u64>,
     alpha: f64,
-) -> Fallible<(T, T, T)>
-where
-    f64: RoundCast<T>,
-    T: RoundCast<f64> + NumCast,
-    UBig: From<T>,
-{
+) -> Fallible<(u64, u64, u64)> {
     if !(0.0..=1.0).contains(&alpha) {
         return fallible!(MakeTransformation, "alpha must be within [0, 1]");
     }
@@ -109,33 +139,47 @@ where
     let alpha_den_approx = if let Some(size) = size {
         // choose the finest granularity that won't overflow
         // must have that size * denom < MAX, so let denom = MAX // size
-        T::MAX_FINITE.neg_inf_div(&size)?
+        u64::MAX.neg_inf_div(&size)?
     } else {
         // default to an alpha granularity of .00001
-        T::exact_int_cast(10_000)?
+        u64::exact_int_cast(10_000)?
     };
 
     let (alpha_num, alpha_den) = if alpha_den_exact < UBig::from(alpha_den_approx) {
-        T::from(alpha_num_exact.into_parts().1)
-            .zip(T::from(alpha_den_exact))
-            .unwrap()
+        (
+            u64::try_from(alpha_num_exact.into_parts().1)?,
+            u64::try_from(alpha_den_exact)?,
+        )
     } else {
         // numer = alpha * denom
-        let alpha_num_approx = T::round_cast(alpha * f64::round_cast(alpha_den_approx.clone())?)?;
+        let alpha_num_approx = u64::round_cast(alpha * f64::round_cast(alpha_den_approx.clone())?)?;
         (alpha_num_approx, alpha_den_approx)
     };
 
     let size_limit = if let Some(size_limit) = size {
-        // ensures that there is no overflow
-        size_limit.alerting_mul(&alpha_den)?;
         size_limit
     } else {
-        T::MAX_FINITE.neg_inf_div(&alpha_den)?
+        u64::MAX.neg_inf_div(&alpha_den)?
     };
+
+    // both of these are un-fail-able, but they make the proof easy
+    assert!(alpha_num <= alpha_den);
+    size_limit.alerting_mul(&alpha_den)?;
 
     Ok((alpha_num, alpha_den, size_limit))
 }
 
+/// # Proof Definition
+/// Assume `alpha_den >= alpha_num`.
+///
+/// If `known_size` is set,
+/// then returns a function that computes $\texttt{d\_in} // 2 \cdot \texttt{alpha\_den}$
+/// for argument $\texttt{d\_in}$.
+///
+/// Otherwise,
+/// returns a function that computes $\texttt{d\_in} \cdot \max(\texttt{alpha\_num}, \texttt{alpha\_den} - \texttt{alpha\_num})$,
+/// for argument $\texttt{d\_in}$.
+#[proven(proof_path = "transformations/quantile_score_candidates/score_candidates_map.tex")]
 pub(crate) fn score_candidates_map<T: Integer + ExactIntCast<IntDistance>>(
     alpha_num: T,
     alpha_den: T,
@@ -143,7 +187,7 @@ pub(crate) fn score_candidates_map<T: Integer + ExactIntCast<IntDistance>>(
 ) -> impl Fn(&IntDistance) -> Fallible<T> {
     move |d_in| {
         if known_size {
-            T::exact_int_cast(d_in / 2 * 2)? // round down to even
+            T::exact_int_cast(d_in / 2)? // round down to even
                 .alerting_mul(&alpha_den)
         } else {
             let abs_dist_const = alpha_num.max(alpha_den - alpha_num);
@@ -156,15 +200,16 @@ pub(crate) fn score_candidates_map<T: Integer + ExactIntCast<IntDistance>>(
 ///
 /// # Proof Definition
 ///
-/// Under the precondition that `x` is non-null,
+/// Under the precondition that `x` is totally ordered,
+/// that `x` has no greater than $2^{64}$ elements,
 /// that `candidates` is strictly increasing,
-/// that `alpha_numer / alpha_denom` is in [0, 1],
-/// and that `size_limit * alpha_denom` does not overflow,
+/// that `alpha_numer / alpha_denom <= 1`,
+/// and that `size_limit * alpha_denom < 2^{64}`,
 /// computes the score of each candidate in `candidates` on the dataset `x`.
 ///
 /// The score for each `c` in `candidates` is computed as follows:
 /// |alpha_denom * min(#(x < c), size_limit) -
-///  alpha_numer * min(n - #(x = c)), size_limit)|
+///  alpha_numer * min(#(x > c)), size_limit)|
 ///
 /// # Intuition
 /// Lower score is better.
@@ -178,141 +223,48 @@ pub(crate) fn score_candidates_map<T: Integer + ExactIntCast<IntDistance>>(
 /// * `alpha_num` - numerator of alpha fraction
 /// * `alpha_den` - denominator of alpha fraction. alpha fraction is {0: min, 0.5: median, 1: max, ...}
 /// * `size_limit` - maximum size of `x`. If `x` is larger than `size_limit`, scores are truncated
-pub(crate) fn compute_score<TIA: PartialOrd>(
-    mut x: Vec<TIA>,
-    candidates: &Vec<TIA>,
-    alpha_num: usize,
-    alpha_den: usize,
-    size_limit: usize,
-) -> Vec<usize> {
-    // x must be sorted because counts are done via binary search
-    x.sort_by(|a, b| a.partial_cmp(&b).unwrap_or(Ordering::Equal));
+#[proven(proof_path = "transformations/quantile_score_candidates/score_candidates.tex")]
+pub(crate) fn score_candidates<TIA: PartialOrd>(
+    x: impl Iterator<Item = TIA>,
+    candidates: Vec<TIA>,
+    alpha_num: u64,
+    alpha_den: u64,
+    size_limit: u64,
+) -> impl Iterator<Item = u64> {
+    // count of the number of records between...
+    //  (-inf, c1), [c1, c2), [c2, c3), ..., [ck, inf)
+    let mut hist_ro = vec![0u64; candidates.len() + 1]; // histogram of right-open intervals
+    //  (-inf, c1], (c1, c2], (c2, c3], ..., (ck, inf)
+    let mut hist_lo = vec![0u64; candidates.len() + 1]; // histogram of left-open intervals
 
-    // compute #(`x` < c) and #(`x` = c) for each c in candidates
-    let mut num_lt = vec![0; candidates.len()];
-    let mut num_eq = vec![0; candidates.len()];
-    count_lt_eq_recursive(
-        num_lt.as_mut_slice(),
-        num_eq.as_mut_slice(),
-        candidates.as_slice(),
-        x.as_slice(),
-        0,
-    );
+    x.for_each(|x_i| {
+        let idx_lt = candidates.partition_point(|c| *c < x_i);
+        hist_lo[idx_lt] += 1;
 
-    // now that we have num_lt and num_eq for each candidate, score all candidates
-    num_lt
-        .into_iter()
-        .zip(num_eq.into_iter())
-        // score function cannot overflow.
-        //     lt <= size_limit, so 0 <= alpha_denom * lt <= usize::MAX
-        //     n - eq <= size_limit, so 0 <= size_limit - eq
-        .map(|(lt, eq)| {
-            // |α_den * #(x < c) - α_num * (n - #(x = c))|
-            (alpha_den * lt.min(size_limit)).abs_diff(alpha_num * (x.len() - eq).min(size_limit))
-        })
-        .collect()
+        let idx_eq = idx_lt + candidates[idx_lt..].partition_point(|c| *c == x_i);
+        hist_ro[idx_eq] += 1;
+    });
+
+    let n: u64 = hist_lo.iter().sum();
+
+    // don't care about the number of elements greater than all candidates
+    hist_ro.pop();
+    hist_lo.pop();
+
+    let (mut lt, mut le) = (0u64, 0u64);
+
+    zip(hist_ro, hist_lo).map(move |(ro, lo)| {
+        // cumsum the right-open histogram to get the total number of records less than the candidate
+        lt += ro;
+        // cumsum the left-open histogram to get the total number of records lt or equal to the candidate
+        le += lo;
+
+        let gt = n - le;
+
+        // ensures the score calculation won't overflow
+        let (lt_lim, gt_lim) = (lt.min(size_limit), gt.min(size_limit));
+
+        // |     (1 - α)         * #(x < c)    -           α * #(x > c)| * α_den
+        ((alpha_den - alpha_num) * lt_lim).abs_diff(alpha_num * gt_lim)
+    })
 }
-
-/// Compute number of elements less than and equal to each edge
-/// Formula is (#(`x` < `e`), #(`x` == `e`)) for each e in `edges`.
-///
-/// # Arguments
-/// * `count_lt` - location to write the count of elements less than each edge
-/// * `count_eq` - location to write the count of elements equal to each edge
-/// * `edges` - edges to collect counts for. Must be sorted
-/// * `x` - dataset to count against
-/// * `x_start_idx` - value to add to the count. Useful for recursion on subslices
-fn count_lt_eq_recursive<TI: PartialOrd>(
-    count_lt: &mut [usize],
-    count_eq: &mut [usize],
-    edges: &[TI],
-    x: &[TI],
-    x_start_idx: usize,
-) {
-    if edges.is_empty() {
-        return;
-    }
-    if edges.len() == 1 {
-        let (num_lt, num_eq) = count_lt_eq(x, &edges[0]);
-        count_lt[0] = x_start_idx + num_lt;
-        count_eq[0] = num_eq;
-        return;
-    }
-    // use binary search to find #(x < middle edge) = |{i; x[i] < middle edge}|
-    let mid_edge_idx = (edges.len() + 1) / 2;
-    let mid_edge = &edges[mid_edge_idx];
-    let (num_lt, num_eq) = count_lt_eq(x, mid_edge);
-    count_lt[mid_edge_idx] = x_start_idx + num_lt;
-    count_eq[mid_edge_idx] = num_eq;
-
-    count_lt_eq_recursive(
-        &mut count_lt[..mid_edge_idx],
-        &mut count_eq[..mid_edge_idx],
-        &edges[..mid_edge_idx],
-        &x[..num_lt + num_eq],
-        x_start_idx,
-    );
-
-    count_lt_eq_recursive(
-        &mut count_lt[mid_edge_idx + 1..],
-        &mut count_eq[mid_edge_idx + 1..],
-        &edges[mid_edge_idx + 1..],
-        &x[num_lt + num_eq..],
-        x_start_idx + num_lt + num_eq,
-    );
-}
-
-/// Find the number of elements in `x` < `target` and number of elements in `x` == `target`.
-/// Formula is (#(`x` < `target`), #(`x` == `target`))
-///
-/// # Arguments
-/// * `x` - dataset to count against
-/// * `target` - value to compare against
-fn count_lt_eq<TI: PartialOrd>(x: &[TI], target: &TI) -> (usize, usize) {
-    if x.is_empty() {
-        return (0, 0);
-    }
-    let (mut lower, mut upper) = (0, x.len());
-    let mut eq_upper = upper;
-
-    while upper - lower > 1 {
-        let middle = lower + (upper - lower) / 2;
-
-        if &x[middle] < target {
-            lower = middle;
-        } else {
-            upper = middle;
-            // tighten eq_upper to last middle where x[middle] was still greater than target
-            if &x[middle] > target {
-                eq_upper = middle;
-            }
-        }
-    }
-    let lt = if &x[lower] < target { upper } else { lower };
-
-    // run another search to find the greatest index equal to target
-    // search starting from the first index equal to target
-    lower = lt;
-    // search for the smallest middle where x[middle] is greater than target
-    upper = eq_upper;
-    while upper - lower > 1 {
-        let middle = lower + (upper - lower) / 2;
-
-        if &x[middle] == target {
-            lower = middle;
-        } else {
-            upper = middle;
-        }
-    }
-
-    let eq = if lower == upper || &x[lower] == target {
-        upper
-    } else {
-        lower
-    } - lt;
-
-    (lt, eq)
-}
-
-#[cfg(test)]
-mod test;
