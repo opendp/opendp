6c6113c231081f19e0b665ce0952081dfbeb9b4d
Michael Shoemate <shoeboxam@gmail.com>
2025-05-08 12:00:09 -0400

    feat: prove `make_private_group_by` (#2166)
    Close #2165

--- rust/src/measurements/make_private_lazyframe/group_by/pseudocode/make_private_group_by.py
+++ rust/src/measurements/make_private_lazyframe/group_by/pseudocode/make_private_group_by.py
@@ -1,109 +1,187 @@
 # type: ignore
 def make_private_group_by(
-    input_domain,
-    input_metric,
-    output_measure,
-    plan,
-    global_scale,
-    threshold,
+    input_domain: DslPlanDomain,
+    input_metric: FrameDistance[MI],
+    output_measure: MO,
+    plan: DslPlan,
+    global_scale: Optional[f64],
+    threshold: Optional[u32],
 ):
-    input_expr, keys, aggs, predicate = match_group_by(plan)
+    input, group_by, aggs, key_sanitizer = match_group_by(plan) # |\label{line:match-group-by}|
 
-    t_prior = input_expr.make_stable(input_domain, input_metric)  # |\label{line:tprior}|
+    # 1: establish stability of `group_by` |\label{line:input_stability}|
+    t_prior = input.make_stable(input_domain, input_metric)  # |\label{line:tprior}|
     middle_domain, middle_metric = t_prior.output_space()
 
-    by = match_grouping_columns(keys)  # |\label{line:groupcols}|
+    for expr in group_by: # |\label{line:group-by-stability}|
+        # grouping keys must be stable
+        t_group_by = expr.make_stable(
+            WildExprDomain(
+                columns=middle_domain.series_domains,
+                context=ExprContext.RowByRow,
+            ),
+            L0PInfDistance(middle_metric[0]),
+        )
 
-    margin = (
-        middle_domain
-        .margins
-        .get(by, Margin.default())
-    )  # |\label{line:margin}|
+        series_domain = t_group_by.output_domain.column
+        try:
+            domain = series_domain.element_domain(CategoricalDomain)
+        except Exception:
+            pass
+            
+        if domain is not None and domain.categories() is None:
+            raise "Categories are data-dependent, which may reveal sensitive record ordering."
 
-    # prepare a joint measurement over all expressions
-    expr_domain = ExprDomain(  # |\label{line:joint}|
-        middle_domain,
-        ExprContext.Aggregate(by),
-    )
+    group_by_id = HashSet.from_iter(group_by)  # |\label{line:groupcols}|
+    margin = middle_domain.get_margin(group_by_id)  # |\label{line:margin}|
 
-    m_exprs = make_composition([
+    # 2: prepare for release of `aggs` |\label{line:prep-release-aggs}|
+    match key_sanitizer:
+        case KeySanitizer.Join(keys):
+            num_keys = LazyFrame.from_(keys).select([len()]).collect()
+            margin.max_num_partitions = num_keys.column("len").u32().last()  # |\label{line:keys-len}|
+            is_join = True
+        case _:
+            is_join = False
+
+    m_expr_aggs = [
         make_private_expr(
-             expr_domain,
-             PartitionDistance(middle_metric),
-             output_measure,
-             expr,
-             global_scale,
+            WildExprDomain(  # |\label{line:joint}|
+                columns=middle_domain.series_domains,
+                context=ExprContext.Aggregation(margin),
+            ),
+            PartitionDistance(middle_metric),
+            output_measure,
+            expr,
+            global_scale,
         ) for expr in aggs
-    ])
+    ]
+    m_aggs = make_composition(m_expr_aggs)
 
-    # reconcile information about the threshold |\label{line:reconcile-threshold}|
-    dp_exprs = m_exprs.invoke((input_expr, all()))
+    f_comp = m_aggs.function
+    f_privacy_map = m_aggs.privacy_map
 
-    if margin.invariant is not None:
+    # 3: prepare for release of `keys` |\label{line:prep-release-keys}|
+    dp_exprs, null_exprs = zip(*((plan.expr, plan.fill) for plan in m_aggs.invoke(input)))
+
+    # 3.2: reconcile information about the threshold |\label{line:reconcile-threshold}|
+    if margin.invariant is not None or is_join:  # |\label{line:no-thresholding}|
         threshold_info = None
-    elif is_threshold_predicate(predicate) is not None:
-        name, threshold_value = is_threshold_predicate(predicate)
+    elif match_filter(key_sanitizer) is not None:  # |\label{line:sanitizer-threshold}|
+        name, threshold_value = match_filter(key_sanitizer)
         noise = find_len_expr(dp_exprs, name)[1]
         threshold_info = name, noise, threshold_value, False
-    elif threshold is not None:
-        name, noise = find_len_expr(dp_exprs, None)[1]
-        threshold_info = name, noise, threshold_value, True
-    else:
-        raise f"The key set of {by} is private and cannot be released."
+    elif threshold is not None:  # |\label{line:constructor-threshold}|
+        name, noise = find_len_expr(dp_exprs, None)
+        threshold_info = name, noise, threshold, True
+    else:  # |\label{line:not-private}|
+        raise f"The key set of {group_by_id} is private and cannot be released."
 
-    # prepare the final_predicate to be used in the function |\label{line:final-predicate}|
-    if threshold_info is not None:
+    # 3.3: update key sanitizer |\label{line:final-sanitizer}|
+    if threshold_info is not None:  # |\label{line:incorporate-threshold}|
         name, _, threshold_value, is_present = threshold_info
-        if not is_present and predicate is not None:
-            final_predicate = threshold_expr.and_(predicate)
+        threshold_expr = col(name).gt(lit(threshold_value))
+        if not is_present and predicate is not None:  # |\label{line:new-threshold-sanitizer}|
+            key_sanitizer = KeySanitizer.Filter(threshold_expr.and_(predicate))
         else:
-            final_predicate = threshold_expr
-    else:
-        final_predicate = predicate
+            key_sanitizer = KeySanitizer.Filter(threshold_expr)
+
+    elif isinstance(key_sanitizer, KeySanitizer.Join):  # |\label{line:incorporate-join-fill}|
+        key_sanitizer.fill_null = []
+        for dp_expr, null_expr in zip(dp_exprs, null_exprs):
+            name = dp_expr.meta().output_name()
+            if null_expr is None:
+                raise f"fill expression for {name} is unknown"
+
+            key_sanitizer.fill_null.append(col(name).fill_null(null_expr))
     
-    # prepare supporting elements
-    def function(arg):  # |\label{line:function}|
+    # 4: build final measurement |\label{line:build-meas}|
+    def function(arg: DslPlan) -> DslPlan:  # |\label{line:function}|
         output = DslPlan.GroupBy(
             input=arg,
-            keys=keys,
-            aggs=m_exprs((arg, all())),
+            keys=group_by,
+            aggs=[p.expr for p in f_comp.eval(arg)],
             apply=None,
             maintain_order=False,
         )
+        match key_sanitizer:
+            case KeySanitizer.Filter(predicate):
+                output = DslPlan.Filter(input=output, predicate=predicate)
+            case KeySanitizer.Join(
+                how,
+                left_on,
+                right_on,
+                options,
+                fill_null,
+                keys=labels,
+            ):
+                match how:  # |\label{line:join-how}|
+                    case JoinType.Left:
+                        input_left, input_right = labels, output
+                    case JoinType.Right:
+                        input_left, input_right = output, labels
+                    case _:
+                        raise "unreachable"
 
-        if final_predicate is not None:
-            output = DslPlan.Filter(
-                input=output,
-                predicate=final_predicate,
-            )
+                output = DslPlan.HStack(
+                    input=DslPlan.Join(
+                        input_left,
+                        input_right,
+                        left_on,
+                        right_on,
+                        options,
+                        predicates=[],
+                    ),
+                    exprs=fill_null,
+                    options=ProjectionOptions.default(),
+                )
         return output
-    
-    def privacy_map(d_in):  # |\label{line:privacy-map}|
-        l0 = margin.l_0(d_in)
-        li = margin.l_inf(d_in)
-        l1 = l0.inf_mul(li).min(d_in)
-        
-        d_out = m_exprs.map((l0, l1, li))
 
-        if threshold is not None:
-            _, plugin, threshold_value = threshold_info
+    def privacy_map(d_in: Bounds):  # |\label{line:privacy-map}|
+        bound = d_in.get_bound(group_by_id)
+
+        # incorporate all information into optional bounds
+        l0 = option_min(bound.num_groups, margin.max_groups)
+        li = option_min(bound.per_group, margin.max_length)
+        l1 = d_in.get_bound(HashSet.new()).per_group  # |\label{line:trivial-l1}|
+
+        # reduce optional bounds to concrete bounds
+        if l0 is not None and l1 is not None and li is not None:
+            pass
+        elif l1 is not None:
+            l0 = l0 or l1 # |\label{line:l0-from-l1}|
+            li = li or l1 # |\label{line:li-from-l1}|
+        elif l0 is not None and li is not None:
+            l1 = l0.inf_mul(li) # |\label{line:l1-from-l0-li}|
+        else: # |\label{line: helpful_error_message}|            
+            raise f"num_groups ({l0}), total contributions ({l1}), and per_group ({li}) are not sufficiently well-defined."
+
+        d_out = f_privacy_map.eval((l0, l1, li))
+
+        if margin.invariant is not None or is_join:  # |\label{line:privacy-map-static-keys}|
+            pass
+        elif threshold_info is not None:  # |\label{line:privacy-map-threshold}|
+            _, noise, threshold_value, _ = threshold_info
             if li >= threshold_value:
                 raise f"Threshold must be greater than {li}."
-            d_instability = f64.inf_cast(threshold_value.inf_sub(li))
-            delta_single = integrate_discrete_noise_tail(plugin.distribution, plugin.scale, d_instability)
-            delta_joint = 1 - (1 - delta_single).inf_powi(l0)
+            
+            d_instability = threshold_value.neg_inf_sub(li)
+            delta_single = integrate_discrete_noise_tail(
+                noise.distribution, noise.scale, d_instability
+            )
+            delta_joint = (1).inf_sub(
+                (1).neg_inf_sub(delta_single).neg_inf_powi(IBig.from_(l0))
+            )
             d_out = MO.add_delta(d_out, delta_joint)
-        elif margin.invariant is None:
-            raise "keys must be public if threshold is unknown"
+        else:
+            raise "the key-set is sensitive"
 
         return d_out
 
-    m_group_by_agg = Measurement(
+    return t_prior >> Measurement.new(
         middle_domain,
         function,
         middle_metric,
         output_measure,
         privacy_map,
     )
-
-    return t_prior >> m_group_by_agg
