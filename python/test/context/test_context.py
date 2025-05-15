3c1fcd5df19619e2a23cc911f77b5695decb5d5e
Shoeboxam <shoeboxam@gmail.com>
2025-05-15 13:40:17 -0400

    respond to review 5/15

--- python/test/context/test_context.py
+++ python/test/context/test_context.py
@@ -89,7 +89,8 @@ def test_context_repr():
         input_metric   = SymmetricDistance(),
         output_measure = MaxDivergence),
     d_in       = 3,
-    d_mids     = [3.0])'''
+    d_mids     = [3.0],
+    d_out      = None)'''
 
     assert repr(
         dp.Context.compositor(
@@ -103,6 +104,7 @@ def test_context_repr():
         input_metric   = SymmetricDistance(),
         output_measure = MaxDivergence),
     d_in       = 3,
+    d_mids     = None,
     d_out      = 3.0)'''
 
     assert repr(
@@ -116,7 +118,9 @@ def test_context_repr():
         input_domain   = VectorDomain(AtomDomain(T=i32)),
         input_metric   = SymmetricDistance(),
         output_measure = MaxDivergence),
-    d_in       = 3)'''
+    d_in       = 3,
+    d_mids     = None,
+    d_out      = None)'''
 
 
 def test_context_init_split_by_weights():
@@ -216,7 +220,8 @@ def test_query_repr():
             input_metric   = SymmetricDistance(),
             output_measure = MaxDivergence),
         d_in       = 1,
-        d_mids     = [1.0]))'''
+        d_mids     = [1.0],
+        d_out      = None))'''
 
 
 def test_subcontext_changes_metric():
