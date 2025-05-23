acfe252052984bd0f78fea495c3c3d81b857565f
maxine-park <maxinempark@gmail.com>
2025-05-23 02:27:05 -0400

    updated requirements-dev.in to have the private-pgm installation

--- python/src/opendp/extras/__init__.py
+++ python/src/opendp/extras/__init__.py
@@ -3,3 +3,5 @@ The ``extras`` module contains pure-python DP tools.
 These are not yet supported by the Rust or R bindings.
 Some tools require extra, optional Python dependencies, and are grouped under corresponding namespaces.
 '''
+
+from . import synthetic # maxine_edit: added this to get dp.synthetic to work?
