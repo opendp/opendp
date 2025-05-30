09e39a94e949182a5c89adc6f6c861d5c0b1ea55
Chuck McCallum <mccallucc@gmail.com>
2025-05-30 17:14:26 -0400

    fill out test

--- python/test/extras/test_private_theil_sen.py
+++ python/test/extras/test_private_theil_sen.py
@@ -1,3 +1,4 @@
+import numpy as np
 from opendp.extras.numpy import make_private_theil_sen
 
 
@@ -6,3 +7,14 @@ def test_private_theil_sen():
     y_bounds = -10, 10
     meas = make_private_theil_sen(x_bounds, y_bounds, scale=1.0)
     assert meas.map(1) == 2
+
+    def f(x):
+        return x * 2 + 1
+
+    x = np.random.normal(size=100, loc=0, scale=1.0)
+    y = f(x) + np.random.normal(size=100, loc=0, scale=0.5)
+
+    slope, intercept = meas(np.stack([x, y], axis=1))
+    # There is a non-zero chance these may fail.
+    assert 1.5 < slope < 2.5
+    assert 0.5 < intercept < 1.5
