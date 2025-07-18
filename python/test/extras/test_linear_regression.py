e866c33638a421b45fa61807af57f0c7fa71b5bc
Chuck McCallum <mccallucc@gmail.com>
2025-07-18 11:50:54 -0400

    ignore typing where we intentionally supply bad input

--- python/test/extras/test_linear_regression.py
+++ python/test/extras/test_linear_regression.py
@@ -30,10 +30,10 @@ def test_private_theil_sen():
 def test_input_validation():
     with optional_dependency('numpy'):
         with pytest.raises(Exception, match=re.escape("For now, the x_bounds array must consist of a single tuple, not [0, 10]")):
-            dp.sklearn.linear_model.LinearRegression().fit(
+            dp.sklearn.linear_model.LinearRegression().fit( # type: ignore
                 X=[[1], [2], [3], [4], [5]],
                 y=[1, 2, 3, 4, 5],
-                x_bounds=(0, 10),
+                x_bounds=(0, 10), # type: ignore
                 y_bounds=(0, 10),
                 scale=1,
             )
