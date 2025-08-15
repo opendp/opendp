e6aba9f3993012f2b2427ca7a46366ef9bd41789
Chuck McCallum <mccallucc@gmail.com>
2025-08-15 11:57:54 -0400

    move more params up to constructor

--- python/src/opendp/extras/sklearn/linear_model/__init__.py
+++ python/src/opendp/extras/sklearn/linear_model/__init__.py
@@ -19,7 +19,6 @@ from opendp.extras.sklearn.linear_model._make_private_theil_sen import (
     make_private_theil_sen as _make_private_theil_sen,
 )  # noqa: F401
 from opendp._lib import import_optional_dependency
-from opendp.mod import assert_features
 
 
 class LinearRegression:
@@ -30,31 +29,38 @@ class LinearRegression:
     `LinearRegression <https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html>`_.
     The ``fit`` method returns an sklearn ``LinearRegression`` object.
 
+    :param x_bounds: Bounds for training data; For the moment, only lists containing a single tuple are supported
+    :param y_bounds: Bounds for target data
+    :param scale: The scale of the noise to be added
+    :param runs: Controls how many times randomized pairwise predictions are computed. Increasing this value can improve the robustness and accuracy of the results; However, it can also increase computational cost and amount of noise needed later in the algorithm.
+    :param candidates_count: How many evenly spaced candidates to generate
     :param fraction_bounds: Lower and upper bounds x_bounds to retain when ``fit`` is called.
     """
-    def __init__(self, fraction_bounds: tuple[float, float] = (0.25, 0.75)):
+    def __init__(
+            self,
+            x_bounds: Iterable[tuple[float, float]],
+            y_bounds: tuple[float, float],
+            scale: float,
+            runs: int = 1,
+            candidates_count: int = 100,
+            fraction_bounds: tuple[float, float] = (0.25, 0.75)):
+        self.x_bounds = x_bounds
+        self.y_bounds = y_bounds
+        self.scale = scale
+        self.runs = runs
+        self.candidates_count = candidates_count 
         self.fraction_bounds = fraction_bounds
 
     def fit(
         self,
         X,
         y,
-        x_bounds: Iterable[tuple[float, float]],
-        y_bounds: tuple[float, float],
-        scale: float,
-        runs: int = 1,
-        candidates_count: int = 100,
     ):
         """
         Fit DP linear model.
 
         :param X: Training data. Array-like of shape (n_samples, 1)
         :param y: Target values. Array-like of shape (n_samples,)
-        :param x_bounds: Bounds for training data; For the moment, only lists containing a single tuple are supported
-        :param y_bounds: Bounds for target data
-        :param scale: The scale of the noise to be added
-        :param runs: Controls how many times randomized pairwise predictions are computed. Increasing this value can improve the robustness and accuracy of the results; However, it can also increase computational cost and amount of noise needed later in the algorithm.
-        :param candidates_count: How many evenly spaced candidates to generate
         :return: A fitted sklearn ``LinearRegression``
 
         :example:
@@ -66,25 +72,26 @@ class LinearRegression:
         ...     import pytest
         ...     pytest.skip('Requires extra install')
         >>> dp.enable_features("floating-point")
-        >>> lin_reg = dp.sklearn.linear_model.LinearRegression().fit(
-        ...     X=[[1], [2], [3], [4], [5]],
-        ...     y=[1, 2, 3, 4, 5],
+        >>> lin_reg = dp.sklearn.linear_model.LinearRegression(
         ...     x_bounds=[(0, 10)],
         ...     y_bounds=(0, 10),
         ...     scale=1,
+        ... ).fit(
+        ...     X=[[1], [2], [3], [4], [5]],
+        ...     y=[1, 2, 3, 4, 5],
         ... )
         >>> lin_reg.predict([[10]])
         array([...])
         """
-        x_bounds = list(x_bounds) # Shouldn't be so large that this is a problem
+        x_bounds = list(self.x_bounds) # Shouldn't be so large that this is a problem
         if len(x_bounds) != 1:
             raise Exception(f'For now, the x_bounds array must consist of a single tuple, not {x_bounds}')
         meas = _make_private_theil_sen(
             x_bounds=x_bounds[0],
-            y_bounds=y_bounds,
-            scale=scale,
-            runs=runs,
-            candidates_count=candidates_count,
+            y_bounds=self.y_bounds,
+            scale=self.scale,
+            runs=self.runs,
+            candidates_count=self.candidates_count,
             fraction_bounds=self.fraction_bounds,
         )
         np = import_optional_dependency("numpy")
