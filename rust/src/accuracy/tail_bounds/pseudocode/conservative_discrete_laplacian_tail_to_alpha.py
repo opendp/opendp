79dc0151500478e2f0a8604cfe304efedff05f53
Michael Shoemate <shoeboxam@gmail.com>
2026-04-20 04:08:13 -0400

    respond to review 4/20

--- rust/src/accuracy/tail_bounds/pseudocode/conservative_discrete_laplacian_tail_to_alpha.py
+++ rust/src/accuracy/tail_bounds/pseudocode/conservative_discrete_laplacian_tail_to_alpha.py
@@ -1,6 +1,5 @@
 # type: ignore
 def conservative_discrete_laplacian_tail_to_alpha(scale: f64, tail: u32) -> f64:
-    t = f64.neg_inf_cast(tail)
-    numer = t.neg_inf_div(-scale).inf_exp()
-    denom = scale.recip().neg_inf_exp().neg_inf_add(1.0)
+    numer = f64.inf_cast(-tail / scale).inf_exp()
+    denom = f64.neg_inf_cast(1.0 / scale).neg_inf_exp().neg_inf_add(1.0)
     return numer.inf_div(denom)
