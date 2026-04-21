390650f9b3a4a9cf0381e6780ad5fa26369127ab
Michael Shoemate <shoeboxam@gmail.com>
2026-04-20 21:54:57 -0400

    rewrite 4/20

--- rust/src/measurements/randomized_response/pseudocode/make_randomized_response.py
+++ rust/src/measurements/randomized_response/pseudocode/make_randomized_response.py
@@ -1,6 +1,6 @@
 # type: ignore
 def make_randomized_response(categories: set[T], prob: f64):
-    input_domain = AtomDomain(bool)
+    input_domain = AtomDomain(T)
     input_metric = DiscreteMetric()
     output_measure = MaxDivergence()
 
@@ -18,7 +18,7 @@ def make_randomized_response(categories: set[T], prob: f64):
     if prob == 1.0:
         c = float("inf")
     else:
-        c = p.inf_div((1).neg_inf_sub(prob)) \
+        c = prob.inf_div((1).neg_inf_sub(prob)) \
             .inf_mul(num_categories.inf_sub(1)) \
             .inf_ln()
     
@@ -28,18 +28,18 @@ def make_randomized_response(categories: set[T], prob: f64):
         else: 
             return c
 
-    def function(truth: bool) -> bool:  # |\label{line:fn}|
-        index = categories.index(truth)
-        sample = usize.sample_uniform_int_below(
-            len(num_categories) - (0 if index == -1 else 1))
+    def function(truth: T) -> T:  # |\label{line:fn}|
+        index = find_index(categories, truth)
+        sample = sample_uniform_uint_below(
+            len(categories) - (1 if index is not None else 0))
         
-        if index != -1 and sample >= index:
+        if index is not None and sample >= index:
             sample += 1
         
         lie = categories[sample]
 
-        be_honest = sample_bernoulli_float(prob, false)
-        is_member = index != -1
+        be_honest = sample_bernoulli_float(prob, False)
+        is_member = index is not None
         return truth if be_honest and is_member else lie
     
     return Measurement(input_domain, function, input_metric, output_measure, privacy_map)
