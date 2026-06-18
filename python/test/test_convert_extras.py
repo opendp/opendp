58d779af1e0e17b36bac576da2bd667fa7e9ea7f
Eddie de Leon <eduardo_deleon@seas.harvard.edu>
2026-06-18 10:11:12 -0400

    Move polars import internal

--- python/test/test_convert_extras.py
+++ python/test/test_convert_extras.py
@@ -1,9 +1,6 @@
 import pytest
 
-
-import polars as pl
 from polars.testing import assert_frame_equal
-
 
 import opendp.prelude as dp
 from opendp.extras.polars import Bound, Margin
@@ -23,10 +20,10 @@ def test_extras_object(val_in, type_name):
     assert val_out == val_in
 
 
-@pytest.mark.parametrize("val_in, type_name", [
-    (pl.LazyFrame(schema={"A": pl.Int32, "B": pl.String}), "LazyFrame")
-])
-def test_extras_object_polars(val_in, type_name):
+def test_extras_object_polars():
+    pl = pytest.importorskip("polars")
+    val_in = pl.LazyFrame(schema={"A": pl.Int32, "B": pl.String})
+    type_name = "LazyFrame"
     obj = py_to_c(val_in, c_type=AnyObjectPtr, type_name=type_name)
     val_out = c_to_py(obj)
     assert_frame_equal(val_out, val_in)
