f4b9f254f386f6f285511cd71e2a8ff170e64e49
mccalluc <mccalluc@users.noreply.github.com>
2026-06-28 06:49:00 +0000

    Update bindings

---
+++ R/opendp/src/convert_elements.h
@@ -0,0 +1,42 @@
+#include <Rinternals.h>
+#include "opendp.h"
+
+// transformation
+AnyTransformation *sexp_to_anytransformationptr(SEXP data);
+SEXP anytransformationptr_to_sexp(AnyTransformation *input, SEXP info);
+
+// measurement
+AnyMeasurement *sexp_to_anymeasurementptr(SEXP data);
+SEXP anymeasurementptr_to_sexp(AnyMeasurement *input, SEXP info);
+
+// odometer
+AnyOdometer *sexp_to_anyodometerptr(SEXP data);
+SEXP anyodometerptr_to_sexp(AnyOdometer *input, SEXP info);
+
+// domain
+AnyDomain *sexp_to_anydomainptr(SEXP data);
+SEXP anydomainptr_to_sexp(AnyDomain *input, SEXP info);
+
+// metric
+AnyMetric *sexp_to_anymetricptr(SEXP data);
+SEXP anymetricptr_to_sexp(AnyMetric *input, SEXP info);
+
+// measure
+AnyMeasure *sexp_to_anymeasureptr(SEXP data);
+SEXP anymeasureptr_to_sexp(AnyMeasure *input, SEXP info);
+
+// function
+AnyFunction *sexp_to_anyfunctionptr(SEXP data);
+SEXP anyfunctionptr_to_sexp(AnyFunction *input, SEXP info);
+
+// privacy profile
+AnyObject *sexp_to_privacyprofileptr(SEXP data);
+SEXP privacyprofileptr_to_sexp(AnyObject *input, SEXP info);
+
+// queryable
+AnyObject *sexp_to_anyqueryableptr(SEXP data);
+SEXP anyqueryableptr_to_sexp(AnyObject *input, SEXP info);
+
+// odometer queryable
+AnyObject *sexp_to_anyodometerqueryableptr(SEXP data);
+SEXP anyodometerqueryableptr_to_sexp(AnyObject *input, SEXP info);
