420c1a0abac0c0da3e0a1998a1e0f40d4856e4e1
Eddie de Leon <eduardo_deleon@seas.harvard.edu>
2026-07-30 11:33:50 -0400

    Apply alp comment to cdp change

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -187,8 +187,6 @@ pub mod internal;
 pub mod measurements;
 pub mod measures;
 pub mod metrics;
-#[cfg(test)]
-pub(crate) mod test_rounding;
 pub mod traits;
 pub mod transformations;
 pub mod utilities;
