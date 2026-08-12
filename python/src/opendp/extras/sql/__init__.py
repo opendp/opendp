20960887046352465bba26afe83b014c79c08bc9
Chuck McCallum <mccallucc@gmail.com>
2026-08-12 10:22:59 -0400

    stub init

---
+++ python/src/opendp/extras/sql/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module requires extra installs: ``pip install 'opendp[sql]'``
+
+For convenience, all the members of this module are also available from :py:mod:`opendp.prelude`.
+We suggest importing under the conventional name ``dp``:
+
+.. code:: pycon
+
+    >>> import opendp.prelude as dp
+
+The members of this module will then be accessible at ``dp.sql``.
+"""
