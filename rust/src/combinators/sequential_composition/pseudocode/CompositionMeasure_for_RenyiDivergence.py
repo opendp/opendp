3ac6c80ab21eff552592a0b49a8954e2b4a725f1
Eddie de Leon <eduardo_deleon@seas.harvard.edu>
2026-09-16 16:31:01 -0400

    Add initial attempt at new proofs for compose k

--- rust/src/combinators/sequential_composition/pseudocode/CompositionMeasure_for_RenyiDivergence.py
+++ rust/src/combinators/sequential_composition/pseudocode/CompositionMeasure_for_RenyiDivergence.py
@@ -5,23 +5,15 @@ class CompositionMeasure(RenyiDivergence):
     ) -> Composability:
         return Composability.Concurrent
 
-    def compose(self, d_mids: Vec[Self_Distance]) -> Self_Distance:
+    def compose(self, d_mids: Vec[tuple[Self_Distance, u32]]) -> Self_Distance:
         # merge equal curves so that each is evaluated once, not once per copy
-        groups = []  # Vec<(Self_Distance, u32)> |\label{line:groups}|
-        for d_mid in d_mids:
-            group = next(
-                (group for group in groups
-                 if Arc.ptr_eq(group[0].function, d_mid.function)),
-                None,
-            )
-            if group is not None:
-                group[1] += 1
-            else:
-                groups.push((d_mid, 1))
+        groups = OrderedCounts()  # |\label{line:groups}|
+        for d_mid, k_i in d_mids:
+            groups.add(d_mid, k_i)
 
         def curve(alpha: float) -> float:  # |\label{line:curve}|
             epsilons = [
-                d_mid(alpha).inf_mul(k)  # |\label{line:inf-mul}|
+                d_mid(alpha).inf_mul(f64.from_(k))  # |\label{line:inf-mul}|
                 for d_mid, k in groups
             ]
 
--- rust/src/combinators/sequential_composition/pseudocode/CompositionMeasure_for_ZeroConcentratedDivergence.py
+++ rust/src/combinators/sequential_composition/pseudocode/CompositionMeasure_for_ZeroConcentratedDivergence.py
@@ -3,14 +3,10 @@ class CompositionMeasure(ZeroConcentratedDivergence):
     def composability(  # |\label{line:composability}|
         self, adaptivity: Adaptivity
     ) -> Composability:
-        match adaptivity:
-            case Adaptivity.FullyAdaptive:
-                return Composability.Sequential
-            case _:
-                return Composability.Concurrent
+        return Composability.Concurrent
 
-    def compose(self, d_mids: Vec[Self_Distance]) -> Self_Distance:
+    def compose(self, d_mids: Vec[tuple[Self_Distance, u32]]) -> Self_Distance:
         d_out = 0.0
-        for d_mid in d_mids:
-            d_out = d_out.inf_add(d_mid)
+        for d_mid, k_i in d_mids:
+            d_out = d_out.inf_add(d_mid.inf_mul(f64.from_(k_i)))
         return d_out
